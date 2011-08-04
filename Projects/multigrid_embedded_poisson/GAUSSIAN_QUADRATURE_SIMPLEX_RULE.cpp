a82b1340d2da73fa21a8576b0d1ce14ffbb0d7d1
unknown <Jeffrey@.(none)>
2011-08-04 09:07:23 -0700

    moving some cpp's into Jeffrey_Utilities

--- Projects/multigrid_embedded_poisson/GAUSSIAN_QUADRATURE_SIMPLEX_RULE.cpp
+++
@@ -1,7 +0,0 @@
-//#####################################################################
-// Copyright 2011, Jeffrey Hellrung
-// This file is part of PhysBAM whose distribution is governed by the
-// license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
-//#####################################################################
-
-#include <Jeffrey_Utilities/Math/GAUSSIAN_QUADRATURE_SIMPLEX_RULE.ipp>
