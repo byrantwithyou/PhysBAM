14a928e3c46cacff222dda32d331a009e7faa437
Craig Schroeder <cas43@cs.stanford.edu>
2011-10-09 16:05:15 -0700

    Adjust for multiple libs.

--- Projects/deformable_interface/build.sh
+++ Projects/deformable_interface/build.sh
@@ -2,7 +2,7 @@
 
 g++ -fPIC libmain.cpp -I./../../Public_Library -c -o libmain.os -g
 g++ -fPIC DEFORMABLE_EXAMPLE.cpp -I./../../Public_Library -c -o DEFORMABLE_EXAMPLE.os -g
-g++ -shared DEFORMABLE_EXAMPLE.os libmain.os -o libPhysBAM_Wrapper.so -lPhysBAM -L../../build/nocona/debug/Public_Library -g
+g++ -shared DEFORMABLE_EXAMPLE.os libmain.os -o libPhysBAM_Wrapper.so -lPhysBAM_PhysBAM_Solids -lPhysBAM_PhysBAM_Tools -lPhysBAM_PhysBAM_Geometry -lPhysBAM_PhysBAM_Dynamics -lPhysBAM_PhysBAM_Fluids -lz -L../../build/nocona/debug/Public_Library -g
 
 g++ -o test_sim main.cpp -ldl -g
 
