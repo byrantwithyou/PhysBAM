f07687557feccdc8338428c2e110aeb1d0fe8ddb
Craig Schroeder <snubdodecahedron@gmail.com>
2011-10-25 18:29:51 -0700

    Test triangles.

--- Projects/area_test/main.cpp
+++ Projects/area_test/main.cpp
@@ -43,21 +43,21 @@ bool Test()
     rn.Fill_Uniform(da(3),-e,e);
     rn.Fill_Uniform(da(4),-e,e);
 
-    fprintf(stderr, "1 1 1 setrgbcolor newpath 0 0 moveto 1000 0 lineto 1000 1000 lineto 0 1000 lineto closepath fill\n");
-    VECTOR<TV,4> dA1;
+//    fprintf(stderr, "1 1 1 setrgbcolor newpath 0 0 moveto 1000 0 lineto 1000 1000 lineto 0 1000 lineto closepath fill\n");
+    VECTOR<TV,4> G1;
     VECTOR<VECTOR<MATRIX<T,2>,4>,4> H1;
-    T A1 = Trapezoid_Intersection_Area(a(1),a(2),a(3),a(4),dA1,H1);
+    T A1 = Trapezoid_Intersection_Area(a(1),a(2),a(3),a(4),G1,H1);
 
-    VECTOR<TV,4> dA2;
+    VECTOR<TV,4> G2;
     VECTOR<VECTOR<MATRIX<T,2>,4>,4> H2;
-    T A2 = Trapezoid_Intersection_Area(a(1)+da(1),a(2)+da(2),a(3)+da(3),a(4)+da(4),dA2,H2);
+    T A2 = Trapezoid_Intersection_Area(a(1)+da(1),a(2)+da(2),a(3)+da(3),a(4)+da(4),G2,H2);
 
     VECTOR<T,8>& Va=(VECTOR<T,8>&)da;
-    VECTOR<T,8>& V1=(VECTOR<T,8>&)dA1;
-    VECTOR<T,8>& V2=(VECTOR<T,8>&)dA2;
+    VECTOR<T,8>& V1=(VECTOR<T,8>&)G1;
+    VECTOR<T,8>& V2=(VECTOR<T,8>&)G2;
 
-    T dA=Va.Dot_Product(Va,(V1+V2)/(T)2);
-    T aa=dA/e;
+    T G=Va.Dot_Product(Va,(V1+V2)/(T)2);
+    T aa=G/e;
     T bb=(A2-A1)/e;
     if((!aa != !bb) || fabs((aa-bb)/bb)>1e-5) printf("AG %g %g %g\n", aa, bb, fabs((aa-bb)/bb));
 
@@ -70,6 +70,61 @@ bool Test()
     T cc=dG2.Magnitude()/e;
     T dd=(dG2-dG1).Magnitude()/e;
     if((!cc != !dd) || dd/cc>1e-5) printf("AH %g %g %g       ", cc, dd, dd/cc),LOG::cout<<dG1<<" "<<dG2<<std::endl;
+
+    return true;
+}
+
+bool Tri_Test()
+{
+    typedef double T;
+    typedef VECTOR<double,2> TV;
+    VECTOR<TV,6> a;
+    rn.Fill_Uniform(a(1),0,1);
+    rn.Fill_Uniform(a(2),0,1);
+    rn.Fill_Uniform(a(3),0,1);
+    rn.Fill_Uniform(a(4),0,1);
+    rn.Fill_Uniform(a(5),0,1);
+    rn.Fill_Uniform(a(6),0,1);
+
+    T e=1e-4;
+    VECTOR<TV,6> da;
+    rn.Fill_Uniform(da(1),-e,e);
+    rn.Fill_Uniform(da(2),-e,e);
+    rn.Fill_Uniform(da(3),-e,e);
+    rn.Fill_Uniform(da(4),-e,e);
+    rn.Fill_Uniform(da(5),-e,e);
+    rn.Fill_Uniform(da(6),-e,e);
+
+//    fprintf(stderr, "1 1 1 setrgbcolor newpath 0 0 moveto 1000 0 lineto 1000 1000 lineto 0 1000 lineto closepath fill\n");
+    VECTOR<TV,6> G1;
+    VECTOR<VECTOR<MATRIX<T,2>,6>,6> H1;
+    T A1 = Triangle_Intersection_Area(TRIANGLE_2D<T>(a(1),a(2),a(3)),TRIANGLE_2D<T>(a(4),a(5),a(6)),G1,H1);
+
+    VECTOR<TV,6> G2;
+    VECTOR<VECTOR<MATRIX<T,2>,6>,6> H2;
+    T A2 = Triangle_Intersection_Area(TRIANGLE_2D<T>(a(1)+da(1),a(2)+da(2),a(3)+da(3)),TRIANGLE_2D<T>(a(4)+da(4),a(5)+da(5),a(6)+da(6)),G2,H2);
+
+    LOG::cout<<G1<<std::endl;
+    LOG::cout<<G2<<std::endl;
+
+    VECTOR<T,12>& Va=(VECTOR<T,12>&)da;
+    VECTOR<T,12>& V1=(VECTOR<T,12>&)G1;
+    VECTOR<T,12>& V2=(VECTOR<T,12>&)G2;
+
+    T G=Va.Dot_Product(Va,(V1+V2)/(T)2);
+    T aa=G/e;
+    T bb=(A2-A1)/e;
+    if((!aa != !bb) || fabs((aa-bb)/bb)>1e-5 || 1) printf("AG %g %g %g\n", aa, bb, fabs((aa-bb)/bb));
+
+    MATRIX<T,12> M1,M2;
+    for(int i=1;i<=12;i++) for(int j=1;j<=12;j++) M1(i,j)=H1((i+1)/2)((j+1)/2)((i+1)%2+1,(j+1)%2+1);
+    for(int i=1;i<=12;i++) for(int j=1;j<=12;j++) M2(i,j)=H2((i+1)/2)((j+1)/2)((i+1)%2+1,(j+1)%2+1);
+
+    VECTOR<T,12> dG1=(M1+M2)/(T)2*Va/e;
+    VECTOR<T,12> dG2=(V2-V1)/e;
+    T cc=dG2.Magnitude();
+    T dd=(dG2-dG1).Magnitude();
+    if((!cc != !dd) || dd/cc>1e-5 || 1) printf("AH %g %g %g       ", cc, dd, dd/cc),LOG::cout<<dG1<<" "<<dG2<<std::endl;
 
     return true;
 }
@@ -133,12 +188,13 @@ int main(int argc,char *argv[])
 //    fprintf(stderr, "%%!PS-Adobe-3.0 EPSF-3.0\n");
 //    fprintf(stderr, "%%%%BoundingBox: 0 0 1000 1000\n");
 
+    Tri_Test();
 
-    for(int k=0;k<10000;k++){
-        Test();
-    }
+//    for(int k=0;k<10000;k++){
+//        Test();
+//    }
 
-    for(int i=1;i<=100;i++) Test_Triangle_Intersection<TV>();
+//    for(int i=1;i<=100;i++) Test_Triangle_Intersection<TV>();
 
     return 0;
 }
