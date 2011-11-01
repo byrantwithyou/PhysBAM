dcb9e5338fe88ef70f4bc82c031c7dc123ec950f
Craig Schroeder <snubdodecahedron@gmail.com>
2011-10-31 23:04:07 -0700

    Pruning feature.

--- Public_Library/PhysBAM_Solids/PhysBAM_Deformables/Collisions_And_Interactions/VOLUME_COLLISIONS.cpp
+++ Public_Library/PhysBAM_Solids/PhysBAM_Deformables/Collisions_And_Interactions/VOLUME_COLLISIONS.cpp
@@ -7,6 +7,7 @@
 #include <PhysBAM_Geometry/Basic_Geometry/TRIANGLE_2D.h>
 #include <PhysBAM_Geometry/Spatial_Acceleration/BOX_HIERARCHY_DEFINITION.h>
 #include <PhysBAM_Geometry/Spatial_Acceleration/TRIANGLE_HIERARCHY_2D.h>
+#include <PhysBAM_Solids/PhysBAM_Deformables/Collisions_And_Interactions/SEGMENT_ORIGIN_AREAS.h>
 #include <PhysBAM_Solids/PhysBAM_Deformables/Collisions_And_Interactions/TRIANGLE_INTERSECTION.h>
 #include <PhysBAM_Solids/PhysBAM_Deformables/Collisions_And_Interactions/VOLUME_COLLISIONS.h>
 #include <algorithm>
@@ -55,16 +56,36 @@ Compute_Collision_Triangles(TRIANGULATED_AREA<T>& ta1,TRIANGULATED_AREA<T>& ta2)
     VOLUME_COLLISIONS_VISITOR<T> visitor(ta1,ta2);
     ta1.hierarchy->Intersection_List(*ta2.hierarchy,visitor,ZERO());
 
+    HASHTABLE<VECTOR<int,4>,int> to_process;
+    ARRAY_VIEW<TV> X(ta1.particles.X);
+
     for(int i=1;i<=visitor.pairs.m;i++){
         int a,b;visitor.pairs(i).Get(a,b);
-        VECTOR<TV,6> G;
-        VECTOR<VECTOR<MATRIX<T,2>,6>,6> H;
-        area+=Triangle_Intersection_Area(ta1.Get_Element(a),ta2.Get_Element(b),G,H);
-        VECTOR<int,6> I;
-        ta1.mesh.elements(a).Get(I(1),I(2),I(3));
-        ta2.mesh.elements(b).Get(I(4),I(5),I(6));
-        for(int k=1;k<=6;k++) gradient.Get_Or_Insert(I(k))+=G(k);
-        for(int k=1;k<=6;k++) for(int m=1;m<=6;m++) hessian.Get_Or_Insert(VECTOR<int,2>(I(k),I(m)))+=H(k)(m);}
+
+        VECTOR<int,6> me;
+        ta1.mesh.elements(a).Get(me(1),me(2),me(3));
+        ta2.mesh.elements(b).Get(me(4),me(5),me(6));
+
+        for(int i=1;i<=3;i++){
+            int in=i%3+1;
+            for(int j=1;j<=3;j++){
+                int jn=j%3+1;
+                VECTOR<int,4> I(me(i),me(in),me(j+3),me(jn+3));
+                int sign=1;
+                if(I(1)<I(2)){exchange(I(1),I(2));sign=-sign;}
+                if(I(3)<I(4)){exchange(I(3),I(4));sign=-sign;}
+                if(I(1)<I(3)){exchange(I(1),I(3));exchange(I(2),I(4));}
+                to_process.Get_Or_Insert(I)+=sign;}}}
+
+    for(typename HASHTABLE<VECTOR<int,4>,int>::ITERATOR it(to_process);it.Valid();it.Next()){
+        T sign=it.Data();
+        if(sign==0) continue;
+        VECTOR<int,4> I=it.Key();
+        SEGMENT_ORIGIN_AREAS::DATA<T,1,4> data;
+        Area_From_Segments(data,X(I(1)),X(I(2)),X(I(3)),X(I(4)));
+        area+=sign*data.V.x;
+        for(int k=1;k<=4;k++) gradient.Get_Or_Insert(I(k))+=sign*(const TV&)data.G[k-1];
+        for(int k=1;k<=4;k++) for(int m=1;m<=4;m++) hessian.Get_Or_Insert(VECTOR<int,2>(I(k),I(m)))+=sign*data.H[0][k-1][m-1];}
 }
 template class VOLUME_COLLISIONS<VECTOR<float,2> >;
 #ifndef COMPILE_WITHOUT_DOUBLE_SUPPORT
