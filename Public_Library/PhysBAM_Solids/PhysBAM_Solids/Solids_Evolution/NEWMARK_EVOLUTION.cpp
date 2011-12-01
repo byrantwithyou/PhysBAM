127c40f2d2bf2293686863aca6a5f3c4674d125c
Craig Schroeder <cas43@cs.stanford.edu>
2011-12-01 00:08:25 -0800

    test system

--- Public_Library/PhysBAM_Solids/PhysBAM_Solids/Solids_Evolution/NEWMARK_EVOLUTION.cpp
+++ Public_Library/PhysBAM_Solids/PhysBAM_Solids/Solids_Evolution/NEWMARK_EVOLUTION.cpp
@@ -236,6 +236,7 @@ Backward_Euler_Step_Velocity_Helper(const T dt,const T current_velocity_time,con
         OCTAVE_OUTPUT<T>(STRING_UTILITIES::string_sprintf("b-%i.txt",solve_id).c_str()).Write("b",B);}
 
     system.Project(V);
+    if(solids_parameters.implicit_solve_parameters.test_system) system.Test_System(S,R,F);
     if(!solver->Solve(system,V,B,F,S,R,AR,solids_parameters.implicit_solve_parameters.cg_tolerance,1,solids_parameters.implicit_solve_parameters.cg_iterations) && solids_parameters.implicit_solve_parameters.throw_exception_on_backward_euler_failure)
         throw std::runtime_error("Backward Euler Failed");
     if(print_matrix) OCTAVE_OUTPUT<T>(STRING_UTILITIES::string_sprintf("x-%i.txt",solve_id).c_str()).Write("x",V);
