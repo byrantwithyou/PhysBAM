5e7a22de0e8b9bd8db42ca505fcd27a555b1b6e6
Craig Schroeder <snubdodecahedron@gmail.com>
2012-01-11 19:26:48 -0800

    use right last frame

--- Projects/extrap_3d/sim.sh
+++ Projects/extrap_3d/sim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-extrap_3d -framerate 9.45 -parameter 10 -stiffen .2 -dampen 0 100 -image_size 30 -sigma_range 3 -dt 0.01 -last_frame 960 -poissons_ratio .45 -cutoff .001 -ether_drag 0 -o fig-tet-corot -use_corotated >&/dev/null
+extrap_3d -framerate 9.45 -parameter 10 -stiffen .2 -dampen 0 100 -image_size 30 -sigma_range 3 -dt 0.01 -last_frame 240 -poissons_ratio .45 -cutoff .001 -ether_drag 0 -o fig-tet-corot -use_corotated >&/dev/null
 extrap_3d -framerate 24 -parameter 10 -stiffen .2 -dampen 0 100 -image_size 30 -sigma_range 3 -dt 0.01 -last_frame 240 -poissons_ratio .45 -cutoff .001 -ether_drag 0 -o fig-tet-corot -use_corotated >&/dev/null
-extrap_3d -framerate 9.47 -parameter 10 -stiffen .2 -dampen 0 100 -image_size 30 -sigma_range 3 -dt 0.01 -last_frame 960 -poissons_ratio .45 -cutoff .001 -ether_drag 0 -o fig-tet-corot -use_corotated >&/dev/null
+extrap_3d -framerate 9.47 -parameter 10 -stiffen .2 -dampen 0 100 -image_size 30 -sigma_range 3 -dt 0.01 -last_frame 240 -poissons_ratio .45 -cutoff .001 -ether_drag 0 -o fig-tet-corot -use_corotated >&/dev/null
 
 for d in fig-tet-corot ; do
     mkdir -p $d/frames
