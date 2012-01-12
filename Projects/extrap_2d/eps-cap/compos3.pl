b366dd0f9b8c49db3d98245b00a2c00b83c39704
Craig Schroeder <snubdodecahedron@gmail.com>
2012-01-11 22:19:11 -0800

    multiple sets

--- Projects/extrap_2d/eps-cap/compos3.pl
+++ Projects/extrap_2d/eps-cap/compos3.pl
@@ -6,16 +6,18 @@ my $h=480;
 my $w=640;
 my $sr=3;
 my $s=($h-2*$mg)/(2*$sr);
-my $sh=$mg + $sr * $s;
+my $shx=$mg + $sr * $s;
+my $shy=$shx;
 my $arlen=.135;
+my $dispx=120;
 
 my $d="([0-9.eE+-]+)";
 my $nd="[^0-9.eE+-]+";
 
 sub pt
 {
-    my $x=$_[0] * $s + $sh;
-    my $y=$_[1] * $s + $sh;
+    my $x=$_[0] * $s + $shx;
+    my $y=$_[1] * $s + $shy;
     return sprintf("(%.2f,%.2f)", $x, $y);
 }
 
@@ -28,91 +30,114 @@ sub spt
 my $arrows='';
 my $arrow='';
 my $vertline='';
-my @trails=();
-my @triangles=();
+my $alltri='';
+my $alltrails='';
 my @cc=qw(coltri1 coltri2 coltri3);
 my %contour=();
 my $tristretchstr='';
 my $tristretchpts='';
-$"=",";
+$"="";
+
+my $dir=$ARGV[0];
+shift @ARGV;
+my $number=@ARGV;
 
 my $tristretch=0;
-while(<>)
+for $i (0..$#ARGV)
 {
-    if(/p $nd$d$nd$d$nd$d$nd$d\]/)
+    my @triangles=();
+    my @trails=();
+    my $last=($i==$#ARGV);
+    open F, "$dir/$ARGV[$i]";
+    while(<F>)
     {
-        if($1 + $2 > 0)
+        if(/p $nd$d$nd$d$nd$d$nd$d\]/ && $last)
         {
-            my $xx=$3*$arlen+$1;
-            my $yy=$4*$arlen+$2;
-            $arrows.="\\psline[linewidth=1px,linecolor=colarrow]{->}@{[&pt($1,$2)]}@{[&pt($xx,$yy)]}\n";
-            $arrows.="\\psline[linewidth=1px,linecolor=colarrowref]{->}@{[&pt(-$1,-$2)]}@{[&pt(-$xx,-$yy)]}\n";
-        }
-    }
-    if(/c /)
-    {
-        my $col=$cc[@trails];
-        my $trail='';
-        chomp;
-        my @pts=split /[\[\]()] ?[\[\]()]/, "$'";
-        shift @pts;
-        pop @pts;
-        my $p=-1;
-        for(@pts)
-        {
-            /$d $d/;
-            my $q=($2>0);
-            if($p!=$q)
+            if($1 + $2 > 0)
             {
-                $p=$q;
-                my $c=$p?$col:'invtri';
-                $trail.="\n\\psline[linewidth=5px,linecolor=$c]{c-c}".&spt($_);
+                my $xx=$3*$arlen+$1;
+                my $yy=$4*$arlen+$2;
+                $arrows.="\\psline[linewidth=1px,linecolor=colarrow]{->}@{[&pt($1,$2)]}@{[&pt($xx,$yy)]}\n";
+                $arrows.="\\psline[linewidth=1px,linecolor=colarrowref]{->}@{[&pt(-$1,-$2)]}@{[&pt(-$xx,-$yy)]}\n";
             }
-            $trail.=&spt($_);
         }
-        my $c=$p?$col:'invtri';
-        push @trails, "$trail\n\\pscircle[fillcolor=$c,linestyle=none,fillstyle=solid]@{[&spt($pts[$#pts])]} {6}\n";
-        if($tristretch)
+        if(/c /)
         {
-            $vertline = "\\psline[linecolor=arvelocity,fillcolor=arvelocity,fillstyle=solid,linewidth=2px]" . &pt($pts[$#pts],-$sr) . &pt($pts[$#pts],$sr) . "\n";
-        }
-    }
-    if(/u $nd$d$nd$d$nd$d$nd$d\]/)
-    {
-        $contour{"$1,$2"}=1;
-        $contour{"$3,$4"}=1;
-    }
-    if(/t /)
-    {
-        my @pts=split /[\[\]()] ?[\[\]()]/, "$'";
-        shift @pts;
-        pop @pts;
-        for(my $i=0;$i+2<@pts;$i+=3)
-        {
-            my $col=$cc[@triangles];
-            my ($a,$b,$c,$d,$e,$f)=((split ' ',$pts[$i]), (split ' ',$pts[$i+1]), (split ' ',$pts[$i+2]));
-            $c-=$a;
-            $d-=$b;
-            $e-=$a;
-            $f-=$b;
-            if($c*$f<$d*$e){$col='invtri'}
-            $triangle = "\\pspolygon[linecolor=black,fillcolor=$col,fillstyle=solid]";
-            $triangle .= &spt($pts[$i]) . &spt($pts[$i+1]) . &spt($pts[$i+2]) . "\n";
-            push @triangles, $triangle;
+            my $col=$cc[@trails];
+            my $trail='';
+            chomp;
+            my @pts=split /[\[\]()] ?[\[\]()]/, "$'";
+            shift @pts;
+            pop @pts;
+            my $p=-1;
+            for(@pts)
+            {
+                /$d $d/;
+                my $q=($2>0);
+                if($p!=$q)
+                {
+                    $p=$q;
+                    my $c=$p?$col:'invtri';
+                    if($last)
+                    {
+                        $trail.="\n\\psline[linewidth=5px,linecolor=$c]{c-c}".&spt($_);
+                    }
+                }
+                if($last)
+                {
+                    $trail.=&spt($_);
+                }
+            }
+            my $c=$p?$col:'invtri';
+            push @trails, "$trail\n\\pscircle[fillcolor=$c,linestyle=none,fillstyle=solid]@{[&spt($pts[$#pts])]} {6}\n";
             if($tristretch)
             {
-                my @p=split ' ',$pts[$i];
-                #$tristretchstr .= "\\psline[linecolor=black,fillcolor=black,fillstyle=solid,linewidth=2px]{->}" . &pt(@p) . &pt($p[0], $p[1]+.3) . "\n";
-                $arrow = "\\psline[linecolor=arvelocity,fillcolor=arvelocity,arrowinset=0,arrowlength=0.8,fillstyle=solid,linewidth=13px]{c->}" . &pt(@p) . &pt($p[0], $p[1]+.5) . "\n";
-                my @q=split ' ',$pts[$i+1];
-                $tristretchstr .= "\\psline[linecolor=coltriline,fillcolor=coltriline,fillstyle=solid,linewidth=1pt]" . &pt(3.3, $q[1]) . &pt(4.9, $q[1]) . "\n";
-                $tristretchpts .= "\\pscircle[fillcolor=black,linestyle=none,fillstyle=solid]".&spt($pts[$i+0])."{3}\n";
-                $tristretchpts .= "\\pscircle[fillcolor=black,linestyle=none,fillstyle=solid]".&spt($pts[$i+1])."{3}\n";
-                $tristretchpts .= "\\pscircle[fillcolor=black,linestyle=none,fillstyle=solid]".&spt($pts[$i+2])."{3}\n";
+                $vertline = "\\psline[linecolor=arvelocity,fillcolor=arvelocity,fillstyle=solid,linewidth=2px]" . &pt($pts[$#pts],-$sr) . &pt($pts[$#pts],$sr) . "\n";
             }
         }
+        if(/u $nd$d$nd$d$nd$d$nd$d\]/ && $i==$#ARGV)
+        {
+            $contour{"$1,$2"}=1;
+            $contour{"$3,$4"}=1;
+        }
+        if(/t /)
+        {
+            $shx = $shy + $i * $dispx;
+            my @pts=split /[\[\]()] ?[\[\]()]/, "$'";
+            shift @pts;
+            pop @pts;
+            for(my $i=0;$i+2<@pts;$i+=3)
+            {
+                my $col=$cc[@triangles];
+                my ($a,$b,$c,$d,$e,$f)=((split ' ',$pts[$i]), (split ' ',$pts[$i+1]), (split ' ',$pts[$i+2]));
+                $c-=$a;
+                $d-=$b;
+                $e-=$a;
+                $f-=$b;
+                if($c*$f<$d*$e){$col='invtri'}
+                $triangle = "\\pspolygon[linecolor=black,fillcolor=$col,fillstyle=solid]";
+                $triangle .= &spt($pts[$i]) . &spt($pts[$i+1]) . &spt($pts[$i+2]) . "\n";
+                push @triangles, $triangle;
+                if($tristretch)
+                {
+                    my @p=split ' ',$pts[$i];
+                    #$tristretchstr .= "\\psline[linecolor=black,fillcolor=black,fillstyle=solid,linewidth=2px]{->}" . &pt(@p) . &pt($p[0], $p[1]+.3) . "\n";
+                    $arrow = "\\psline[linecolor=arvelocity,fillcolor=arvelocity,arrowinset=0,arrowlength=0.8,fillstyle=solid,linewidth=13px]{c->}" . &pt(@p) . &pt($p[0], $p[1]+.5) . "\n";
+                    my @q=split ' ',$pts[$i+1];
+                    $tristretchstr .= "\\psline[linecolor=coltriline,fillcolor=coltriline,fillstyle=solid,linewidth=1pt]" . &pt(3.3, $q[1]) . &pt(4.9, $q[1]) . "\n";
+                    $tristretchpts .= "\\pscircle[fillcolor=black,linestyle=none,fillstyle=solid]".&spt($pts[$i+0])."{3}\n";
+                    $tristretchpts .= "\\pscircle[fillcolor=black,linestyle=none,fillstyle=solid]".&spt($pts[$i+1])."{3}\n";
+                    $tristretchpts .= "\\pscircle[fillcolor=black,linestyle=none,fillstyle=solid]".&spt($pts[$i+2])."{3}\n";
+                }
+            }
+            $shx = $shy;
+        }
+        if(/tristretch/){$tristretch=1;}
     }
-    if(/tristretch/){$tristretch=1;}
+    close F;
+
+    $alltri.="@triangles";
+    $alltrails.="@trails";
 }
 
 my $orig="\\pscircle[fillcolor=colcontour,linestyle=none,fillstyle=solid]@{[&pt(1,1)]} {11}\n";
@@ -120,8 +145,10 @@ my $contour = join '', map {&spt($_)} sort {$a=~/$d/;my $A=$1;$b=~/$d/;my $B=$1;
 $contour="\\psline[linewidth=5px,linecolor=colcontour]{c-c}$contour\n";
 
 $"='';
-my $wm1=$w-1;
-my $hm1=$h-1;
+my $H=$h;
+my $W=$w+$dispx*($number-1);
+my $wm1=$W-1;
+my $hm1=$H-1;
 print <<EOS;
 \\documentclass{article}
 \\usepackage{pstricks}
@@ -131,8 +158,8 @@ print <<EOS;
 \\definecolor{bg}{rgb}{1,1,1}
 \\definecolor{coltri1}{rgb}{0,0.9,0}
 \\definecolor{coltri2}{rgb}{0,0.25,1}
-\\definecolor{coltri3}{rgb}{1,0,0}
-\\definecolor{invtri}{rgb}{0,.9,.9}
+\\definecolor{coltri3}{rgb}{0,.9,.9}
+\\definecolor{invtri}{rgb}{1,0,0}
 \\definecolor{backtri}{rgb}{0.90,0.90,0.90}
 \\definecolor{ltbacktri}{rgb}{1,1,1}
 \\definecolor{vertline}{rgb}{0.575,0.575,0.585}
@@ -145,7 +172,7 @@ print <<EOS;
 \\noindent
 \\psset{unit=1px}
 \\begin{pspicture}(0,0)($wm1,$hm1)
-\\psframe[fillcolor=bg,fillstyle=solid,linecolor=bg](0,0)($w,$h)
+\\psframe[fillcolor=bg,fillstyle=solid,linecolor=bg](0,0)($W,$H)
 \\pspolygon[linecolor=backtri,fillcolor=backtri,fillstyle=solid]@{[&pt(-$sr,$sr)]}@{[&pt(-$sr,-$sr)]}@{[&pt($sr,-$sr)]}
 \\pspolygon[linecolor=ltbacktri,fillcolor=ltbacktri,fillstyle=solid]@{[&pt(-$sr,$sr)]}@{[&pt($sr,$sr)]}@{[&pt($sr,-$sr)]}
 
@@ -156,9 +183,9 @@ $orig
 $contour
 $tristretchstr
 $arrow
-@triangles
+$alltri
 $tristretchpts
-@trails
+$alltrails
 $arrows
 
 \\psframe[fillstyle=solid,linestyle=none,fillcolor=backtri]@{[&pt(-3,.02)]}@{[&pt(-2.5,.3)]}
--- Projects/extrap_2d/sim3.sh
+++ Projects/extrap_2d/sim3.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-extrap_2d -stiffen .1 -dampen 0 100 -image_size 30 -sigma_range 3 -dt 1 -last_frame 500 -poissons_ratio .45 -cutoff .001 -ether_drag 20 -framerate 48 -use_corotated -o fig-corot-with-inv -parameter 2
 
-for d in fig-corot-with-inv ; do
-    mkdir -p $d/frames
-    for f in `( cd $d/data ; echo *txt )` ; do
-        eps-cap/compos3.pl < $d/data/$f > $d.tex
-        latex $d.tex >/dev/null ; dvips $d.dvi ; convert $d.ps $d/frames/${f/txt/png}
-        rm -f $d.tex $d.dvi $d.ps
+function work()
+{
+    d=$1
+    shift
+    name=`echo "$@" | sed 's/\.txt./-/g' | sed 's/txt$/png/'`
+    eps-cap/compos3.pl $d/data "$@" > $d.tex
+    latex $d.tex >/dev/null ; dvips $d.dvi ; convert $d.ps $d/frames/$name
+#    rm -f $d.tex $d.dvi $d.ps
+}
+
+if false ; then
+    extrap_2d -stiffen .1 -dampen 0 100 -image_size 30 -sigma_range 3 -dt 1 -last_frame 500 -poissons_ratio .45 -cutoff .001 -ether_drag 20 -framerate 48 -use_corotated -o fig-corot-with-inv -parameter 2
+
+    for d in fig-corot-with-inv ; do
+        mkdir -p $d/frames
+        for f in `( cd $d/data ; echo *txt )` ; do
+            work $d/data/$f
+        done
     done
-done
+else
+    work fig-corot-with-inv 001.txt 020.txt 100.txt 200.txt
+fi
