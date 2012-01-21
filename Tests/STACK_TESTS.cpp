cba3f207855366b9608b6b81f65d1b406879065e
fanfu <fanfu@TurtleFairy.(none)>
2012-01-20 22:59:23 -0800

    indexing test

---
+++ Tests/STACK_TESTS.cpp
@@ -0,0 +1,61 @@
+//#####################################################################
+// Copyright 2012, Chenfanfu Jiang.
+// This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
+//#####################################################################
+// Class STACK_TESTS
+//#####################################################################
+
+#include <PhysBAM_Tools/Data_Structures/STACK.h>
+#include <PhysBAM_Tools/Utilities/TEST_BASE.h>
+#include <PhysBAM_Tools/Vectors/VECTOR.h>
+namespace PhysBAM{
+
+class STACK_TESTS:public TEST_BASE
+{
+    bool Test(const bool test,const char* str_test,int line,bool& ok) const
+    {if(test) return true;LOG::cout<<"FAILED: "<<str_test<<"    line "<<line<<std::endl;ok=false;return false;}
+
+public:
+    STACK_TESTS()
+        :TEST_BASE("stack")
+    {}
+
+    virtual ~STACK_TESTS(){}
+
+#define TEST(x) Test((x),#x,__LINE__,ok)
+
+    TEST_RESULT Run_Test(int n)
+    {
+        bool ok=true;
+        STACK<int> q;
+
+       TEST(q.Empty()==true);
+
+        q.Push(3);
+        q.Push(5);
+        TEST(q.Pop()==5);
+        TEST(q.Empty()==false);
+
+        q.Remove_All();
+        TEST(q.Empty()==true);
+        
+        STACK<int> s;
+        s.Preallocate(2);
+        TEST(s.Empty()==true);
+        s.Push(5);
+        s.Push(9);
+        TEST(s.Peek()==9);
+
+        s.Clean_Memory();
+        TEST(s.Empty()==true);
+        
+        return ok?success:failure;
+    }
+
+    int Number_Of_Tests() const
+    {return 1;}
+
+//#####################################################################
+};
+static STACK_TESTS stack_tests;
+}
