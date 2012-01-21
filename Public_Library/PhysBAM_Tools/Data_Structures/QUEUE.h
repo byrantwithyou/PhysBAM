03c1c8e925fc5ea5d0dc388a3fbffd6e93beeb7c
fanfu <fanfu@TurtleFairy.(none)>
2012-01-20 22:47:02 -0800

    indexing

--- Public_Library/PhysBAM_Tools/Data_Structures/QUEUE.h
+++ Public_Library/PhysBAM_Tools/Data_Structures/QUEUE.h
@@ -18,29 +18,29 @@ public:
     int front,back;
 
     explicit QUEUE(const int size)
-         :array(size+1),front(1),back(1)
+         :array(size+1),front(0),back(0)
     {}
 
     void Enqueue(const T& element)
-    {array(back)=element;if(++back>array.m) back=1;
+    {array(back)=element;if(++back>=array.m) back=0;
     assert(back!=front);} // dies if you run out of room
 
     void Safe_Enqueue(const T& element) // resizes array on overflow
-    {array(back)=element;if(++back>array.m) back=1;
+    {array(back)=element;if(++back>=array.m) back=0;
     if(back==front){
-        ARRAY<T> new_array(4*array.m/3+2);back=1;
-        for(int index=front;index<=array.m;index++) new_array(back++)=array(index);
-        for(int index=1;index<front;index++) new_array(back++)=array(index);
-        front=1;ARRAY<T>::Exchange_Arrays(array,new_array);}}
+        ARRAY<T> new_array(4*array.m/3+2);back=0;
+        for(int index=front;index<array.m;index++) new_array(back++)=array(index);
+        for(int index=0;index<front;index++) new_array(back++)=array(index);
+        front=0;array.Exchange(new_array);}}
 
     T Dequeue()
-    {assert(!Empty());int index=front;if(++front>array.m) front=1;return array(index);}
+    {assert(!Empty());int index=front;if(++front>=array.m) front=0;return array(index);}
 
     const T& Peek() const
     {return array(front);}
 
     const T& operator()(const int i)
-    {assert(i<Size());int index=front+i-1;if(index>array.m) index=index-array.m;return array(index);}
+    {assert(i<Size());int index=front+i;if(index>=array.m) index=index-array.m;return array(index);}
 
     int Size() const
     {if(back<front) return back+array.m-front;else return back-front;}
@@ -52,7 +52,7 @@ public:
     {return Size()==array.m-1;}
 
     void Remove_All()
-    {front=1;back=1;}
+    {front=0;back=0;}
 
 //#####################################################################
 };
