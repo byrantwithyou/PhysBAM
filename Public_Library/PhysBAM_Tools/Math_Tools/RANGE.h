4a82aea5888d5d3698c7e2ba58bc4ade76aba5aa
Alexey <alexey@Centurion.(none)>
2012-02-01 10:14:22 -0800

    update

--- Public_Library/PhysBAM_Tools/Math_Tools/RANGE.h
+++ Public_Library/PhysBAM_Tools/Math_Tools/RANGE.h
@@ -215,8 +215,12 @@ public:
     {return location.All_Greater_Equal(min_corner) && location.All_Less_Equal(max_corner);}
 
     bool Lazy_Inside_Half_Open(const TV& location) const
-    {return location.All_Greater_Equal(min_corner) && location.All_Less(max_corner);}
-
+    {
+        std::cout<<"location:  "<<location<<std::endl;
+        std::cout<<"min_corner:"<<min_corner<<std::endl;
+        std::cout<<"max_corner:"<<max_corner<<std::endl;
+        return location.All_Greater_Equal(min_corner) && location.All_Less(max_corner);}
+    
     bool Inside(const TV& location,const T thickness_over_two) const
     {return Thickened(-thickness_over_two).Lazy_Inside(location);}
 
