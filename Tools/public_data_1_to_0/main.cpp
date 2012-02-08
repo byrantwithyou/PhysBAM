80a222de563386561c7ddf168e45439f3c0505e9
Alexey <alexey@Centurion.(none)>
2012-02-07 18:36:46 -0800

    formats ready to comvert: tri, tri2, phi, tet

--- Tools/public_data_1_to_0/main.cpp
+++ Tools/public_data_1_to_0/main.cpp
@@ -28,17 +28,18 @@
 using namespace PhysBAM;
 using namespace FILE_UTILITIES;
 template<class T> void Convert_File(const std::string& ifilename,const std::string& ofilename);
-template<class T> void Convert_Tri2D_File(const std::string& ifilename,const std::string& ofilename);
 template<class T> void Convert_Tri_File(const std::string& ifilename,const std::string& ofilename);
+template<class T> void Convert_Tri2D_File(const std::string& ifilename,const std::string& ofilename);
 template<class T> void Convert_Phi_File(const std::string& ifilename,const std::string& ofilename);
-template<class T> void Convert_Curve_File(const std::string& ifilename,const std::string& ofilename);
-template<class T> void Convert_Curve2D_File(const std::string& ifilename,const std::string& ofilename);
 template<class T> void Convert_Tet_File(const std::string& ifilename,const std::string& ofilename);
 //#################################################################
 // Function main
 //#################################################################
 int main(int argc,char *argv[])
 {
+#ifndef COMPILE_WITH_READ_ONE_BASED_DATA
+    LOG::cerr<<"1-indexed data reading functionality is not enabled."<<std::endl;exit(1);
+#endif
     bool type_double=false;    
     Initialize_Geometry_Particle();
     Initialize_Read_Write_General_Structures();
@@ -73,28 +74,9 @@ template<class T> void Convert_File(const std::string& ifilename,const std::stri
         case TRI_FILE: Convert_Tri_File<T>(ifilename,ofilename);break;
         case TRI2D_FILE: Convert_Tri2D_File<T>(ifilename,ofilename);break;
         case PHI_FILE: Convert_Phi_File<T>(ifilename,ofilename);break;
-        case CURVE_FILE: Convert_Curve_File<T>(ifilename,ofilename);break;
-        case CURVE2D_FILE: Convert_Curve2D_File<T>(ifilename,ofilename);break;
         case TET_FILE: Convert_Tet_File<T>(ifilename,ofilename);break;
         default: LOG::cerr<<"Unrecognized file type "<<ifilename<<std::endl;}
     LOG::cout<<std::flush;
-}
-//#################################################################
-// Function Convert_Tri2D_File
-//#################################################################
-template<class T> void Convert_Tri2D_File(const std::string& ifilename,const std::string& ofilename)
-{
-    LOG::cout<<"TRI2D: "<<ifilename<<" -> "<<ofilename<<std::endl;
-    try{
-        TRIANGULATED_AREA<T>* area;
-        FILE_UTILITIES::Create_From_File<T>(ifilename,area);
-        for(int i=0; i<area->mesh.elements.m;i++){
-            area->mesh.elements(i)-=VECTOR<int,3>(1,1,1);
-            if (area->mesh.elements(i)(0)<0 || area->mesh.elements(i)(1)<0 || area->mesh.elements(i)(2)<0){
-                LOG::cerr<<"Negative vertex index"<<std::endl; PHYSBAM_FATAL_ERROR();}}
-        FILE_UTILITIES::Write_To_File<T>(ofilename,*area);
-    }
-    catch(FILESYSTEM_ERROR&){}
 }
 //#################################################################
 // Function Convert_Tri_File
@@ -106,10 +88,29 @@ template<class T> void Convert_Tri_File(const std::string& ifilename,const std::
         TRIANGULATED_SURFACE<T>* surface;
         FILE_UTILITIES::Create_From_File<T>(ifilename,surface);
         for(int i=0; i<surface->mesh.elements.m;i++){
-            surface->mesh.elements(i)-=VECTOR<int,3>(1,1,1);
-            if (surface->mesh.elements(i)(0)<0 || surface->mesh.elements(i)(1)<0 || surface->mesh.elements(i)(2)<0){
+            VECTOR<int,3>& element=surface->mesh.elements(i);
+            element-=1;
+            if (element.Min()<0){
                 LOG::cerr<<"Negative vertex index"<<std::endl; PHYSBAM_FATAL_ERROR();}}
         FILE_UTILITIES::Write_To_File<T>(ofilename,*surface);
+    }
+    catch(FILESYSTEM_ERROR&){}
+}
+//#################################################################
+// Function Convert_Tri2D_File
+//#################################################################
+template<class T> void Convert_Tri2D_File(const std::string& ifilename,const std::string& ofilename)
+{
+    LOG::cout<<"TRI2D: "<<ifilename<<" -> "<<ofilename<<std::endl;
+    try{
+        TRIANGULATED_AREA<T>* area;
+        FILE_UTILITIES::Create_From_File<T>(ifilename,area);
+        for(int i=0; i<area->mesh.elements.m;i++){
+            VECTOR<int,3>& element=area->mesh.elements(i);
+            element-=1;
+            if (element.Min()<0){
+                LOG::cerr<<"Negative vertex index"<<std::endl; PHYSBAM_FATAL_ERROR();}}
+        FILE_UTILITIES::Write_To_File<T>(ofilename,*area);
     }
     catch(FILESYSTEM_ERROR&){}
 }
@@ -123,30 +124,10 @@ template<class T> void Convert_Phi_File(const std::string& ifilename,const std::
         LEVELSET_IMPLICIT_OBJECT<VECTOR<T,3> >* surface;
         FILE_UTILITIES::Create_From_File<T>(ifilename,surface);
         VECTOR<int,3>& min_corner = surface->levelset.phi.domain.min_corner;
-        if (min_corner.x<0 || min_corner.y<0 || min_corner.z<0){
+        if (min_corner.Min()<0){
             LOG::cerr<<"Negative vertex index"<<std::endl; PHYSBAM_FATAL_ERROR();}
         FILE_UTILITIES::Write_To_File<T>(ofilename,*surface);
     }
-    catch(FILESYSTEM_ERROR&){}
-}
-//#################################################################
-// Function Convert_Curve_File
-//#################################################################
-template<class T> void Convert_Curve_File(const std::string& ifilename,const std::string& ofilename)
-{
-    try{
-        SEGMENTED_CURVE<VECTOR<T,3> >* curve;
-        FILE_UTILITIES::Create_From_File<T>(ifilename,curve);}
-    catch(FILESYSTEM_ERROR&){}
-}
-//#################################################################
-// Function Convert_Curve2D_File
-//#################################################################
-template<class T> void Convert_Curve2D_File(const std::string& ifilename,const std::string& ofilename)
-{
-    try{
-        SEGMENTED_CURVE_2D<T>* curve;
-        FILE_UTILITIES::Create_From_File<T>(ifilename,curve);}
     catch(FILESYSTEM_ERROR&){}
 }
 //#################################################################
@@ -154,9 +135,17 @@ template<class T> void Convert_Curve2D_File(const std::string& ifilename,const s
 //#################################################################
 template<class T> void Convert_Tet_File(const std::string& ifilename,const std::string& ofilename)
 {
+    LOG::cout<<"TET: "<<ifilename<<" -> "<<ofilename<<std::endl;
     try{
         TETRAHEDRALIZED_VOLUME<T>* tetrahedralized_volume;
-        FILE_UTILITIES::Create_From_File<T>(ifilename,tetrahedralized_volume);}
+        FILE_UTILITIES::Create_From_File<T>(ifilename,tetrahedralized_volume);
+        for(int i=0; i<tetrahedralized_volume->mesh.elements.m;i++){
+            VECTOR<int,4>& element=tetrahedralized_volume->mesh.elements(i);
+            element-=1;
+            if (element.Min()<0){
+                LOG::cerr<<"Negative vertex index"<<std::endl; PHYSBAM_FATAL_ERROR();}}
+        FILE_UTILITIES::Write_To_File<T>(ofilename,*tetrahedralized_volume);
+    }
     catch(FILESYSTEM_ERROR&){}
 }
 //#################################################################
