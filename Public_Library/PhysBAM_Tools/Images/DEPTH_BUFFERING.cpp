61b5206c8dbbee31158d035295d8e8abf747389f
Alexey <alexey@Centurion.(none)>
2012-03-10 19:59:34 -0800

    depth buffering

--- Public_Library/PhysBAM_Tools/Images/DEPTH_BUFFERING.cpp
+++ Public_Library/PhysBAM_Tools/Images/DEPTH_BUFFERING.cpp
@@ -1,5 +1,5 @@
 //#####################################################################
-// Copyright 2012, Alexey Stomakhin.
+// Copyright 2012, Alexey, Alexey Stomakhin.
 // This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
 //#####################################################################
 #include <PhysBAM_Tools/Images/DEPTH_BUFFERING.h>
@@ -7,35 +7,30 @@ using namespace PhysBAM;
 //#####################################################################
 // Constructor
 //#####################################################################
-template<class T> PRIMITIVE<T>::
-PRIMITIVE()
-{ 
-    type=EMPTY;
-}
-//#####################################################################
-// Constructor
-//#####################################################################
-template<class T> PRIMITIVE<T>::
-PRIMITIVE(const TV &a,const int style):style(style)
+template<class T> DISPLAY_PRIMITIVE<T>::
+DISPLAY_PRIMITIVE(const TV &a,const int style):style(style)
 { 
     type=POINT;
     vertices(0)=a;
+    vertices(1)=a;
+    vertices(2)=a;
 }
 //#####################################################################
 // Constructor
 //#####################################################################
-template<class T> PRIMITIVE<T>::
-PRIMITIVE(const TV &a,const TV &b,const int style):style(style)
+template<class T> DISPLAY_PRIMITIVE<T>::
+DISPLAY_PRIMITIVE(const TV &a,const TV &b,const int style):style(style)
 { 
     type=SEGMENT;
     vertices(0)=a;
     vertices(1)=b;
+    vertices(2)=b;
 }
 //#####################################################################
 // Constructor
 //#####################################################################
-template<class T> PRIMITIVE<T>::
-PRIMITIVE(const TV &a,const TV &b,const TV &c,const int style):style(style)
+template<class T> DISPLAY_PRIMITIVE<T>::
+DISPLAY_PRIMITIVE(const TV &a,const TV &b,const TV &c,const int style):style(style)
 { 
     type=TRIANGLE;
     vertices(0)=a;
@@ -43,21 +38,76 @@ PRIMITIVE(const TV &a,const TV &b,const TV &c,const int style):style(style)
     vertices(2)=c;
 }
 //#####################################################################
+// Function Initialize_Elements
+//#####################################################################
+template<class T> void DISPLAY_PRIMITIVE<T>::
+Initialize_Elements()
+{
+    elements.Clean_Memory();
+    elements.Append(vertices);
+}
+//#####################################################################
+// Function Initialize_Bounding_Box
+//#####################################################################
+template<class T> void DISPLAY_PRIMITIVE<T>::
+Initialize_Bounding_Box()
+{
+    bounding_box=RANGE<TV2>::Bounding_Box(
+        DB::Project(vertices(0)),
+        DB::Project(vertices(1)),
+        DB::Project(vertices(2)));
+}
+//#####################################################################
+// Function Cut_By_Primitive
+//#####################################################################
+template<class T> void DISPLAY_PRIMITIVE<T>::
+Cut_By_Primitive(const DISPLAY_PRIMITIVE<T> &p)
+{
+    if(!(type==POINT||p.type==POINT||RANGE<TV2>::Intersect(bounding_box,p.bounding_box).Empty())){
+        switch(p.type){
+            case SEGMENT:{
+                Cut_By_Plane(DB::Cutting_Plane(p.vertices(0),p.vertices(1)));
+            }break;
+            case TRIANGLE:{
+                Cut_By_Plane(DB::Cutting_Plane(p.vertices(0),p.vertices(1)));
+                Cut_By_Plane(DB::Cutting_Plane(p.vertices(1),p.vertices(2)));
+                Cut_By_Plane(DB::Cutting_Plane(p.vertices(2),p.vertices(0)));
+                Cut_By_Plane(PLANE<T>(p.vertices(0),p.vertices(1),p.vertices(2)));
+            }break;
+            default:;}}
+}
+//#####################################################################
+// Function Cut_By_Plane
+//#####################################################################
+template<class T> void DISPLAY_PRIMITIVE<T>::
+Cut_By_Plane(const PLANE<T> &p)
+{
+    
+}
+//#####################################################################
+// Function Cutting_Plane
+//#####################################################################
+template<class T> PLANE<T> DEPTH_BUFFERING<T>::
+Cutting_Plane(const TV &a,const TV &b)
+{ 
+    return PLANE<T>(Embed(Project(a-b).Rotate_Clockwise_90().Normalized()),a);
+}
+//#####################################################################
 // Function Add_Element
 //#####################################################################
 template<class T> int DEPTH_BUFFERING<T>::
 Add_Element(const TV &a,const int style)
 { 
-    primitives.Append(PRIMITIVE<T>(a,style));
+    primitives.Append(DISPLAY_PRIMITIVE<T>(a,style));
     return primitives.Size()-1;
 }
-template<class T> int DEPTH_BUFFERING<T>::
 //#####################################################################
 // Function Add_Element
 //#####################################################################
+template<class T> int DEPTH_BUFFERING<T>::
 Add_Element(const TV &a,const TV &b,const int style)
 {
-    primitives.Append(PRIMITIVE<T>(a,b,style));
+    primitives.Append(DISPLAY_PRIMITIVE<T>(a,b,style));
     return primitives.Size()-1;
 }
 //#####################################################################
@@ -66,7 +116,7 @@ Add_Element(const TV &a,const TV &b,const int style)
 template<class T> int DEPTH_BUFFERING<T>::
 Add_Element(const TV &a,const TV &b,const TV &c,const int style)
 {
-    primitives.Append(PRIMITIVE<T>(a,b,c,style));
+    primitives.Append(DISPLAY_PRIMITIVE<T>(a,b,c,style));
     return primitives.Size()-1;
 }
 //#####################################################################
@@ -75,16 +125,16 @@ Add_Element(const TV &a,const TV &b,const TV &c,const int style)
 template<class T> void DEPTH_BUFFERING<T>::
 Process_Primitives()
 {
-    for(int i=0;i<primitives.m;i++){
-        primitives(i).elements.Remove_All();
-        primitives(i).elements.Clean_Memory();}
-    
-    
+    for(int i=0;i<primitives.m;i++) {
+        primitives(i).Initialize_Elements();
+        primitives(i).Initialize_Bounding_Box();
+    };
+    for(int i=0;i<primitives.m;i++) for(int j=0;j<primitives.m;j++) if(i!=j) primitives(i).Cut_By_Primitive(primitives(j));
 }
 //#####################################################################
-template class PRIMITIVE<float>;
+template class DISPLAY_PRIMITIVE<float>;
 template class DEPTH_BUFFERING<float>;
 #ifndef COMPILE_WITHOUT_DOUBLE_SUPPORT
-template class PRIMITIVE<double>;
+template class DISPLAY_PRIMITIVE<double>;
 template class DEPTH_BUFFERING<double>;
 #endif
--- Public_Library/PhysBAM_Tools/Images/DEPTH_BUFFERING.h
+++ Public_Library/PhysBAM_Tools/Images/DEPTH_BUFFERING.h
@@ -11,41 +11,59 @@
 #include <PhysBAM_Tools/Math_Tools/RANGE.h>
 #include <PhysBAM_Tools/Vectors/VECTOR_2D.h>
 #include <PhysBAM_Tools/Vectors/VECTOR_3D.h>
+#include <PhysBAM_Geometry/Basic_Geometry/PLANE.h>
 
 namespace PhysBAM{
 
+template<class T> class DEPTH_BUFFERING;
+
 template<class T>
-class PRIMITIVE
+class DISPLAY_PRIMITIVE
 {
 public:
 
-    enum TYPE{EMPTY=0,POINT,SEGMENT,TRIANGLE};
     typedef VECTOR<T,3> TV;
+    typedef VECTOR<T,2> TV2;
+    typedef DEPTH_BUFFERING<T> DB;
+
+    enum TYPE{EMPTY=0,POINT,SEGMENT,TRIANGLE};
     
     VECTOR<TV,3> vertices;
     TYPE type;
     int style;
 
+    RANGE<TV2> bounding_box;
     ARRAY<VECTOR<TV, 3> > elements;
 
-    PRIMITIVE();
-    PRIMITIVE(const TV &a,const int style);
-    PRIMITIVE(const TV &a,const TV &b,const int style);
-    PRIMITIVE(const TV &a,const TV &b,const TV &c,const int style);
+    DISPLAY_PRIMITIVE() {type=EMPTY;}
+    DISPLAY_PRIMITIVE(const TV &a,const int style);
+    DISPLAY_PRIMITIVE(const TV &a,const TV &b,const int style);
+    DISPLAY_PRIMITIVE(const TV &a,const TV &b,const TV &c,const int style);
+
+    void Initialize_Bounding_Box();
+    void Initialize_Elements();
+    
+    void Cut_By_Primitive(const DISPLAY_PRIMITIVE<T> &p);
+    void Cut_By_Plane(const PLANE<T> &p);
 };
 
 template<class T>
 class DEPTH_BUFFERING
 {
     typedef VECTOR<T,3> TV;
+    typedef VECTOR<T,2> TV2;
 
-    ARRAY<PRIMITIVE<T> > primitives;
+    ARRAY<DISPLAY_PRIMITIVE<T> > primitives;
 
 public:
 
     DEPTH_BUFFERING(){}
     ~DEPTH_BUFFERING(){}
-    
+
+    static VECTOR<T,2> Project(const VECTOR<T,3> &a) {return VECTOR<T,2>(a(0),a(1));}
+    static VECTOR<T,3> Embed  (const VECTOR<T,2> &a) {return VECTOR<T,3>(a(0),a(1),0);}
+    static PLANE<T> Cutting_Plane(const TV &a,const TV &b);
+
     int Add_Element(const TV &a,const int style);
     int Add_Element(const TV &a,const TV &b,const int style);
     int Add_Element(const TV &a,const TV &b,const TV &c,const int style);
