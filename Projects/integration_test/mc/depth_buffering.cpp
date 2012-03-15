08f36b2d8deb924e70a0047b2b43ddc54a610703
Craig Schroeder <snubdodecahedron@gmail.com>
2012-03-14 21:45:51 -0700

    Random testing.

--- Projects/integration_test/mc/depth_buffering.cpp
+++ Projects/integration_test/mc/depth_buffering.cpp
@@ -7,6 +7,7 @@
 #include <PhysBAM_Tools/Parsing/PARSE_ARGS.h>
 #include <PhysBAM_Tools/Random_Numbers/RANDOM_NUMBERS.h>
 #include <PhysBAM_Geometry/Basic_Geometry/TRIANGLE_2D.h>
+#include <PhysBAM_Geometry/Basic_Geometry/TRIANGLE_3D.h>
 #include <PhysBAM_Geometry/Images/EPS_FILE.h>
 #include <PhysBAM_Geometry/Images/HIDDEN_SURFACE.h>
 #include <PhysBAM_Geometry/Images/HIDDEN_SURFACE_PRIMITIVES.h>
@@ -27,6 +28,24 @@ int main(int argc, char* argv[])
     parse_args.Parse(argc,argv);
     std::string file=parse_args.Get_String_Value("-o");
 
+    LOG::cout<<std::setprecision(16);
+    RANDOM_NUMBERS<T> random;
+    int N=50;
+    ARRAY<TV> colors(N);
+    random.Fill_Uniform(colors,0,1);
+
+#if 1
+    for(int i=0;i<N;i++){
+        TV a,b,c;
+        random.Fill_Uniform(a,0,1);
+        random.Fill_Uniform(b,0,1);
+        random.Fill_Uniform(c,0,1);
+        LOG::cout<<a<<"  "<<b<<"  "<<c<<std::endl;
+        if(TRIANGLE_3D<T>::Area(a,b,c)<1e-10) continue;
+        hsp.Add(a,b,c);}
+#endif
+    hs.Compute();
+
     VECTOR_IMAGE<T>* vi;
     if(file.length()>=4 && file.substr(file.length()-4)==".eps")
         vi=new EPS_FILE<T>(file);
@@ -38,16 +57,9 @@ int main(int argc, char* argv[])
     vi->cur_format.fill_style=1;
     vi->cur_format.line_style=0;
 
-    HIDDEN_SURFACE_PRIMITIVES<T> hsp;
-    HIDDEN_SURFACE<T> hs(hsp);
-    hsp.Add(TV(0,0,0),TV(1,0.2,0),TV(0.5,1.1,1));
-    hsp.Add(TV(0.1,-0.1,1),TV(1,0.1,1),TV(0,1,0));
-    hsp.Add(TV(-0.1,0.1,0),TV(1.1,0.3,-1),TV(1,0.6,2));
-    hs.Compute();
-
     for(int i=0;i<hsp.order.m;i++){
         SURFACE_PRIMITIVE<T>& sp=hsp.primitives(hsp.order(i));
-        vi->cur_format.fill_color=TV::Axis_Vector(sp.parent);
+        vi->cur_format.fill_color=colors(sp.parent);
         PHYSBAM_ASSERT(sp.num_vertices==3);
         vi->Draw_Object(sp.vertices(0).Remove_Index(2),sp.vertices(1).Remove_Index(2),sp.vertices(2).Remove_Index(2));}
 
