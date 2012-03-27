6aad3953d6a35d9ebdff2be566f23cc6c3e960c7
Craig Schroeder <snubdodecahedron@gmail.com>
2012-03-27 12:22:43 -0700

    finish implementation

--- Public_Library/PhysBAM_Tools/Symbolics/STATIC_POLYNOMIAL.h
+++ Public_Library/PhysBAM_Tools/Symbolics/STATIC_POLYNOMIAL.h
@@ -21,45 +21,64 @@ struct STATIC_POLYNOMIAL
     TV_INT size;
     STATIC_TENSOR<T,rank,d+1> terms;
 
-    STATIC_POLYNOMIAL();
     ~STATIC_POLYNOMIAL();
+
+    void Set_Term(TV_INT& power,T x)
+    {size=size.Componentwise_Max(power);terms(power)=x;}
 
     template<int d2>
     STATIC_POLYNOMIAL<T,rank,(d>d2?d:d2)> operator+ (const STATIC_POLYNOMIAL<T,rank,d2>& p)
-    {
-        STATIC_POLYNOMIAL<T,rank,(d>d2?d:d2)> r;
-        RANGE<TV_INT> range(TV_INT(),size+1),range2(TV_INT(),p.size+1);
-        for(UNIFORM_ARRAY_ITERATOR<rank> it(range);it.Valid();it.Next())
-            r.terms(it.index)+=terms(it.index);
-        for(UNIFORM_ARRAY_ITERATOR<rank> it(range2);it.Valid();it.Next())
-            r.terms(it.index)+=p.terms(it.index);
-        r.size=size.Componentwise_Max(p.size);
-        return r;
-    }
+    {STATIC_POLYNOMIAL<T,rank,(d>d2?d:d2)> r;r+=*this;r+=p;return r;}
 
     template<int d2>
     STATIC_POLYNOMIAL<T,rank,(d>d2?d:d2)> operator- (const STATIC_POLYNOMIAL<T,rank,d2>& p)
-    {
-        STATIC_POLYNOMIAL<T,rank,(d>d2?d:d2)> r;
-        RANGE<TV_INT> range(TV_INT(),size+1),range2(TV_INT(),p.size+1);
-        for(UNIFORM_ARRAY_ITERATOR<rank> it(range);it.Valid();it.Next())
-            r.terms(it.index)+=terms(it.index);
-        for(UNIFORM_ARRAY_ITERATOR<rank> it(range2);it.Valid();it.Next())
-            r.terms(it.index)-=p.terms(it.index);
-        r.size=size.Componentwise_Max(p.size);
-        return r;
-    }
+    {STATIC_POLYNOMIAL<T,rank,(d>d2?d:d2)> r;r+=*this;r-=p;return r;}
 
     template<int d2>
     STATIC_POLYNOMIAL<T,rank,(d+d2)> operator* (const STATIC_POLYNOMIAL<T,rank,d2>& p)
+    {STATIC_POLYNOMIAL<T,rank,(d+d2)> r;r.Multiply(*this,p,false);return r;}
+
+    template<int d2,int d3> // Result must fit
+    void Multiply(const STATIC_POLYNOMIAL<T,rank,d2>& a, const STATIC_POLYNOMIAL<T,rank,d3>& b, bool need_clear=true)
     {
-        STATIC_POLYNOMIAL<T,rank,(d+d2)> r;
-        RANGE<TV_INT> range(TV_INT(),size+1),range2(TV_INT(),p.size+1);
-        for(UNIFORM_ARRAY_ITERATOR<rank> it(range);it.Valid();it.Next())
+        if(&b==this){
+            if(&a==this){STATIC_POLYNOMIAL<T,rank,d3> copy(b);return Multiply(copy,copy);}
+            return Multiply(b,a);}
+        PHYSBAM_ASSERT((a.size+b.size).All_Less_Equal(TV_INT()+d));
+        RANGE<TV_INT> range(TV_INT(),a.size+1),range2(TV_INT(),b.size+1);
+        if(need_clear){
+            UNIFORM_ARRAY_ITERATOR<rank> it(range2);
+            it.Next();
+            for(;it.Valid();it.Next()) terms(it.index+a.size)=0;}
+        for(UNIFORM_ARRAY_ITERATOR<rank> it(range);it.Valid();it.Next()){
+            TV_INT index=size-it.index;
+            T x=terms(index);
+            terms(index)=0;
             for(UNIFORM_ARRAY_ITERATOR<rank> it2(range2);it2.Valid();it2.Next())
-                r.terms(it.index+it2.index)+=terms(it.index)*p.terms(it2.index);
-        r.size=size+p.size;
-        return r;
+                terms(index+it2.index)+=a.terms(index)*b.terms(it2.index);}
+        size=a.size+b.size;
+    }
+
+    template<int d2> // Result must fit
+    STATIC_POLYNOMIAL& operator+=(const STATIC_POLYNOMIAL<T,rank,d2>& a)
+    {
+        PHYSBAM_ASSERT(a.size.All_Less_Equal(TV_INT()+d));
+        RANGE<TV_INT> range(TV_INT(),a.size+1);
+        for(UNIFORM_ARRAY_ITERATOR<rank> it(range);it.Valid();it.Next())
+            terms(it.index)+=a.terms(it.index);
+        size=size.Componentwise_Max(a.size);
+        return *this;
+    }
+
+    template<int d2> // Result must fit
+    STATIC_POLYNOMIAL& operator-=(const STATIC_POLYNOMIAL<T,rank,d2>& a)
+    {
+        PHYSBAM_ASSERT(a.size.All_Less_Equal(TV_INT()+d));
+        RANGE<TV_INT> range(TV_INT(),a.size+1);
+        for(UNIFORM_ARRAY_ITERATOR<rank> it(range);it.Valid();it.Next())
+            terms(it.index)-=a.terms(it.index);
+        size=size.Componentwise_Max(a.size);
+        return *this;
     }
 
     STATIC_POLYNOMIAL Differentiate(int v) const
@@ -180,9 +199,106 @@ struct STATIC_POLYNOMIAL
 
     // T Integrate_Over_Primitive(const VECTOR<TV,3>& vertices) const; // triangle
     // T Integrate_Over_Primitive(const VECTOR<TV,2>& vertices) const; // interval
+    
+    T Integrate_Over_Primitive(const VECTOR<TV,2>& vertices) const
+    {
+        STATIC_POLYNOMIAL copy(*this);
+        
+        copy.Shift(vertices(0));
+        TV a=vertices(1)-vertices(0);
+        
+        T table[rank][d+1];
+        for(int v=0;v<rank;v++){
+            table[v][0]=1;
+            for(int i=1;i<=size(v);i++) table[v][i]=table[v][i-1]*a(v);}
+        
+        typedef VECTOR<T,1> TV1;
+        typedef VECTOR<int,1> TV_INT1;
+        
+        STATIC_POLYNOMIAL<T,1,d*rank> barycentric;
+        RANGE<TV_INT> range(TV_INT(),size+1);
+        for(UNIFORM_ARRAY_ITERATOR<rank> it(range);it.Valid();it.Next()){
+            T scale=1;
+            for(int v=0;v<TV::m;v++) scale*=table[v][it.index(v)];
+            barycentric.terms(TV_INT1(it.index.Sum()))=terms(it.index)*scale;}
+        barycentric.size=size.Sum();
+
+        T integral=0;
+        for(int i=0;i<barycentric.size(0);i++)
+             integral+=barycentric.terms(TV_INT1(i))/(i+1);
+
+        return integral*a.Magnitude();
+    }
+
+    T Integrate_Over_Primitive(const VECTOR<TV,3>& vertices) const
+    {
+        STATIC_POLYNOMIAL copy(*this);
+
+        copy.Shift(vertices(0));
+        TV a=vertices(1)-vertices(0);
+        TV b=vertices(2)-vertices(0);
+    
+        T table[rank][d+1][d+1];
+        for(int v=0;v<rank;v++){
+            table[v][0][0]=1;
+            for(int i=1;i<=size(v);i++){
+                table[v][i][0]=table[v][i-1][0]*a(v);
+                table[v][i][i]=table[v][i-1][i-1]*b(v);
+                for(int j=1;j<i;j++)
+                    table[v][i][j]=table[v][i-1][j-1]*b(v)+table[v][i-1][j]*a(v);}}
+    
+        typedef VECTOR<T,2> TV2;
+        typedef VECTOR<int,2> TV_INT2;
+
+        STATIC_POLYNOMIAL<T,2,d*rank> barycentric;
+        RANGE<TV_INT> range(TV_INT(),size+1);
+        for(UNIFORM_ARRAY_ITERATOR<rank> it(range);it.Valid();it.Next()){
+            STATIC_POLYNOMIAL<T,2,0> monomial;
+            monomial.Set_Term(TV_INT2(),copy.terms(it.index));
+            for(int v=0;v<TV::m;v++){
+                STATIC_POLYNOMIAL<T,2,d> variable;
+                int n=it.index(v);
+                for(int j=0;j<=n;j++)
+                    variable.terms(TV_INT2(j,n-j))=table[v][n][j];
+                variable.size=TV_INT2(n,n);
+                monomial.Multiply(monomial,variable);}
+            barycentric+=monomial;}
+    
+        int max_factorial=barycentric.Max_Power_Sum()+2;
+        T factorial[20];
+        factorial[0]=(T)1;
+        for(int i=1;i<=max_factorial;i++) factorial[i]=factorial[i-1]*i;        
+
+        T integral=0;
+        for(int i=0;i<barycentric.terms.m;i++){
+            TV_INT2 power=barycentric.terms(i).power;
+            integral+=barycentric.terms(i).coeff*factorial[power(0)]*factorial[power(1)]/factorial[power(0)+power(1)+2];}
+
+        return integral*TV::Cross_Product(a,b).Magnitude();
+    }
 };
 
-//template<class TV>
-//std::ostream& operator<< (std::ostream& o, const STATIC_POLYNOMIAL<TV>& p);
+template<class T,int rank,int d>
+std::ostream& operator<< (std::ostream& o, const STATIC_POLYNOMIAL<T,rank,d>& p)
+{
+    bool first=true;
+    o<<"(";
+    RANGE<VECTOR<int,rank> > range(VECTOR<int,rank>(),p.size+1);
+    for(UNIFORM_ARRAY_ITERATOR<rank> it(range);it.Valid();it.Next()){
+        T c=p.terms(it.index);
+        if(c<0){o<<"-";c=-c;}
+        else o<<(first?"":"+");
+        if(c!=1 || it.index==VECTOR<int,rank>()) o<<c;
+        first=false;
+
+        for(int j=0;j<d;j++)
+            if(it.index(j)>0){
+                o<<"abcdefghijklmnopqrstuvwxyz"[j];
+                if(it.index(j)>1)
+                    o<<'^'<<it.index(j);}}
+
+    if(first) o<<"0";
+    return o<<")";
+}
 }
 #endif
