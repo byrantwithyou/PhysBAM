246e5117618253ae1e22bfab23e8929d1d4af8f2
Craig Schroeder <snubdodecahedron@gmail.com>
2012-04-02 16:18:15 -0700

    add new system for Diego stuff.

---
+++ Public_Library/PhysBAM_Geometry/Finite_Elements/INTERFACE_FLUID_SYSTEM.cpp
@@ -0,0 +1,217 @@
+//#####################################################################
+// Copyright 2012.
+// This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
+//#####################################################################
+#include <PhysBAM_Tools/Grids_Uniform/GRID.h>
+#include <PhysBAM_Tools/Grids_Uniform/UNIFORM_GRID_ITERATOR_CELL.h>
+#include <PhysBAM_Tools/Krylov_Solvers/KRYLOV_VECTOR_BASE.h>
+#include <PhysBAM_Tools/Symbolics/STATIC_POLYNOMIAL.h>
+#include <PhysBAM_Tools/Utilities/DEBUG_CAST.h>
+#include <PhysBAM_Tools/Vectors/VECTOR_ND.h>
+#include <PhysBAM_Geometry/Basic_Geometry/SEGMENT_2D.h>
+#include <PhysBAM_Geometry/Basic_Geometry/TRIANGLE_3D.h>
+#include <PhysBAM_Geometry/Finite_Elements/BASIS_INTEGRATION_CUTTING.h>
+#include <PhysBAM_Geometry/Finite_Elements/BASIS_STENCIL_UNIFORM.h>
+#include <PhysBAM_Geometry/Finite_Elements/CELL_MAPPING.h>
+#include <PhysBAM_Geometry/Finite_Elements/INTERFACE_FLUID_SYSTEM.h>
+#include <PhysBAM_Geometry/Grids_Uniform_Computations/MARCHING_CUBES.h>
+using namespace PhysBAM;
+//#####################################################################
+// Constructor
+//#####################################################################
+template<class TV> INTERFACE_FLUID_SYSTEM<TV>::
+INTERFACE_FLUID_SYSTEM(const GRID<TV>& grid_input,const GRID<TV>& coarse_grid_input,const ARRAY<T,TV_INT>& phi_input)
+    :BASE(false,false),grid(grid_input),coarse_grid(coarse_grid_input),phi(phi_input)
+{
+}
+//#####################################################################
+// Destructor
+//#####################################################################
+template<class TV> INTERFACE_FLUID_SYSTEM<TV>::
+~INTERFACE_FLUID_SYSTEM()
+{
+}
+//#####################################################################
+// Function Compute
+//#####################################################################
+template<class TV> void INTERFACE_FLUID_SYSTEM<TV>::
+Compute(const VECTOR<T,2>& mu,VECTOR_T& rhs,const ARRAY<TV,TV_INT> f_body[2],const ARRAY<TV>& f_interface)
+{
+    BASIS_STENCIL_UNIFORM<TV,0> p_stencil(grid.dX);
+    BASIS_STENCIL_UNIFORM<TV,1> *u_stencil[TV::m],*udx_stencil[TV::m][TV::m];
+    p_stencil.Set_Center();
+    p_stencil.Set_Constant_Stencil();
+    p_stencil.Dice_Stencil();
+    for(int i=0;i<TV::m;i++){
+        u_stencil[i]=new BASIS_STENCIL_UNIFORM<TV,1>(grid.dX);
+        u_stencil[i]->Set_Face(i);
+        u_stencil[i]->Set_Multilinear_Stencil();
+        u_stencil[i]->Dice_Stencil();
+        for(int j=0;j<TV::m;j++){
+            udx_stencil[i][j]=new BASIS_STENCIL_UNIFORM<TV,1>(*u_stencil[i]);
+            udx_stencil[i][j]->Differentiate(j);
+            udx_stencil[i][j]->Dice_Stencil();}}
+
+    MARCHING_CUBES<TV>::Create_Surface(object,coarse_grid,phi);
+
+    CELL_MAPPING<TV> index_map_p(grid),*index_map_u[TV::m];
+    index_map_p.periodic.Fill(true);
+    for(int i=0;i<TV::m;i++){
+        index_map_u[i]=new CELL_MAPPING<TV>(grid);
+        index_map_u[i]->periodic.Fill(true);}
+
+    SYSTEM_MATRIX_HELPER<T> helper;
+    RANGE<TV_INT> boundary_conditions;
+    boundary_conditions.min_corner.Fill(BASIS_INTEGRATION_CUTTING<TV,2>::periodic);
+    boundary_conditions.max_corner.Fill(BASIS_INTEGRATION_CUTTING<TV,2>::periodic);
+
+    SYSTEM_MATRIX_HELPER<T> helper_uu[TV::m][TV::m],helper_p[TV::m],helper_q[TV::m];
+    BASIS_INTEGRATION_CUTTING<TV,2> bic(boundary_conditions,grid,coarse_grid,phi);
+
+    // Diagonal blocks
+    for(int i=0;i<TV::m;i++)
+        for(int j=0;j<TV::m;j++){
+            bic.Add_Block(helper_uu[i][i],*udx_stencil[i][j],*udx_stencil[i][j],*index_map_u[i],*index_map_u[i],mu*(1+(i==j)));}
+
+    // Off-diagonal blocks
+    for(int i=0;i<TV::m;i++)
+        for(int j=i+1;j<TV::m;j++)
+            bic.Add_Block(helper_uu[i][j],*udx_stencil[i][j],*udx_stencil[j][i],*index_map_u[i],*index_map_u[j],mu);
+
+    // Pressure blocks
+    for(int i=0;i<TV::m;i++)
+        bic.Add_Block(helper_p[i],*udx_stencil[i][i],p_stencil,*index_map_u[i],index_map_p,VECTOR<T,2>(1,1));
+
+    // Traction blocks
+    for(int i=0;i<TV::m;i++)
+        bic.Add_Block(helper_q[i],*u_stencil[i],*index_map_u[i],VECTOR<T,2>(1,1));
+
+    // Rhs traction blocks
+    SYSTEM_MATRIX_HELPER<T> helper_rhs_q[TV::m],helper_rhs_p[TV::m];
+    for(int i=0;i<TV::m;i++)
+        bic.Add_Block(helper_rhs_q[i],*u_stencil[i],*index_map_u[i],-(T).5*VECTOR<T,2>(1,-1));
+
+    // Rhs pressure blocks
+    for(int i=0;i<TV::m;i++)
+        bic.Add_Block(helper_rhs_p[i],*u_stencil[i],p_stencil,*index_map_u[i],index_map_p,VECTOR<T,2>(1,1));
+
+    bic.Compute();
+
+    INTERVAL<int> index_range_u[TV::m] = {INTERVAL<int>(0,index_map_u[0]->next_index)};
+    for(int i=1;i<TV::m;i++) index_range_u[i]=INTERVAL<int>(index_range_u[i-1].max_corner,index_range_u[i-1].max_corner+index_map_u[i]->next_index);
+    INTERVAL<int> index_range_p(index_range_u[TV::m-1].max_corner,index_range_u[TV::m-1].max_corner+index_map_p.next_index);
+    INTERVAL<int> index_range_q[TV::m] = {INTERVAL<int>(index_range_p.max_corner,index_range_p.max_corner+object.mesh.elements.m)};
+    for(int i=1;i<TV::m;i++) index_range_q[i]=INTERVAL<int>(index_range_q[i-1].max_corner,index_range_q[i-1].max_corner+object.mesh.elements.m);
+    system_size=index_range_q[TV::m-1].max_corner;
+
+    for(int i=0;i<TV::m;i++)
+        for(int j=0;j<TV::m;j++){
+            helper_uu[i][j].Shift(index_range_u[i].min_corner,index_range_u[j].min_corner);
+            helper.Add_Helper(helper_uu[i][j]);
+            if(i!=j) helper.Add_Transpose();}
+
+    for(int i=0;i<TV::m;i++){
+        helper_p[i].Shift(index_range_u[i].min_corner,index_range_p.min_corner);
+        helper.Add_Helper(helper_p[i]);
+        helper.Add_Transpose();}
+
+    for(int i=0;i<TV::m;i++){
+        helper_q[i].Shift(index_range_u[i].min_corner,index_range_q[i].min_corner);
+        helper.Add_Helper(helper_q[i]);
+        helper.Add_Transpose();}
+
+    for(int i=0;i<TV::m;i++){
+        helper_rhs_p[i].Shift(index_range_u[i].min_corner,index_range_p.min_corner);
+        helper_rhs_q[i].Shift(index_range_u[i].min_corner,index_range_q[i].min_corner);}
+
+    helper.Set_Matrix(system_size,system_size,matrix,1e-14);
+
+    VECTOR_ND<T> f(system_size);
+    for(int i=0;i<TV::m;i++)
+        for(int j=0;j<f_interface.m;j++)
+            f(j+index_range_q[i].min_corner)=f_interface(j)(i);
+    for(UNIFORM_GRID_ITERATOR_CELL<TV> it(grid);it.Valid();it.Next())
+        for(int i=0;i<TV::m;i++)
+            for(int s=0;s<2;s++){
+                int index=index_map_u[i]->Get_Index_Fixed(it.index, s);
+                if(index>=0)
+                    f(index)=f_body[s](it.index)(i);}
+
+    rhs.v.Resize(system_size);
+    for(int i=0;i<TV::m;i++)
+        for(int j=0;j<helper_rhs_q[i].data.m;j++)
+            rhs.v(helper_rhs_q[i].data(j).x)+=helper_rhs_q[i].data(j).z*f(helper_rhs_q[i].data(j).y);
+    for(int i=0;i<TV::m;i++)
+        for(int j=0;j<helper_rhs_p[i].data.m;j++)
+            rhs.v(helper_rhs_p[i].data(j).x)+=helper_rhs_p[i].data(j).z*f(helper_rhs_p[i].data(j).y);
+
+    for(int i=0;i<TV::m;i++){
+        null[i].Resize(system_size);
+        for(int j=index_range_u[i].min_corner;j<index_range_u[i].max_corner;j++)
+            null[i](j)=1;
+        null[i].Normalize();}
+
+    null_p.Resize(system_size);
+    for(int i=index_range_p.min_corner;i<index_range_p.max_corner;i++)
+        null_p(i)=1;
+    for(int i=0;i<TV::m;i++)
+        for(int j=index_range_q[i].min_corner;j<index_range_q[i].max_corner;j++)
+            null_p(j)=-object.Get_Element(j-index_range_q[i].min_corner).Normal()(i);
+    null_p.Normalize();
+}
+//#####################################################################
+// Function Multiply
+//#####################################################################
+template<class TV> void INTERFACE_FLUID_SYSTEM<TV>::
+Multiply(const KRYLOV_VECTOR_BASE<T>& x,KRYLOV_VECTOR_BASE<T>& result) const
+{
+    matrix.Times(debug_cast<const VECTOR_T&>(x).v,debug_cast<VECTOR_T&>(result).v);
+}
+//#####################################################################
+// Function Inner_Product
+//#####################################################################
+template<class TV> double INTERFACE_FLUID_SYSTEM<TV>::
+Inner_Product(const KRYLOV_VECTOR_BASE<T>& x,const KRYLOV_VECTOR_BASE<T>& y) const
+{
+    return debug_cast<const VECTOR_T&>(x).v.Dot(debug_cast<const VECTOR_T&>(y).v);
+}
+//#####################################################################
+// Function Convergence_Norm
+//#####################################################################
+template<class TV> typename TV::SCALAR INTERFACE_FLUID_SYSTEM<TV>::
+Convergence_Norm(const KRYLOV_VECTOR_BASE<T>& x) const
+{
+    return debug_cast<const VECTOR_T&>(x).v.Max_Abs();
+}
+//#####################################################################
+// Function Project
+//#####################################################################
+template<class TV> void INTERFACE_FLUID_SYSTEM<TV>::
+Project(KRYLOV_VECTOR_BASE<T>& x) const
+{
+    VECTOR_ND<T>& v=debug_cast<VECTOR_T&>(x).v;
+    v-=v.Dot(null_p)*null_p;
+    for(int i=0;i<TV::m;i++)
+        v-=v.Dot(null[i])*null[i];
+}
+//#####################################################################
+// Function Set_Boundary_Conditions
+//#####################################################################
+template<class TV> void INTERFACE_FLUID_SYSTEM<TV>::
+Set_Boundary_Conditions(KRYLOV_VECTOR_BASE<T>& x) const
+{
+}
+//#####################################################################
+// Function Project_Nullspace
+//#####################################################################
+template<class TV> void INTERFACE_FLUID_SYSTEM<TV>::
+Project_Nullspace(KRYLOV_VECTOR_BASE<T>& x) const
+{
+    Project(x);
+}
+template class INTERFACE_FLUID_SYSTEM<VECTOR<float,2> >;
+template class INTERFACE_FLUID_SYSTEM<VECTOR<float,3> >;
+#ifndef COMPILATE_WITHOUT_DOUBLE_SUPPORT
+template class INTERFACE_FLUID_SYSTEM<VECTOR<double,2> >;
+template class INTERFACE_FLUID_SYSTEM<VECTOR<double,3> >;
+#endif
---
+++ Public_Library/PhysBAM_Geometry/Finite_Elements/INTERFACE_FLUID_SYSTEM.h
@@ -0,0 +1,54 @@
+//#####################################################################
+// Copyright 2012.
+// This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
+//#####################################################################
+// Class INTERFACE_FLUID_SYSTEM
+//#####################################################################
+#ifndef __INTERFACE_FLUID_SYSTEM__
+#define __INTERFACE_FLUID_SYSTEM__
+#include <PhysBAM_Tools/Krylov_Solvers/KRYLOV_SYSTEM_BASE.h>
+#include <PhysBAM_Tools/Krylov_Solvers/KRYLOV_VECTOR_WRAPPER.h>
+#include <PhysBAM_Tools/Matrices/SPARSE_MATRIX_FLAT_MXN.h>
+#include <PhysBAM_Tools/Vectors/VECTOR.h>
+#include <PhysBAM_Geometry/Topology_Based_Geometry/SEGMENTED_CURVE_2D.h>
+#include <PhysBAM_Geometry/Topology_Based_Geometry/TOPOLOGY_BASED_SIMPLEX_POLICY.h>
+#include <PhysBAM_Geometry/Topology_Based_Geometry/TRIANGULATED_SURFACE.h>
+#include <PhysBAM_Dynamics/Coupled_Evolution/SYSTEM_MATRIX_HELPER.h>
+
+namespace PhysBAM{
+
+template<class TV> class GRID;
+template<class TV>
+class INTERFACE_FLUID_SYSTEM:public KRYLOV_SYSTEM_BASE<typename TV::SCALAR>
+{
+    typedef typename TV::SCALAR T;typedef VECTOR<int,TV::m> TV_INT;
+    typedef KRYLOV_VECTOR_WRAPPER<T,VECTOR_ND<T> > VECTOR_T;
+    typedef KRYLOV_SYSTEM_BASE<T> BASE;
+public:
+    SPARSE_MATRIX_FLAT_MXN<T> matrix;
+    const GRID<TV>& grid;
+    const GRID<TV>& coarse_grid;
+    const ARRAY<T,TV_INT>& phi;
+    VECTOR_ND<T> null[TV::m],null_p;
+    bool run_self_tests;
+    bool print_matrix;
+    bool print_rhs;
+    static int solve_id;
+    int system_size;
+    typename TOPOLOGY_BASED_SIMPLEX_POLICY<TV,TV::m-1>::OBJECT object;
+
+    INTERFACE_FLUID_SYSTEM(const GRID<TV>& grid_input,const GRID<TV>& coarse_grid_input,const ARRAY<T,TV_INT>& phi_input);
+    virtual ~INTERFACE_FLUID_SYSTEM();
+
+//#####################################################################
+    void Compute(const VECTOR<T,2>& mu,VECTOR_T& rhs,const ARRAY<TV,TV_INT> f_body[2],const ARRAY<TV>& f_interface);
+    void Multiply(const KRYLOV_VECTOR_BASE<T>& x,KRYLOV_VECTOR_BASE<T>& result) const;
+    double Inner_Product(const KRYLOV_VECTOR_BASE<T>& x,const KRYLOV_VECTOR_BASE<T>& y) const;
+    T Convergence_Norm(const KRYLOV_VECTOR_BASE<T>& x) const;
+    void Project(KRYLOV_VECTOR_BASE<T>& x) const;
+    void Set_Boundary_Conditions(KRYLOV_VECTOR_BASE<T>& x) const;
+    void Project_Nullspace(KRYLOV_VECTOR_BASE<T>& x) const;
+//#####################################################################
+};
+}
+#endif
