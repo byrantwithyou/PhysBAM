39cb734b684d230f2bae057b7d2cad2366a2e96b
Craig Schroeder <snubdodecahedron@gmail.com>
2012-07-11 17:18:41 -0700

    time and space varying solution

--- Projects/fluids_color_2d/FLUIDS_COLOR.h
+++ Projects/fluids_color_2d/FLUIDS_COLOR.h
@@ -26,22 +26,49 @@ public:
     using BASE::grid;using BASE::output_directory;using BASE::domain_boundary;using BASE::face_velocities;
     using BASE::particle_levelset_evolution;using BASE::write_substeps_level;using BASE::restart;using BASE::last_frame;
     using BASE::dt;using BASE::levelset_color;using BASE::mu;using BASE::dump_matrix;
+    int test_number;
+    int resolution;
+    int stored_last_frame;
+    bool user_last_frame;
+    T mu0,mu1;
+    T rho0,rho1;
+    T m,s,kg;
 
     FLUIDS_COLOR(const STREAM_TYPE stream_type,PARSE_ARGS& parse_args)
-        :PLS_FC_EXAMPLE<TV>(stream_type)
+        :PLS_FC_EXAMPLE<TV>(stream_type),test_number(0),resolution(32),stored_last_frame(0),user_last_frame(false),mu0(1),mu1(2),rho0(1),rho1(2),m(1),s(1),kg(1)
     {
-        int test_number=1,resolution=32;
-        last_frame=20;
+        last_frame=16;
+        parse_args.Set_Extra_Arguments(1,"<test-number>");
         parse_args.Add("-restart",&restart,"frame","restart frame");
-        parse_args.Add("-resolution",&resolution,"resolution","fine scale grid resolution");
+        parse_args.Add("-resolution",&resolution,"resolution","grid resolution");
         parse_args.Add("-substep",&write_substeps_level,"level","output-substep level");
-        parse_args.Add("-last_frame",&last_frame,"frame","last simulation frame");
+        parse_args.Add("-dt",&dt,"dt","time step size to use for simulation");
+        parse_args.Add("-steps",&this->time_steps_per_frame,"steps","number of time steps per frame");
+        parse_args.Add("-last_frame",&last_frame,&user_last_frame,"frame","number of frames to simulate");
         parse_args.Add("-dump_matrix",&dump_matrix,"dump out system and rhs");
+        parse_args.Add("-mu0",&mu0,"viscosity","viscosity for first fluid region");
+        parse_args.Add("-mu1",&mu1,"viscosity","viscosity for second fluid region");
+        parse_args.Add("-rho0",&rho0,"density","density for first fluid region");
+        parse_args.Add("-rho1",&rho1,"density","density for second fluid region");
+        parse_args.Add("-m",&m,"scale","meter scale");
+        parse_args.Add("-s",&s,"scale","second scale");
+        parse_args.Add("-kg",&kg,"scale","kilogram scale");
         parse_args.Print_Arguments();
         parse_args.Parse();
+        if(!STRING_UTILITIES::String_To_Value(parse_args.Extra_Arg(0),test_number)) throw VALUE_ERROR("The argument is not an integer.");
+
+        stored_last_frame=last_frame;
+        mu0*=kg/s;
+        mu1*=kg/s;
+        rho0*=kg/sqr(m);
+        rho1*=kg/sqr(m);
+        dt*=s;
 
         output_directory=STRING_UTILITIES::string_sprintf("Test_%d",test_number);
-        grid.Initialize(TV_INT()+resolution,RANGE<TV>::Unit_Box(),true);
+        switch(test_number){
+            case 0: grid.Initialize(TV_INT()+resolution,RANGE<TV>::Unit_Box()*m,true);break;
+            case 1: grid.Initialize(TV_INT()+resolution,RANGE<TV>::Unit_Box()*(2*(T)pi)*m,true);break;
+            default: PHYSBAM_FATAL_ERROR("Missing test number");}
     }
 
     void Write_Output_Files(const int frame)
@@ -49,9 +76,32 @@ public:
 
     void Initialize()
     {
-        dt=(T).1;
-        mu.Append(1);
+        switch(test_number){
+            case 0: Uniform_Translation_Periodic();break;
+            case 1: Taylor_Green_Vortex_Periodic();break;
+            default: PHYSBAM_FATAL_ERROR("Missing test number");}
+
+        if(user_last_frame) last_frame=stored_last_frame;
+    }
+
+    void Uniform_Translation_Periodic()
+    {
+        mu.Append(mu0);
         face_velocities.Fill(1);
+        levelset_color.phi.Fill(-1);
+        levelset_color.color.Fill(0);
+    }
+
+    TV Taylor_Green_Vortex_Velocity(const TV& X,T t)
+    {
+        return TV(sin(X.x)*cos(X.y),-cos(X.x)*sin(X.y))*exp(-2*mu0/rho0*t);
+    }
+
+    void Taylor_Green_Vortex_Periodic()
+    {
+        mu.Append(mu0);
+        for(UNIFORM_GRID_ITERATOR_FACE<TV> it(grid);it.Valid();it.Next())
+            face_velocities(it.Full_Index())=Taylor_Green_Vortex_Velocity(it.Location(),0)(it.Axis());
         levelset_color.phi.Fill(-1);
         levelset_color.color.Fill(0);
     }
