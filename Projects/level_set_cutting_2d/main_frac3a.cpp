e638475a4e055768bf1e218bf8a4f60787952723
Craig Schroeder <snubdodecahedron@gmail.com>
2012-09-20 11:52:04 -0700

    cut mesh for Fanfu

---
+++ Projects/level_set_cutting_2d/main_frac3a.cpp
@@ -0,0 +1,66 @@
+#include <PhysBAM_Tools/Arrays/INDIRECT_ARRAY.h>
+#include <PhysBAM_Tools/Data_Structures/UNION_FIND.h>
+#include <PhysBAM_Tools/Grids_Uniform/GRID.h>
+#include <PhysBAM_Tools/Math_Tools/RANGE.h>
+#include <PhysBAM_Tools/Parsing/PARSE_ARGS.h>
+#include <PhysBAM_Tools/Random_Numbers/RANDOM_NUMBERS.h>
+#include <PhysBAM_Tools/Read_Write/FILE_UTILITIES.h>
+#include <PhysBAM_Tools/Vectors/VECTOR.h>
+#include <PhysBAM_Geometry/Basic_Geometry/TETRAHEDRON.h>
+#include <PhysBAM_Geometry/Images/EPS_FILE.h>
+#include <PhysBAM_Geometry/Images/TEX_FILE.h>
+#include <PhysBAM_Geometry/Topology/TRIANGLE_MESH.h>
+#include <PhysBAM_Geometry/Topology_Based_Geometry/SEGMENTED_CURVE.h>
+#include <PhysBAM_Geometry/Topology_Based_Geometry/TETRAHEDRALIZED_VOLUME.h>
+#include <PhysBAM_Geometry/Topology_Based_Geometry/TRIANGULATED_AREA.h>
+#include <PhysBAM_Geometry/Topology_Based_Geometry/TRIANGULATED_SURFACE.h>
+#include "MARCHING_TETRAHEDRA_CUTTING.h"
+using namespace PhysBAM;
+
+#define P(x) LOG::cout<<#x<<" : "<<(x)<<std::endl;
+
+int main(int argc,char* argv[])
+{
+    typedef double T;
+    typedef float RW;
+    typedef VECTOR<int,3> TV_INT;
+    typedef VECTOR<int,4> E;
+    typedef VECTOR<T,3> TV;
+    typedef MARCHING_TETRAHEDRA_CUTTING<TV>::DATA DATA;
+
+    int seed=time(0),size=4;
+    T w=(T).2,d=(T).4;
+    PARSE_ARGS parse_args(argc,argv);
+    parse_args.Add("-s",&seed,"value","Seed");
+    parse_args.Add("-n",&size,"value","Domain size");
+    parse_args.Add("-w",&w,"value","Slit width");
+    parse_args.Add("-d",&d,"value","Slit depth");
+    parse_args.Parse();
+
+    GRID<TV> grid(TV_INT()+size,RANGE<TV>::Unit_Box());
+    TETRAHEDRALIZED_VOLUME<T> tv;
+    tv.Initialize_Cube_Mesh_And_Particles(grid);
+
+    ARRAY<T> phi0(tv.particles.X.m),phi1(tv.particles.X.m);
+    for(int i=0;i<tv.particles.X.m;i++){
+        phi0(i)=RANGE<TV>::Unit_Box().Thickened(-(T).1).Signed_Distance(tv.particles.X(i))>0?1:0;
+        TV Z=tv.particles.X(i)-TV((T).5,(T).5,1);
+        phi1(i)=sqr(Z.x/w)+sqr(Z.z/d)-1;}
+
+    ARRAY<E> out_mesh[2];
+    ARRAY<DATA> data[2];
+    ARRAY<TV_INT> surface[2];
+    ARRAY<int> node_map;
+    ARRAY<TV> X(tv.particles.X);
+    MARCHING_TETRAHEDRA_CUTTING<TV>::Fracture_Cutting(tv.mesh.elements,X,phi0,phi1,out_mesh,data,surface,node_map);
+    const char* files[2]={"in.tri.gz","out.tri.gz"};
+    for(int s=0;s<2;s++){
+        TRIANGULATED_SURFACE<T> ts;
+        ts.particles.Add_Elements(X.m);
+        ts.particles.X=X;
+        ts.mesh.elements=surface[s];
+        ts.Update_Number_Nodes();
+        FILE_UTILITIES::Write_To_File<RW>(files[s],ts);}
+
+    return 0;
+}
