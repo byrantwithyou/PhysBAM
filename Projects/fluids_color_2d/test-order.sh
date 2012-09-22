e879d7a2db3a56796e1341cbbec15ea694ec852b
Craig Schroeder <snubdodecahedron@gmail.com>
2012-09-21 22:07:42 -0700

    run tests and plot to determine convergence

---
+++ Projects/fluids_color_2d/test-order.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+for t in {0..11} ; do
+    for b in d n s ; do
+        (
+            T="o-$t-$b"
+            L=""
+            for r in {1..9} ; do
+                (
+                    echo -n "$((8*$r)) "
+                    nice fluids_color_2d -bc_$b -resolution $((8*$r)) $t -last_frame $r -dt `perl -e "print .05/$r"` -steps 1 -mu0 1 | grep error | tail -n 1
+                ) > $T-$r &
+                L="$L $T-$r"
+            done
+            wait
+
+            cat $L | sort -n > $T
+            rm $L
+
+            gnuplot -p -e "set terminal png ; set output 'conv-$t-$b.png' ; plot '$T' u (log10(\$1)):(log10(\$3)) , -2*x , -2*x-1 , -x-2"
+        ) &
+    done
+done
+wait
+times
+
