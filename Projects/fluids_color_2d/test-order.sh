f85b388da72ed2582d8d6ada61f53cffb0955898
Alexey Stomakhin <alexey@kraken.(none)>
2012-09-26 13:37:56 -0700

    add ./ to the script

--- Projects/fluids_color_2d/test-order.sh
+++ Projects/fluids_color_2d/test-order.sh
@@ -7,7 +7,7 @@ for t in {0..11} ; do
             for r in {1..9} ; do
                 (
                     echo -n "$((8*$r)) "
-                    nice fluids_color_2d -bc_$b -resolution 8 $t -last_frame 1 -dt .05 -steps 1 -mu0 1 -refine $r | grep error | tail -n 1
+                    nice ./fluids_color_2d -bc_$b -resolution 8 $t -last_frame 1 -dt .05 -steps 1 -mu0 1 -refine $r | grep error | tail -n 1
                 ) > $T-$r &
                 L="$L $T-$r"
             done
