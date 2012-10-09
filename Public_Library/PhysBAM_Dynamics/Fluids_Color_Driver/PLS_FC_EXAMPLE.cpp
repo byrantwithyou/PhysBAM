a7d92824f98314e582494ff9ba88e329641e1fec
Alexey Stomakhin <st.alexey@gmail.com>
2012-10-08 21:59:55 -0700

    do not project p null mode by default (think Neumann)

--- Public_Library/PhysBAM_Dynamics/Fluids_Color_Driver/PLS_FC_EXAMPLE.cpp
+++ Public_Library/PhysBAM_Dynamics/Fluids_Color_Driver/PLS_FC_EXAMPLE.cpp
@@ -18,7 +18,7 @@ PLS_FC_EXAMPLE(const STREAM_TYPE stream_type_input)
     write_substeps_level(-1),write_output_files(true),output_directory("output"),restart(0),
     number_of_ghost_cells(3),dt(1),time_steps_per_frame(1),use_preconditioner(true),max_iter(100000),
     dump_matrix(false),wrap(true),use_advection(true),use_reduced_advection(false),omit_solve(false),
-    number_of_colors(1),use_discontinuous_velocity(false),grid(TV_INT(),RANGE<TV>::Unit_Box(),true),
+    number_of_colors(1),use_discontinuous_velocity(false),use_p_null_mode(false),grid(TV_INT(),RANGE<TV>::Unit_Box(),true),
     particle_levelset_evolution(grid,number_of_ghost_cells),boundary(0),
     levelset_color(grid,*new ARRAY<T,TV_INT>,*new ARRAY<int,TV_INT>),collision_bodies_affecting_fluid(grid),debug_particles(*new DEBUG_PARTICLES<TV>)
 {
