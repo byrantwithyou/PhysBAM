db0a29c8c69e69e9f2a7805163ecf39bed691326
Alexey Stomakhin <st.alexey@gmail.com>
2012-10-29 20:28:59 -0700

    add missing file

---
+++ Public_Library/PhysBAM_Geometry/Finite_Elements/VOLUME_FORCE_SCALAR_COLOR.h
@@ -0,0 +1,26 @@
+//#####################################################################
+// Copyright 2012, Craig Schroeder, Alexey Stomakhin.
+// This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
+//#####################################################################
+// Class VOLUME_FORCE_SCALAR_COLOR
+//#####################################################################
+#ifndef __VOLUME_FORCE_SCALAR_COLOR__
+#define __VOLUME_FORCE_SCALAR_COLOR__
+
+#include <PhysBAM_Tools/Utilities/NONCOPYABLE.h>
+#include <PhysBAM_Tools/Vectors/VECTOR.h>
+
+namespace PhysBAM{
+
+template<class TV>
+struct VOLUME_FORCE_SCALAR_COLOR: public NONCOPYABLE
+{
+    typedef typename TV::SCALAR T;
+    
+    VOLUME_FORCE_SCALAR_COLOR(){}
+    virtual ~VOLUME_FORCE_SCALAR_COLOR(){}
+
+    virtual T F(const TV& X,int color)=0;
+};
+}
+#endif
