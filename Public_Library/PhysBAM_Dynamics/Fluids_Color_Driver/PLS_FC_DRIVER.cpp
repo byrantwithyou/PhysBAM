c8b76dc45313f0dedc31417ee4302b36fd1c09d0
Craig Schroeder <snubdodecahedron@gmail.com>
2012-11-06 16:16:00 -0800

    Set orders.

--- Public_Library/PhysBAM_Dynamics/Fluids_Color_Driver/PLS_FC_DRIVER.cpp
+++ Public_Library/PhysBAM_Dynamics/Fluids_Color_Driver/PLS_FC_DRIVER.cpp
@@ -109,11 +109,13 @@ Initialize()
         example.particle_levelset_evolution_multiple.particle_levelset.Set_Band_Width(6);
         example.collision_bodies_affecting_fluid.Initialize_Grids();
     }
-    
+
     example.particle_levelset_evolution_multiple.Particle_Levelset(0).number_of_ghost_cells=example.number_of_ghost_cells;
     example.particle_levelset_evolution_multiple.Set_Number_Particles_Per_Cell(16);
     example.particle_levelset_evolution_multiple.Set_Levelset_Callbacks(example);
     example.particle_levelset_evolution_multiple.Initialize_FMM_Initialization_Iterative_Solver(true);
+    example.particle_levelset_evolution_multiple.runge_kutta_order_levelset=3;
+    example.particle_levelset_evolution_multiple.runge_kutta_order_particles=3;
 
     example.particle_levelset_evolution_multiple.particle_levelset.levelset.Set_Custom_Boundary(*example.phi_boundary);
     example.particle_levelset_evolution_multiple.Bias_Towards_Negative_Particles(false);
