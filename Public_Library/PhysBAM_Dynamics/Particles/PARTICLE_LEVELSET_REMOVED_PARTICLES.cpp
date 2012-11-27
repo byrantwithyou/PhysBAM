53ce5fa57313cbaa36f64f2a9b5260f423445506
Craig Schroeder <snubdodecahedron@gmail.com>
2012-11-26 17:03:23 -0800

    use V from base

--- Public_Library/PhysBAM_Dynamics/Particles/PARTICLE_LEVELSET_REMOVED_PARTICLES.cpp
+++ Public_Library/PhysBAM_Dynamics/Particles/PARTICLE_LEVELSET_REMOVED_PARTICLES.cpp
@@ -11,9 +11,9 @@ namespace PhysBAM{
 //#####################################################################
 template<class TV> PARTICLE_LEVELSET_REMOVED_PARTICLES<TV>::
 PARTICLE_LEVELSET_REMOVED_PARTICLES()
-    :V(0,0),next(0)
+    :next(0)
 {
-    this->Add_Array(ATTRIBUTE_ID_V,&V);
+    this->Store_Velocity();
 }
 //#####################################################################
 // Constructor
