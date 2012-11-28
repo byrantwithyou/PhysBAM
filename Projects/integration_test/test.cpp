2088a8307ba1447912cc91372ba91c576748eb20
Craig Schroeder <snubdodecahedron@gmail.com>
2012-11-27 16:24:41 -0800

    try out triple junction stuff

--- Projects/integration_test/test.cpp
+++ Projects/integration_test/test.cpp
@@ -4,12 +4,21 @@
 //#####################################################################
 
 #include <PhysBAM_Tools/Arrays/ARRAY.h>
+#include <PhysBAM_Tools/Arrays/INDIRECT_ARRAY.h>
+#include <PhysBAM_Tools/Grids_Uniform/GRID.h>
+#include <PhysBAM_Tools/Grids_Uniform/UNIFORM_GRID_ITERATOR_CELL.h>
+#include <PhysBAM_Tools/Grids_Uniform/UNIFORM_GRID_ITERATOR_NODE.h>
+#include <PhysBAM_Tools/Grids_Uniform_Arrays/FACE_ARRAYS.h>
 #include <PhysBAM_Tools/Log/LOG.h>
+#include <PhysBAM_Tools/Math_Tools/RANGE_ITERATOR.h>
 #include <PhysBAM_Tools/Matrices/MATRIX.h>
 #include <PhysBAM_Tools/Matrices/SYMMETRIC_MATRIX_2X2.h>
 #include <PhysBAM_Tools/Matrices/SYMMETRIC_MATRIX_3X3.h>
 #include <PhysBAM_Tools/Random_Numbers/RANDOM_NUMBERS.h>
 #include <PhysBAM_Tools/Vectors/VECTOR.h>
+#include <PhysBAM_Geometry/Basic_Geometry/LINE_2D.h>
+#include <PhysBAM_Geometry/Geometry_Particles/DEBUG_PARTICLES.h>
+#include <PhysBAM_Geometry/Topology_Based_Geometry/TRIANGULATED_AREA.h>
 
 using namespace PhysBAM;
 
@@ -17,78 +26,187 @@ const int d=2;
 typedef double T;
 typedef VECTOR<T,d> TV;
 typedef VECTOR<int,d> TV_INT;
+std::string output_directory="output";
+typedef float RW;
+VECTOR<T,3> colors[3]={VECTOR<T,3>(1,0,0),VECTOR<T,3>(0,1,0),VECTOR<T,3>(0,0,1)};
 
-T Compute(const ARRAY<TV>& x,const TV& n,TV& dE,SYMMETRIC_MATRIX<T,d>& ddE)
+template<class T>
+T Weight_Function(T A) {return sqrt(abs(A))*sign(A);}
+
+template<class TV>
+GRID<TV>* Global_Grid(GRID<TV>* grid_in=0)
 {
-    TV average=x.Average();
-    
-    const SYMMETRIC_MATRIX<T,TV::m> nnt=SYMMETRIC_MATRIX<T,TV::m>::Outer_Product(n);
-    const SYMMETRIC_MATRIX<T,TV::m> dn=(T)1-nnt;    
+    static GRID<TV>* grid=0;
+    GRID<TV>* old_grid=grid;
+    if(grid_in) grid=grid_in;
+    return old_grid;
+}
 
-    T E=0;
-        
-    const T average_dot_n=average.Dot(n);
-    for(int i=0;i<x.m;i++){
-        const T xi_dot_n=x(i).Dot(n);
-        dE+=(x(i)-average)*(2*xi_dot_n);
-        E+=sqr(xi_dot_n);}
-    dE=dn*dE;
-    E-=sqr(average_dot_n)*x.m;
-    
-    for(int i=0;i<x.m;i++){
-        const TV xma=x(i)-average;
-        SYMMETRIC_MATRIX<T,TV::m> tmp=
-            MATRIX<T,TV::m>::Outer_Product(n,xma).Symmetric_Part()*(-2)
-            +(nnt*(T)3-1)*n.Dot(xma);
-        ddE+=tmp*(x(i).Dot(n));
-        ddE+=SYMMETRIC_MATRIX<T,TV::m>::Outer_Product(dn*x(i));}
-    ddE-=SYMMETRIC_MATRIX<T,TV::m>::Outer_Product(dn*average)*x.m;
-    ddE*=2;
-    
-    return E;
+template<class TV> DEBUG_PARTICLES<TV>& Get_Debug_Particles()
+{
+    static DEBUG_PARTICLES<TV> debug_particles;
+    return debug_particles;
+}
+
+template<class T, class TV>
+void Dump_Frame(const ARRAY<T,FACE_INDEX<TV::m> >& u,const char* title)
+{
+    static int frame=0;
+    if(frame==0){
+        FILE_UTILITIES::Create_Directory(output_directory);
+        FILE_UTILITIES::Create_Directory(output_directory+"/common");
+        LOG::Instance()->Copy_Log_To_File(output_directory+"/common/log.txt",false);
+        FILE_UTILITIES::Write_To_File<RW>(output_directory+"/common/grid.gz",*Global_Grid<TV>());}
+
+    char buff[100];
+    sprintf(buff, "%s/%i", output_directory.c_str(), frame);
+    FILE_UTILITIES::Create_Directory(buff);
+    FILE_UTILITIES::Write_To_File<RW>((std::string)buff+"/mac_velocities.gz",u);
+    if(title) FILE_UTILITIES::Write_To_Text_File((std::string)buff+"/frame_title",title);
+    Get_Debug_Particles<TV>().Write_Debug_Particles(STREAM_TYPE((RW())),output_directory,frame);
+    frame++;
+}
+
+template<class T,class TV>
+void Flush_Frame(const char* title)
+{
+    Dump_Frame<T,TV>(ARRAY<T,FACE_INDEX<TV::m> >(*Global_Grid<TV>()),title);
+}
+
+template<class T,class TV_INT>
+void Dump_Interface(const ARRAY<T,TV_INT>& p,const ARRAY<VECTOR<TV_INT,3> >& stencils,const VECTOR<T,3>& col)
+{
+    const GRID<TV>& grid=*Global_Grid<TV>();
+    for(int i=0;i<stencils.m;i++){
+        VECTOR<T,3> phi(p.Subset(stencils(i)));
+        for(int k=0;k<3;k++)
+            for(int m=k+1;m<3;m++){
+                int j=3-k-m;
+                if((phi(j)>0)!=(phi(k)>0)){
+                    if((phi(j)>0)!=(phi(m)>0)){
+                        TV Xj=grid.Node(stencils(i)(j)),Xk=grid.Node(stencils(i)(k)),Xm=grid.Node(stencils(i)(m));
+                        TV X=Xj+phi(j)/(phi(j)-phi(k))*(Xk-Xj);
+                        TV Y=Xj+phi(j)/(phi(j)-phi(m))*(Xm-Xj);
+                        Add_Debug_Object(VECTOR<TV,2>(X,Y),col);}}}}
+}
+
+template<class T,int dp1>
+T Bad_Fraction(const VECTOR<VECTOR<T,dp1>,3>& phi)
+{
+    int pos_mask=0,neg_mask=0;
+    for(int c=0;c<3;c++)
+        for(int j=0;j<dp1;j++){
+            if(phi(c)(j)>0) pos_mask|=1<<c;
+            else if(phi(c)(j)<0) neg_mask|=1<<c;}
+
+//    printf("%i %i\n", neg_mask, pos_mask);
+
+    if(pos_mask==7){
+        if(neg_mask==0) return 1;}
+    else if(neg_mask==7){
+        if(pos_mask==0) return -1;}
+    else return 0;
+
+    for(int c=0;c<3;c++)
+        for(int j=0;j<dp1;j++)
+            if(T pj=phi(c)(j))
+                for(int k=j+1;k<dp1;k++)
+                    if(T pk=phi(c)(k))
+                        if((pj>0)!=(pk>0)){
+                            VECTOR<VECTOR<T,dp1>,3> phi0=phi,phi1=phi;
+                            T th=pj/(pj-pk);
+                            for(int e=0;e<3;e++){
+                                T ej=phi(e)(j),ek=phi(e)(k),el=ej+th*(ek-ej);
+                                phi0(e)(j)=ej;
+                                phi0(e)(k)=el;
+                                phi1(e)(j)=el;
+                                phi1(e)(k)=ek;}
+                            phi0(c)(k)=phi1(c)(j)=0;
+                            return th*Bad_Fraction(phi0)+(1-th)*Bad_Fraction(phi1);}
+
+    PHYSBAM_FATAL_ERROR();
+}
+
+template<class T>
+void Evolve(VECTOR<VECTOR<T,3>,3> phi)
+{
+    for(int t=0;t<20;t++){
+        T A=Bad_Fraction(phi);
+        LOG::cout<<A<<std::endl;
+        phi-=(T).9*Weight_Function(A)+VECTOR<T,3>();}
+}
+
+template<class TV,class T>
+void Evolve_Step(GRID<TV>& grid,ARRAY<T,TV_INT> q[3],const ARRAY<T,TV_INT> p[3],const ARRAY<VECTOR<TV_INT,3> >& stencils)
+{
+    for(int i=0;i<3;i++) q[i].Fill(0);
+    for(int i=0;i<stencils.m;i++){
+        VECTOR<VECTOR<T,3>,3> phi;
+        for(int c=0;c<3;c++)
+            phi(c)=VECTOR<T,3>(p[c].Subset(stencils(i)));
+        T A=Bad_Fraction(phi);
+        for(int c=0;c<3;c++)
+            q[c].Subset(stencils(i))+=A;}
+
+    for(UNIFORM_GRID_ITERATOR_NODE<TV> it(grid);it.Valid();it.Next())
+        for(int c=0;c<3;c++)
+            q[c](it.index)=p[c](it.index)-(T).25*Weight_Function(q[c](it.index)*grid.dX.Product());
 }
 
 int main(int argc, char* argv[])
 {
-    for(int e=0;e<12;e++){
-        const int axis=e>>2;
-        const int offset1=e&1;
-        const int offset2=(e>>1)&1;
-        LOG::cout<<e<<"\t"<<axis<<"\t"<<offset1<<"\t"<<offset2<<"\t";
-        LOG::cout<<VECTOR<int,2>(offset1,offset2).Insert(0,axis)<<std::endl;}
+    Get_Debug_Particles<TV>();
 
+    LOG::cout<<"A"<<std::endl;
+//    VECTOR<T,3> a(2,-1,-1),b(-1,2,-1),c(-1,-1,2);
+    VECTOR<T,3> a(-2,-1,2),b(-2,1,-3),c(1,-2.5,-1);
 
-    // const int x_full=2;
-    // RANDOM_NUMBERS<T> rand;
+    VECTOR<VECTOR<T,3>,3> phi(a,b,c);
+    int res=50;
 
-    // ARRAY<TV> x1(x_full);
-    // TV n1;
-    // TV dE1;
-    // SYMMETRIC_MATRIX<T,d> ddE1;
+    T frac=Bad_Fraction(phi);
 
-    // x1(0)=TV(.2,.5);
-    // x1(1)=TV(.7,.1);
-    // n1=TV(1,1); n1.Normalize();
+    LOG::cout<<frac<<std::endl;
 
-    // ARRAY<TV> x2(x1);
-    // TV n2(n1);
-    // TV dE2;
-    // SYMMETRIC_MATRIX<T,d> ddE2;
+    GRID<TV> grid(TV_INT()+res,RANGE<TV>::Unit_Box(),true);
+    Global_Grid(&grid);
 
-    // const T e=1e-6;
-    // TV dn;
-    // rand.Fill_Uniform(dn,-e,e);
-    // n2+=dn;//n2.Normalize();
-    // dn=n2-n1;
+    ARRAY<T,TV_INT> p[3],q[3];
 
-    // T E1=Compute(x1,n1,dE1,ddE1);
-    // T E2=Compute(x2,n2,dE2,ddE2);
+    GRID<TV> grid_ta(TV_INT()+res,RANGE<TV>::Unit_Box()*res,false);
+    TRIANGULATED_AREA<T> ta;
+    ta.Initialize_Square_Mesh_And_Particles(grid_ta,true);
+    ARRAY<VECTOR<TV_INT,3> > stencils(ta.mesh.elements.m);
+    for(int i=0;i<ta.mesh.elements.m;i++)
+        for(int v=0;v<TV::m+1;v++)
+            stencils(i)(v)=TV_INT(rint(ta.particles.X(ta.mesh.elements(i)(v))));
 
-    // LOG::cout<<"dE  "<<(E2-E1-(dE1.Dot(dn)+dE2.Dot(dn))*(T).5)/e<<std::endl;
-    // LOG::cout<<"ddE "<<(dE2-dE1-(ddE1*dn+ddE2*dn)*(T).5).Max_Abs()/e<<std::endl;
+    LINE_2D<T> lines[3];
+    lines[0]=LINE_2D<T>(TV(.31,.31).Normalized(),TV(.51,.51));
+    lines[1]=LINE_2D<T>(-TV(.31,.11).Normalized(),TV(.51,.41));
+    lines[2]=LINE_2D<T>(-TV(.11,.41).Normalized(),TV(.41,.61));
 
-    // LOG::cout<<E1<<" "<<dE1<<" "<<ddE1<<std::endl;
-    // LOG::cout<<E2<<" "<<dE2<<" "<<ddE2<<std::endl;
+    for(int c=0;c<3;c++){
+        p[c].Resize(grid.Node_Indices(3));
+        q[c].Resize(grid.Node_Indices(3));
+        for(UNIFORM_GRID_ITERATOR_NODE<TV> it(grid,3);it.Valid();it.Next())
+            p[c](it.index)=lines[c].Signed_Distance(it.Location());}
 
+    for(int c=0;c<3;c++)
+        Dump_Interface(p[c],stencils,colors[c]);
+    Flush_Frame<T,TV>("Initial");
+
+    for(int i=0;i<5;i++){
+        Evolve_Step(grid,q,p,stencils);
+
+        for(int c=0;c<3;c++)
+            p[c].Exchange(q[c]);
+
+        for(int c=0;c<3;c++)
+            Dump_Interface(p[c],stencils,colors[c]);
+        Flush_Frame<T,TV>("Phew");}
+    Flush_Frame<T,TV>("Phew");
+
+    LOG::Finish_Logging();
     return 0;
 }
