545a3d6950556f18e8fb0e5ff4da202e531dc55e
Craig Schroeder <snubdodecahedron@gmail.com>
2012-11-28 17:49:48 -0800

    working on constructing pairwise level sets

--- Projects/integration_test/test.cpp
+++ Projects/integration_test/test.cpp
@@ -5,6 +5,9 @@
 
 #include <PhysBAM_Tools/Arrays/ARRAY.h>
 #include <PhysBAM_Tools/Arrays/INDIRECT_ARRAY.h>
+#include <PhysBAM_Tools/Data_Structures/HASHTABLE.h>
+#include <PhysBAM_Tools/Data_Structures/TRIPLE.h>
+#include <PhysBAM_Tools/EXTRAPOLATION_HIGHER_ORDER_POLY.h>
 #include <PhysBAM_Tools/Grids_Uniform/GRID.h>
 #include <PhysBAM_Tools/Grids_Uniform/UNIFORM_GRID_ITERATOR_CELL.h>
 #include <PhysBAM_Tools/Grids_Uniform/UNIFORM_GRID_ITERATOR_NODE.h>
@@ -12,6 +15,7 @@
 #include <PhysBAM_Tools/Log/LOG.h>
 #include <PhysBAM_Tools/Math_Tools/RANGE_ITERATOR.h>
 #include <PhysBAM_Tools/Matrices/MATRIX.h>
+#include <PhysBAM_Tools/Matrices/ROTATION.h>
 #include <PhysBAM_Tools/Matrices/SYMMETRIC_MATRIX_2X2.h>
 #include <PhysBAM_Tools/Matrices/SYMMETRIC_MATRIX_3X3.h>
 #include <PhysBAM_Tools/Parsing/PARSE_ARGS.h>
@@ -78,7 +82,7 @@ void Flush_Frame(const char* title)
 }
 
 template<class T,class TV_INT>
-void Dump_Interface(const ARRAY<T,TV_INT>& p,const ARRAY<VECTOR<TV_INT,3> >& stencils,const VECTOR<T,3>& col)
+void Dump_Interface(ARRAY_VIEW<T,TV_INT> p,const ARRAY<VECTOR<TV_INT,3> >& stencils,const VECTOR<T,3>& col)
 {
     typedef VECTOR<T,TV_INT::m> TV;
     const GRID<TV>& grid=*Global_Grid<TV>();
@@ -96,7 +100,7 @@ void Dump_Interface(const ARRAY<T,TV_INT>& p,const ARRAY<VECTOR<TV_INT,3> >& ste
 }
 
 template<class T,class TV_INT>
-void Dump_Interface(const ARRAY<T,TV_INT>& p,const ARRAY<VECTOR<TV_INT,4> >& stencils,const VECTOR<T,3>& col)
+void Dump_Interface(ARRAY_VIEW<T,TV_INT> p,const ARRAY<VECTOR<TV_INT,4> >& stencils,const VECTOR<T,3>& col)
 {
     typedef VECTOR<T,TV_INT::m> TV;
     const GRID<TV>& grid=*Global_Grid<TV>();
@@ -174,6 +178,18 @@ T Bad_Fraction(const VECTOR<VECTOR<T,dp1>,3>& phi)
 }
 
 template<class T>
+struct PAIRWISE_LEVEL_SET_DATA
+{
+    T phi;
+    int valid_flags;
+    VECTOR<short,2> trust;
+
+    PAIRWISE_LEVEL_SET_DATA()
+        :phi(FLT_MAX),valid_flags(0),trust(-1,-1)
+    {}
+};
+
+template<class T>
 void Evolve(VECTOR<VECTOR<T,3>,3> phi)
 {
     for(int t=0;t<20;t++){
@@ -183,7 +199,7 @@ void Evolve(VECTOR<VECTOR<T,3>,3> phi)
 }
 
 template<class TV,class T,class TV_INT>
-void Evolve_Step(GRID<TV>& grid,ARRAY<T,TV_INT> q[3],const ARRAY<T,TV_INT> p[3],const ARRAY<VECTOR<TV_INT,TV_INT::m+1> >& stencils)
+void Evolve_Step(GRID<TV>& grid,ARRAY_VIEW<T,TV_INT> q[3],const ARRAY_VIEW<T,TV_INT> p[3],const ARRAY<VECTOR<TV_INT,TV_INT::m+1> >& stencils)
 {
     for(int i=0;i<3;i++) q[i].Fill(0);
     for(int i=0;i<stencils.m;i++){
@@ -197,6 +213,84 @@ void Evolve_Step(GRID<TV>& grid,ARRAY<T,TV_INT> q[3],const ARRAY<T,TV_INT> p[3],
     for(UNIFORM_GRID_ITERATOR_NODE<TV> it(grid);it.Valid();it.Next())
         for(int c=0;c<3;c++)
             q[c](it.index)=p[c](it.index)-(T).25*Weight_Function(q[c](it.index)*grid.dX.Product());
+}
+
+template<class T,class TV,class TV_INT>
+void Compute_Pairwise_Level_Set_Data(const GRID<TV>& grid,const ARRAY<ARRAY<T,TV_INT> >& phi,int ghost,const ARRAY<VECTOR<TV_INT,TV::m+1> >& stencils,ARRAY<ARRAY<ARRAY<T,TV_INT> > >& pairwise_phi)
+{
+    T trust_buffer=grid.dX.Max(),valid_width=ghost*grid.dX.Max(),extent=4*valid_width;
+    int extrap_width=2*ghost+3;
+    ARRAY<PAIRWISE_LEVEL_SET_DATA<T>,TV_INT> pairwise_data(grid.Node_Indices(ghost));
+
+    for(UNIFORM_GRID_ITERATOR_NODE<TV> it(grid,ghost);it.Valid();it.Next()){
+        PAIRWISE_LEVEL_SET_DATA<T>& data=pairwise_data(it.index);
+        VECTOR<T,3> min(FLT_MAX,FLT_MAX,FLT_MAX);
+        for(int i=0;i<phi.m;i++){
+            T p=phi(i)(it.index);
+            if(p<min(0)){min=min.Remove_Index(2).Insert(p,0);data.trust=data.trust.Remove_Index(1).Insert(i,0);}
+            else if(p<min(1)){min=min.Remove_Index(2).Insert(p,1);data.trust(1)=i;}
+            else if(p<min(2)) min(2)=p;
+            if(p<=extent) data.valid_flags|=1<<i;}
+        min-=(min(0)+min(1))/2; // In case of boundary conditions
+        if(min(1)>valid_width){
+            data=PAIRWISE_LEVEL_SET_DATA<T>();
+            continue;}
+        if(min(1)>min(2)-trust_buffer){
+            data.trust=VECTOR<short,2>(-1,-1);
+            continue;}
+        data.phi=min.x;
+        if(data.trust.y<data.trust.x){
+            exchange(data.trust.x,data.trust.y);
+            data.phi=min.y;}}
+
+    pairwise_phi.Resize(phi.m);
+    for(int i=0;i<phi.m;i++){
+        pairwise_phi(i).Resize(phi.m);
+        for(int j=i+1;j<phi.m;j++){
+            pairwise_phi(i)(j).Resize(grid.Node_Indices(extrap_width),false);
+            pairwise_phi(i)(j).Fill(FLT_MAX);}}
+
+    for(UNIFORM_GRID_ITERATOR_NODE<TV> it(grid,ghost);it.Valid();it.Next()){
+        PAIRWISE_LEVEL_SET_DATA<T>& data=pairwise_data(it.index);
+        if(data.trust.x>=0)
+            pairwise_phi(data.trust.x)(data.trust.y)(it.index)=data.phi;}
+
+    for(int i=0;i<phi.m;i++)
+        for(int j=i+1;j<phi.m;j++){
+            struct LOCAL_MASK:public EXTRAPOLATION_HIGHER_ORDER_POLY<TV,T>::MASK
+            {
+                const ARRAY<PAIRWISE_LEVEL_SET_DATA<T>,TV_INT>& pairwise_data;
+                VECTOR<short,2> current_pair;
+                LOCAL_MASK(const ARRAY<PAIRWISE_LEVEL_SET_DATA<T>,TV_INT>& pairwise_data,const VECTOR<short,2>& current_pair)
+                    :pairwise_data(pairwise_data),current_pair(current_pair)
+                {}
+                bool Inside(const TV_INT& index) PHYSBAM_OVERRIDE
+                {return pairwise_data(index).trust==current_pair;}
+            } inside_mask(pairwise_data,VECTOR<short,2>(i,j));
+            EXTRAPOLATION_HIGHER_ORDER_POLY<TV,T>::Extrapolate_Node(grid,inside_mask,extrap_width,pairwise_phi(i)(j),3,extrap_width,3*grid.dX.Max());}
+
+    for(int t=0;t<20;t++){
+        HASHTABLE<TRIPLE<int,int,TV_INT>,T> total_size;
+        for(int i=0;i<stencils.m;i++){
+            int mask=~0;
+            for(int j=0;j<stencils(i).m;j++)
+                mask&=pairwise_data(stencils(i)(j)).valid_flags;
+            for(int a=0;a<phi.m;a++)
+                if(mask&(1<<a))
+                    for(int b=a+1;b<phi.m;b++)
+                        if(mask&(1<<b))
+                            for(int c=b+1;c<phi.m;c++)
+                                if(mask&(1<<c)){
+                                    VECTOR<T,TV::m+1> ab(pairwise_phi(a)(b).Subset(stencils(i)));
+                                    VECTOR<T,TV::m+1> bc(pairwise_phi(b)(c).Subset(stencils(i)));
+                                    VECTOR<T,TV::m+1> ca(-pairwise_phi(a)(c).Subset(stencils(i)));
+                                    T A=Bad_Fraction(VECTOR<VECTOR<T,TV::m+1>,3>(ab,bc,ca));
+                                    for(int j=0;j<stencils(i).m;j++){
+                                        total_size.Get_Or_Insert(TRIPLE<int,int,TV_INT>(a,b,stencils(i)(j)))+=A;
+                                        total_size.Get_Or_Insert(TRIPLE<int,int,TV_INT>(b,c,stencils(i)(j)))+=A;
+                                        total_size.Get_Or_Insert(TRIPLE<int,int,TV_INT>(a,c,stencils(i)(j)))-=A;}}}
+        for(typename HASHTABLE<TRIPLE<int,int,TV_INT>,T>::ITERATOR it(total_size);it.Valid();it.Next())
+            pairwise_phi(it.Key().x)(it.Key().y)(it.Key().z)-=(T).25*Weight_Function(it.Data()*grid.dX.Product());}
 }
 
 template<class TV,class TV_INT>
@@ -244,6 +338,17 @@ void Initialize(const GRID<TV>& grid,ARRAY<VECTOR<TV_INT,4> >& stencils,ARRAY<T,
             p[c](it.index)=planes[c].Signed_Distance(it.Location());}
 }
 
+ROTATION<VECTOR<T,2> > rot(ROTATION<VECTOR<T,2> >::From_Angle(-2*(T)pi/3));
+
+template <class TV> typename TV::SCALAR
+Levelset(TV X)
+{
+    typedef typename TV::SCALAR T;
+    T angle=atan2(X.y,X.x);
+    if(angle>=-2*(T)pi/3 && angle<=-(T)pi/2) return X.Magnitude();
+    return max(-X.y,-rot.Rotate(X).y);
+}
+
 template<class TV>
 void Compute(PARSE_ARGS& parse_args)
 {
@@ -254,32 +359,54 @@ void Compute(PARSE_ARGS& parse_args)
     typedef typename TV::SCALAR T;
     typedef VECTOR<int,TV::m> TV_INT;
     Get_Debug_Particles<TV>();
+    Get_Debug_Particles<TV>().debug_particles.template Add_Array<T>(ATTRIBUTE_ID_DISPLAY_SIZE);
 
-    GRID<TV> grid(TV_INT()+resolution,RANGE<TV>::Unit_Box(),true);
+    GRID<TV> grid(TV_INT()+resolution,RANGE<TV>::Centered_Box(),true);
     Global_Grid(&grid);
 
     ARRAY<T,TV_INT> p[3],q[3];
     for(int c=0;c<3;c++){
         p[c].Resize(grid.Node_Indices(3));
         q[c].Resize(grid.Node_Indices(3));}
+    ARRAY_VIEW<T,TV_INT> pv[3]={ARRAY_VIEW<T,TV_INT>(p[0]),ARRAY_VIEW<T,TV_INT>(p[1]),ARRAY_VIEW<T,TV_INT>(p[2])};
 
     ARRAY<VECTOR<TV_INT,TV::m+1> > stencils;
     Initialize(grid,stencils,p,resolution);
 
     for(int c=0;c<3;c++)
-        Dump_Interface(p[c],stencils,colors[c]);
+        Dump_Interface(pv[c],stencils,colors[c]);
     Flush_Frame<T,TV>("Initial");
 
-    for(int i=0;i<500;i++){
-        Evolve_Step(grid,q,p,stencils);
+    ARRAY<ARRAY<T,TV_INT> > color_phi(3);
+    for(int i=0;i<3;i++)
+        color_phi(i).Resize(grid.Node_Indices(3));
 
-        for(int c=0;c<3;c++)
-            p[c].Exchange(q[c]);
+    for(UNIFORM_GRID_ITERATOR_NODE<TV> it(grid,3);it.Valid();it.Next()){
+        TV X=it.Location()+(T).0001;
+        color_phi(0)(it.index)=Levelset(X);
+        color_phi(1)(it.index)=Levelset(rot.Rotate(X));
+        color_phi(2)(it.index)=Levelset(rot.Rotate(rot.Rotate(X)));}
 
-        for(int c=0;c<3;c++)
-            Dump_Interface(p[c],stencils,colors[c]);
-        Flush_Frame<T,TV>("Phew");}
-    Flush_Frame<T,TV>("Phew");
+    for(int i=0;i<3;i++){
+        for(UNIFORM_GRID_ITERATOR_NODE<TV> it(grid,3);it.Valid();it.Next()){
+            T p=color_phi(i)(it.index);
+            Add_Debug_Particle(it.Location(),VECTOR<T,3>(p<0,p>=0,0));
+            Debug_Particle_Set_Attribute<TV>(ATTRIBUTE_ID_DISPLAY_SIZE,abs(p));}
+        Flush_Frame<T,TV>("Initial level set");}
+
+    ARRAY<ARRAY<ARRAY<T,TV_INT> > > pairwise_phi;
+    Compute_Pairwise_Level_Set_Data(grid,color_phi,3,stencils,pairwise_phi);
+
+    // for(int i=0;i<500;i++){
+    //     Evolve_Step(grid,qv,pv,stencils);
+
+    //     for(int c=0;c<3;c++)
+    //         pv[c].Exchange(qv[c]);
+
+    //     for(int c=0;c<3;c++)
+    //         Dump_Interface(pv[c],stencils,colors[c]);
+    //     Flush_Frame<T,TV>("Phew");}
+    // Flush_Frame<T,TV>("Phew");
 
     LOG::Finish_Logging();
 }
@@ -291,7 +418,8 @@ int main(int argc, char* argv[])
     parse_args.Add("-3d",&use_3d,"use 3d");
     parse_args.Parse(true);
 
-    if(use_3d) Compute<VECTOR<T,3> >(parse_args);
-    else Compute<VECTOR<T,2> >(parse_args);
+//    if(use_3d) Compute<VECTOR<T,3> >(parse_args);
+//    else 
+    Compute<VECTOR<T,2> >(parse_args);
 }
 
