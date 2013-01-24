467eaf95f098ff81786ba8f1c11ad5d74517e2c6
Craig Schroeder <snubdodecahedron@gmail.com>
2013-01-24 10:50:35 -0800

    Cleanup.

--- Projects/boundary_conditions_2d/run-test.sh
+++ Projects/boundary_conditions_2d/run-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 F=""
 for r in 8 12 16 24 32 48 64 96 128 192 256 384 512 ; do
     T=`tempfile`
@@ -9,13 +10,14 @@ done
 wait
 T=`tempfile`
 cat $F | grep -v 'iterations\|Simulation' | tr '\n' '@' | sed 's/@ *\([up]\)/ \1/g' | tr @ '\n' | sort -n > $T
-cat $T
+E1=`tempfile --suffix=.eps`
+P1=`tempfile --suffix=.png`
 gnuplot <<EOF
 set terminal postscript color eps
-set output 'plot.eps'
-set size 1,1
-plot '$T' u (log10(\$1)):(log10(\$3)) title 'L-inf p' , '$T' u (log10(\$1)):(log10(\$4)) title 'L-1 p' , '$T' u (log10(\$1)):(log10(\$6)) title 'L-inf u' , '$T' u (log10(\$1)):(log10(\$7)) title 'L-1 p' , -2*x title 'order 2' ls 1 , -x title 'order 1' ls 2
+set output '$E1'
+set logscale xy
+plot '$T' u 1:3 title 'L-inf p' , '$T' u 1:4 title 'L-1 p' , '$T' u 1:6 title 'L-inf u' , '$T' u 1:7 title 'L-1 u' , x**(-2) title 'order 2' ls 1 , x**(-1) title 'order 1' ls 2
 EOF
-inkscape -D -e plot2.png -h 1200 $PWD/plot.eps
-convert plot2.png -flatten plot.png
-rm -f plot2.png plot.eps $F $T
+inkscape -D -e $P1 -h 1200 $E1
+convert $P1 -flatten plot-`echo "$@" | sed 's/ /-/g'`.png
+rm -f $P1 $E1 $F $T
