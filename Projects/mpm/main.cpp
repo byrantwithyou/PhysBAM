5ab8c2b2967099d20eaee57a22f89de43f269e35
fanfu <fanfu@Wahaha.(none)>
2013-02-12 22:42:55 -0800

    visualization done

--- Projects/mpm/main.cpp
+++ Projects/mpm/main.cpp
@@ -10,6 +10,11 @@
 #include <PhysBAM_Tools/Math_Tools/RANGE_ITERATOR.h>
 #include <PhysBAM_Geometry/Basic_Geometry/SPHERE.h>
 #include <PhysBAM_Geometry/Basic_Geometry/TETRAHEDRON.h>
+#include <PhysBAM_Geometry/Geometry_Particles/VIEWER_OUTPUT.h>
+#include <PhysBAM_Geometry/Geometry_Particles/DEBUG_PARTICLES.h>
+#include <PhysBAM_Tools/Read_Write/TYPED_STREAM.h>
+#include <PhysBAM_Tools/Read_Write/READ_WRITE_FORWARD.h>
+#include <PhysBAM_Tools/Utilities/TYPE_UTILITIES.h>
 #include "MPM_PARTICLES.h"
 #include "MPM_CONSTITUTIVE_MODEL.h"
 #include "MPM_CUBIC_B_SPLINE.h"
@@ -19,55 +24,28 @@ using namespace PhysBAM;
 int main(int argc,char *argv[])
 {
     static const int dimension=2;
-    typedef float T;
+    typedef double T;
+    typedef float RW;
     typedef VECTOR<T,dimension> TV;
     typedef VECTOR<int,dimension> TV_INT;
+
+    // MPM_SIMULATION<TV> sim;
+    // sim.Initialize();
 
     TV_INT grid_counts(6,7);
     RANGE<TV> grid_box(TV(0,0),TV(5,6));
     GRID<TV> grid(grid_counts,grid_box);
+    VIEWER_OUTPUT<TV> vo(STREAM_TYPE((RW)0),grid,"output");
 
-    RANGE<TV_INT> range(TV_INT(),TV_INT()+4);
-    for(RANGE_ITERATOR<TV::m> it(range);it.Valid();it.Next()){
-        LOG::cout<<it.index<<std::endl;}
-
-
-    // {
-    //     TV X(2.75,3.5);
-    //     TV_INT influence_corner;
-    //     VECTOR<TV,4> weight;
-    //     VECTOR<TV,4> grad_weight_over_weight;
-    //     MPM_CUBIC_B_SPLINE<TV,3> Bspline;
-    //     Bspline.Build_Weights_And_Grad_Weight_Over_Weights(X,grid,influence_corner,weight,grad_weight_over_weight);
-    //     LOG::cout<<influence_corner<<std::endl;
-    //     LOG::cout<<weight<<std::endl;
-    //     LOG::cout<<grad_weight_over_weight<<std::endl;
-    //     ARRAY<T> weights; weights.Resize(16);
-    //     ARRAY<TV> grad_weights;grad_weights.Resize(16);
-    //     Bspline.Build_Weights_And_Grad_Weights_Exact(X,grid,influence_corner,weights,grad_weights);
-    //     LOG::cout<<weights<<std::endl;
-    //     LOG::cout<<grad_weights<<std::endl;
-
-    // }
-    // {
-    //     TV X(3.5,3);
-    //     TV_INT influence_corner;
-    //     VECTOR<TV,4> weight;
-    //     VECTOR<TV,4> grad_weight_over_weight;
-    //     MPM_CUBIC_B_SPLINE<TV,3> Bspline;
-    //     Bspline.Build_Weights_And_Grad_Weight_Over_Weights(X,grid,influence_corner,weight,grad_weight_over_weight);
-    //     LOG::cout<<influence_corner<<std::endl;
-    //     LOG::cout<<weight<<std::endl;
-    //     LOG::cout<<grad_weight_over_weight<<std::endl;
-    //     ARRAY<T> weights; weights.Resize(16);
-    //     ARRAY<TV> grad_weights;grad_weights.Resize(16);
-    //     Bspline.Build_Weights_And_Grad_Weights_Exact(X,grid,influence_corner,weights,grad_weights);
-    //     for(int i=0;i<16;i++) LOG::cout<<i<<"   "<<weights(i)<<std::endl;
-    //     for(int i=0;i<16;i++) LOG::cout<<i<<"   "<<grad_weights(i)<<std::endl;
-    // }
-
-    // MPM_CONSTITUTIVE_MODEL<TV> test;
-    // test.Derivative_Test();
+    Add_Debug_Particle(TV(0,0), VECTOR<T,3>(0,1,0));
+    Flush_Frame<TV>("mpm");
+    Add_Debug_Particle(TV(1,1), VECTOR<T,3>(0,1,0));
+    Flush_Frame<TV>("mpm");    
+    Add_Debug_Particle(TV(2,2), VECTOR<T,3>(0,1,0));
+    Flush_Frame<TV>("mpm");    
+    Add_Debug_Particle(TV(3,3), VECTOR<T,3>(0,1,0));
+    Add_Debug_Particle(TV(3,4), VECTOR<T,3>(0,1,0));
+    Flush_Frame<TV>("mpm");    
 
     return 0;
 }
