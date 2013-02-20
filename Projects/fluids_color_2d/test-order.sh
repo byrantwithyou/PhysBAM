81497bda57bc99b460d69381fee9cc81f71cbbd8
Craig Schroeder <snubdodecahedron@gmail.com>
2013-02-20 15:06:38 -0800

    This test no longer runs, and that is okay.

--- Projects/fluids_color_2d/test-order.sh
+++ Projects/fluids_color_2d/test-order.sh
@@ -31,7 +31,7 @@ for t in 02 03 04 05 06 10 11 16 17 18 19 24 ; do
         emit_test  new_test_order/conv-$t-$b.png -bc_$b -dt .05 $t
     done
 done
-for t in 00 01 07 08 09 12 13 14 20 21 ; do
+for t in 00 01 08 09 12 13 14 20 21 ; do
     emit_test new_test_order/conv-$t-x.png -dt .05 $t
 done
 
