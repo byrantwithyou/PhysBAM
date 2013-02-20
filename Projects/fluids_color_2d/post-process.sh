50abb4f99d18703a868f8be235065d37a5cfcd2e
Craig Schroeder <snubdodecahedron@gmail.com>
2013-02-20 11:49:12 -0800

    Fix.

--- Projects/fluids_color_2d/post-process.sh
+++ Projects/fluids_color_2d/post-process.sh
@@ -10,7 +10,7 @@ M=`mktemp`
         echo -n `head -n 1 $t`
         grep error $t | tail -n 1
     done
-) | sort -n > M
+) | sort -n > $M
 
 gnuplot -p -e "set terminal png ; set output '$out' ; plot '$M' u (log10(\$1)):(log10(\$3)) title 'L-inf error' , -2*x , -2*x-1 , -x-2;"
 rm "$@" $M
