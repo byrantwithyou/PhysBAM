134232f97c79488b954da0c84adef332cfb79ec9
Craig Schroeder <snubdodecahedron@gmail.com>
2013-04-01 14:29:03 -0700

    Fix derivatives.

--- Public_Library/PhysBAM_Geometry/Analytic_Tests/ANALYTIC_LEVELSET_ELLIPSOID.h
+++ Public_Library/PhysBAM_Geometry/Analytic_Tests/ANALYTIC_LEVELSET_ELLIPSOID.h
@@ -16,8 +16,8 @@ struct ANALYTIC_LEVELSET_ELLIPSOID:public ANALYTIC_IMPLICIT_SURFACE_LEVELSET<TV>
     TV cen;
     TV r;
     MATRIX<T,TV::m> jacobian;
-    ANALYTIC_LEVELSET_ELLIPSOID(TV cc,TV r_in,int c_i,int c_o): ANALYTIC_IMPLICIT_SURFACE_LEVELSET<TV>(c_i,c_o),cen(cc),r(r_in),jacobian() {
-       for(int i=0;i<TV::m;i++) jacobian(i,i)=r.Magnitude_Squared()/r(i)/r(i);
+    ANALYTIC_LEVELSET_ELLIPSOID(TV cc,TV r_in,int c_i,int c_o): ANALYTIC_IMPLICIT_SURFACE_LEVELSET<TV>(c_i,c_o),cen(cc),r(r_in) {
+        for(int i=0;i<TV::m;i++) jacobian(i,i)=2*r.Magnitude_Squared()/r(i)/r(i);
     }
     virtual T f(const TV& X,T t) const {return (((X-cen)*(X-cen)/r/r).Sum()-(T)1)*r.Magnitude_Squared();}
     virtual TV df(const TV& X,T t) const {return (X-cen)/r/r*(T)2*r.Magnitude_Squared();}
