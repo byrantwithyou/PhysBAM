184739d44bfcb8e6d4d483ce0c4fb994a2beff83
Craig Schroeder <snubdodecahedron@gmail.com>
2013-04-18 14:32:24 -0700

    Update convergence computations.

--- Projects/fluids_color_2d/conv.cpp
+++ Projects/fluids_color_2d/conv.cpp
@@ -26,6 +26,37 @@ T Approx_Exact(const ARRAY<int>& n,const ARRAY<T>& x,int order)
     return coeffs(0);
 }
 
+template<class TV>
+struct SIM_DATA
+{
+    typedef typename TV::SCALAR T;typedef VECTOR<int,TV::m> TV_INT;
+    T time;
+    int res;
+    GRID<TV> grid;
+    VECTOR<GRID<TV>,TV::m> face_grids;
+    ARRAY<int,FACE_INDEX<TV::dimension> > face_color;
+    ARRAY<ARRAY<T,FACE_INDEX<TV::dimension> > > face_velocities;
+    ARRAY<T,TV_INT> pressure,phi;
+    LEVELSET<TV> levelset;
+
+    SIM_DATA(STREAM_TYPE& stream_type,const std::string& dir,int frame)
+        :levelset(grid,phi)
+    {
+        ARRAY<int,FACE_INDEX<TV::m> > prev_face_color;
+        ARRAY<ARRAY<T,FACE_INDEX<TV::m> > > prev_face_velocities;
+        FILE_UTILITIES::Read_From_File(stream_type,dir+"/common/grid",grid);
+        FILE_UTILITIES::Read_From_File(stream_type,STRING_UTILITIES::string_sprintf("%s/%d/levelset_%d",dir.c_str(),frame,0),levelset);
+        FILE_UTILITIES::Read_From_File(stream_type,STRING_UTILITIES::string_sprintf("%s/%d/pressure",dir.c_str(),frame),pressure);
+        FILE_UTILITIES::Read_From_File(stream_type,STRING_UTILITIES::string_sprintf("%s/%d/restart_data",dir.c_str(),frame),
+            time,face_color,prev_face_color,face_velocities,prev_face_velocities);
+        res=grid.counts.x;
+        for(int d=0;d<TV::m;d++) face_grids(d)=grid.Get_Face_Grid(d);
+    }
+};
+
+template<class TV>
+bool compare_data(const SIM_DATA<TV>* a,const SIM_DATA<TV>* b) {return a->res<b->res;}
+
 int main(int argc,char *argv[])
 {
     typedef double T;
@@ -46,38 +77,37 @@ int main(int argc,char *argv[])
     parse_args.Extra_Optional(&sim_dirs,"sim dirs","simulation directories");
     parse_args.Parse();
 
-    T time;
-    ARRAY<GRID<TV> > grids(sim_dirs.m);
-    ARRAY<VECTOR<GRID<TV>,TV::m> > face_grids(sim_dirs.m);
-    ARRAY<LEVELSET<TV>*> levelsets(sim_dirs.m);
-    ARRAY<ARRAY<T,TV_INT> > pressures(sim_dirs.m),phis(sim_dirs.m);
-    ARRAY<ARRAY<int,FACE_INDEX<TV::dimension> > > face_color(sim_dirs.m);
-    ARRAY<ARRAY<ARRAY<T,FACE_INDEX<TV::dimension> > > > face_velocities(sim_dirs.m);
-    ARRAY<int> res;
+    ARRAY<SIM_DATA<TV>*> sim_data;
 
-    for(int i=0;i<sim_dirs.m;i++){
-        ARRAY<int,FACE_INDEX<TV::dimension> > prev_face_color;
-        ARRAY<ARRAY<T,FACE_INDEX<TV::dimension> > > prev_face_velocities;
-        FILE_UTILITIES::Read_From_File(stream_type,sim_dirs(i)+"/common/grid",grids(i));
-        levelsets(i)=new LEVELSET<TV>(grids(i),phis(i));
-        FILE_UTILITIES::Read_From_File(stream_type,STRING_UTILITIES::string_sprintf("%s/%d/levelset_%d",sim_dirs(i).c_str(),frame,0),*levelsets(i));
-        FILE_UTILITIES::Read_From_File(stream_type,STRING_UTILITIES::string_sprintf("%s/%d/pressure",sim_dirs(i).c_str(),frame),pressures(i));
-        FILE_UTILITIES::Read_From_File(stream_type,STRING_UTILITIES::string_sprintf("%s/%d/restart_data",sim_dirs(i).c_str(),frame),
-            time,face_color(i),prev_face_color,face_velocities(i),prev_face_velocities);
-        res.Append(grids(i).counts.x);
-        for(int d=0;d<TV::m;d++) face_grids(i)(d)=grids(i).Get_Face_Grid(d);}
+    for(int i=0;i<sim_dirs.m;i++)
+        sim_data.Append(new SIM_DATA<TV>(stream_type,sim_dirs(i),frame));
+
+    sim_data.Sort(compare_data<TV>);
+    ARRAY<int> res;
+    for(int i=0;i<sim_data.m;i++)
+        res.Append(sim_data(i)->res);
+
+    for(int i=0;i<sim_data.m;i++){
+        T p_ave=0;
+        int cnt=0;
+        for(RANGE_ITERATOR<TV::m> it(sim_data(i)->grid.Domain_Indices());it.Valid();it.Next()){
+            p_ave+=sim_data(i)->pressure(it.index);
+            cnt++;}
+        p_ave/=cnt;
+        sim_data(i)->pressure.array-=p_ave;}
 
     ARRAY<int> samples(sim_dirs.m);
     ARRAY<T> u_linf(sim_dirs.m);
     ARRAY<T> u_2(sim_dirs.m);
-    int index_min_res=res.Arg_Min();
     CUBIC_MN_INTERPOLATION_UNIFORM<GRID<TV>,T> interp;
-    for(CELL_ITERATOR<TV> it(grids(index_min_res),-1);it.Valid();it.Next()){
-        int col=phis(index_min_res)(it.index)>0;
+    const GRID<TV>& base_grid(sim_data(0)->grid);
+    const ARRAY<T,TV_INT>& base_phi=sim_data(0)->phi;
+    for(CELL_ITERATOR<TV> it(base_grid,-1);it.Valid();it.Next()){
+        int col=base_phi(it.index)>0;
         for(int d=0;d<TV::m;d++){
             ARRAY<T> values;
             for(int i=0;i<sim_dirs.m;i++){
-                values.Append(interp.Periodic(face_grids(i)(d),face_velocities(i)(col).Component(d),it.Location()));}
+                values.Append(interp.Periodic(sim_data(i)->face_grids(d),sim_data(i)->face_velocities(col).Component(d),it.Location()));}
             T exact=Approx_Exact(res,values,2);
             for(int i=0;i<sim_dirs.m;i++){
                 T v=abs(values(i)-exact);
@@ -98,19 +128,50 @@ int main(int argc,char *argv[])
 
     printf("order %g %g\n", -(Sy*Sx-sim_dirs.m*Sxy)/(-Sxx*sim_dirs.m+sqr(Sx)), -(Sz*Sx-sim_dirs.m*Sxz)/(-Sxx*sim_dirs.m+sqr(Sx)));
 
+    LOG::cout<<"DIRECT ORDER TEST"<<std::endl;
+
+    for(int i=0;i<res.m;i++){
+        int a=res.Find(res(i)*2),b=res.Find(res(i)*4);
+        if(a<0 || b<0) continue;
+        T L2a=0,Linfa=0,L2b=0,Linfb=0;
+        int cnt=0;
+        for(CELL_ITERATOR<TV> it(sim_data(i)->grid,-1);it.Valid();it.Next()){
+            int col=sim_data(i)->phi(it.index)>0;
+            if(col!=(sim_data(a)->phi(it.index)>0)) continue;
+            if(col!=(sim_data(b)->phi(it.index)>0)) continue;
+            for(int d=0;d<TV::m;d++){
+                T e0=interp.Periodic(sim_data(i)->face_grids(d),sim_data(i)->face_velocities(col).Component(d),it.Location());
+                T e1=interp.Periodic(sim_data(a)->face_grids(d),sim_data(a)->face_velocities(col).Component(d),it.Location());
+                T e2=interp.Periodic(sim_data(b)->face_grids(d),sim_data(b)->face_velocities(col).Component(d),it.Location());
+                T f0=abs(e1-e0),f1=abs(e2-e1);
+                cnt++;
+                L2a+=sqr(f0);
+                L2b+=sqr(f1);
+                Linfa=max(Linfa,f0);
+                Linfb=max(Linfb,f1);}}
+        if(cnt)
+        {
+            L2a=sqrt(L2a/cnt);
+            L2b=sqrt(L2b/cnt);
+            printf("ORDER %3d %.3f %.3f\n", res(i), log(Linfa/Linfb)/log(2), log(L2a/L2b)/log(2));
+        }
+    }
+
     if(X0.Max()<FLT_MAX){
+        LOG::cout<<"SAMPLES"<<std::endl;
+
         LOG::cout<<"sample at "<<X0<<std::endl;
         int col=0;
         for(int d=0;d<TV::m;d++){
             ARRAY<T> values;
-            for(int i=0;i<sim_dirs.m;i++) values.Append(interp.Periodic(face_grids(i)(d),face_velocities(i)(col).Component(d),X0));
+            for(int i=0;i<sim_dirs.m;i++) values.Append(interp.Periodic(sim_data(i)->face_grids(d),sim_data(i)->face_velocities(col).Component(d),X0));
             LOG::cout<<"u("<<d<<")  "<<values<<"    "<<Approx_Exact(res,values,2)<<std::endl;}
 
         ARRAY<T> values,values2;
-        for(int i=0;i<sim_dirs.m;i++) values.Append(interp.Periodic(grids(i),levelsets(i)->phi,X0));
+        for(int i=0;i<sim_dirs.m;i++) values.Append(interp.Periodic(sim_data(i)->grid,sim_data(i)->levelset.phi,X0));
         LOG::cout<<"level set  "<<values<<"    "<<Approx_Exact(res,values,2)<<std::endl;
 
-        for(int i=0;i<sim_dirs.m;i++) values2.Append(interp.Periodic(grids(i),pressures(i),X0));
+        for(int i=0;i<sim_dirs.m;i++) values2.Append(interp.Periodic(sim_data(i)->grid,sim_data(i)->pressure,X0));
         LOG::cout<<"pressure  "<<values2<<"    "<<Approx_Exact(res,values2,2)<<std::endl;}
     LOG::Finish_Logging();
     return 0;
