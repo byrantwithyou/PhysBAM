96eb99f277aea53b331c1b4f05ba69e4d1d185aa
Craig Schroeder <snubdodecahedron@gmail.com>
2013-05-06 10:53:13 -0700

    Fix png part of drawing.

--- Projects/fluids_color_2d/draw_bubble.cpp
+++ Projects/fluids_color_2d/draw_bubble.cpp
@@ -2,8 +2,6 @@
 // Copyright 2013.
 // This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
 //#####################################################################
-// TBD
-//#####################################################################
 
 #include <PhysBAM_Tools/Arrays/ARRAY.h>
 #include <PhysBAM_Tools/Arrays/INDIRECT_ARRAY.h>
@@ -12,91 +10,68 @@
 #include <PhysBAM_Tools/Grids_Uniform_Arrays/ARRAYS_ND.h>
 #include <PhysBAM_Tools/Grids_Uniform_Arrays/FACE_ARRAYS.h>
 #include <PhysBAM_Tools/Grids_Uniform_Interpolation/CUBIC_MN_INTERPOLATION_UNIFORM.h>
+#include <PhysBAM_Tools/Images/PNG_FILE.h>
+#include <PhysBAM_Tools/Interpolation/INTERPOLATED_COLOR_MAP.h>
 #include <PhysBAM_Tools/Matrices/MATRIX_MXN.h>
 #include <PhysBAM_Tools/Parsing/PARSE_ARGS.h>
-#include <PhysBAM_Geometry/Level_Sets/LEVELSET.h>
-#include <PhysBAM_Dynamics/Level_Sets/PARTICLE_LEVELSET_MULTIPLE_UNIFORM.h>
-#include <PhysBAM_Geometry/Topology_Based_Geometry/SEGMENTED_CURVE_2D.h>
 #include <PhysBAM_Geometry/Grids_Uniform_Computations/MARCHING_CUBES.h>
-#include <PhysBAM_Tools/Images/PNG_FILE.h>
 #include <PhysBAM_Geometry/Images/EPS_FILE.h>
-
+#include <PhysBAM_Geometry/Level_Sets/LEVELSET.h>
+#include <PhysBAM_Geometry/Topology_Based_Geometry/SEGMENTED_CURVE_2D.h>
+#include <PhysBAM_Dynamics/Level_Sets/PARTICLE_LEVELSET_MULTIPLE_UNIFORM.h>
 
 using namespace PhysBAM;
 
 template<class TV>
 void Draw_Bubble(PARSE_ARGS& parse_args)
 {
-    
     typedef typename TV::SCALAR T;
     typedef VECTOR<int,TV::m> TV_INT;
     STREAM_TYPE stream_type((T()));
-    GRID<TV> grid;
-    ARRAY<T,VECTOR<int,2> > phi;
+    ARRAY<T,TV_INT> phi;
     int frame=1;
-    int expand_factor=1;
-    int number_of_colors=2;
-    std::string sim_dir,filename,filename_image,filename_eps_image;
+    std::string sim_dir,base_filename;
+    TV_INT size(500,500);
 
     parse_args.Add("-frame",&frame,"frame","Frame to draw");
-    parse_args.Add("-colors",&frame,"colors","Number of colors in sim");
-    parse_args.Extra_Optional(&sim_dir,"sim dir","simulation directory");
+    parse_args.Add("-size_x",&size.x,"size","Image size");
+    parse_args.Add("-size_y",&size.y,"size","Image size");
+    parse_args.Extra(&sim_dir,"sim dir","simulation directory");
+    parse_args.Extra(&base_filename,"filename","Base filename for output images");
     parse_args.Parse();
 
-    filename=STRING_UTILITIES::string_sprintf("%s/%d/levelset_0.gz",sim_dir.c_str(),frame);
-    filename_image=STRING_UTILITIES::string_sprintf("russ_image",sim_dir.c_str(),frame);
-    filename_eps_image=STRING_UTILITIES::string_sprintf("russ.eps",sim_dir.c_str(),frame);
-    
+    GRID<TV> grid;
+    FILE_UTILITIES::Read_From_File(stream_type,STRING_UTILITIES::string_sprintf("%s/common/grid",sim_dir.c_str()),grid);
 
-    // eps_writer.Draw_Object(TV(1,2),TV(40,30),TV(22,5));
-
-    ARRAY<LEVELSET<TV>*> levelsets(number_of_colors);
     ARRAY<T,TV_INT> pressure;
-    ARRAY<VECTOR<T,3>, VECTOR<int,2> > pressure_image;
-    
     FILE_UTILITIES::Read_From_File(stream_type,STRING_UTILITIES::string_sprintf("%s/%d/pressure",sim_dir.c_str(),frame),pressure);
 
-    EPS_FILE<T> eps_writer(filename_eps_image,RANGE<TV>(pressure.domain-pressure.domain.min_corner));
-    std::cout<<eps_writer.output_box<<" "<<eps_writer.bounding_box<<std::endl;
+    INTERPOLATED_COLOR_MAP<T> cm;
+    cm.mn=pressure.Min();
+    cm.mx=pressure.Max();
+    cm.colors.Add_Control_Point(cm.mn,VECTOR<T,3>(0,0,0));
+    cm.colors.Add_Control_Point(cm.mx,VECTOR<T,3>(1,1,1));
 
-//         std::cout<<pressure.Min()<<" "<<pressure.Max()<<std::endl;
-         pressure-=pressure.Min();
-         //       std::cout<<pressure.Min()<<" "<<pressure.Max()<<std::endl;
-            pressure/=pressure.Max();
-            //std::cout<<pressure.Min()<<" "<<pressure.Max()<<std::endl;
-            pressure_image.Resize(pressure.domain*expand_factor);
-            pressure_image.Fill(VECTOR<T,3>(1,0,0));
-            //std::cout<<pressure_image.domain<<std::endl;
-             pressure_image*=pressure;
-             // for(int i=-5;i<37;i++)for(int j=-5;j<69;j++)std::cout<<i<<" "<<j<<" "<<pressure_image(i,j)<<std::endl;
+    ARRAY<VECTOR<T,3>,TV_INT> pressure_image(size);
+    GRID<TV> image_grid(size,grid.domain,true);
+    CUBIC_MN_INTERPOLATION_UNIFORM<GRID<TV>,T> interp;
+    for(CELL_ITERATOR<TV> it(image_grid);it.Valid();it.Next())
+        pressure_image(it.index)=cm(interp.Periodic(grid,pressure,it.Location()));
+    PNG_FILE<T>::Write(base_filename+".png",pressure_image);
 
-            PNG_FILE<T>::Write(filename_image,pressure_image);    
-            std::cout<<RANGE<TV>(pressure.domain)<<std::endl;
-    std::cout<<eps_writer.output_box<<" "<<eps_writer.bounding_box<<std::endl;
+    EPS_FILE<T> eps_writer(base_filename+".eps",grid.domain);
+    ARRAY<LEVELSET<TV>*> levelsets;
+    for(int i=0;;i++){
+        LEVELSET<TV>* ls=new LEVELSET<TV>(grid,phi);
+        try{FILE_UTILITIES::Read_From_File<T>(STRING_UTILITIES::string_sprintf("%s/%d/levelset_%d.gz",sim_dir.c_str(),frame,i),*ls);}
+        catch(...){delete ls;break;}
+        levelsets.Append(ls);
 
-    for(int i=0;i<number_of_colors;i++)
-    {
-        levelsets(i)=new LEVELSET<TV>(grid,phi);
-        FILE_UTILITIES::Read_From_File<T>(STRING_UTILITIES::string_sprintf("%s/%d/levelset_%d.gz",sim_dir.c_str(),frame,i),*(levelsets(i)));
         SEGMENTED_CURVE_2D<T>& sc=*SEGMENTED_CURVE_2D<T>::Create();
         MARCHING_CUBES<TV>::Create_Surface(sc,levelsets(i)->grid,levelsets(i)->phi);
-        
-        std::cout<<"This levelset has "<<sc.mesh.elements.m<<" elements"<<std::endl;
-        for (int t=0;t<sc.mesh.elements.m;t++){
-        int node1=sc.mesh.elements(t)(0),node2=sc.mesh.elements(t)(1);
-//        std::cout<<sc.particles.X(node1)<<" "<<sc.particles.X(node2)<<std::endl;
-        eps_writer.Draw_Object(sc.particles.X(node1),sc.particles.X(node2));
-        }
-            
 
-        
-
-        eps_writer.bounding_box=RANGE<TV>(TV(-1,0),TV(1,6));
-
-
-    }
-
-
+        for(int t=0;t<sc.mesh.elements.m;t++)
+            eps_writer.Draw_Object(VECTOR<TV,2>(sc.particles.X.Subset(sc.mesh.elements(t))));}
 }
 
 int main(int argc,char *argv[])
@@ -107,8 +82,8 @@ int main(int argc,char *argv[])
     LOG::cout<<parse_args.Print_Arguments()<<std::endl;
     parse_args.Add("-double",&use_double,"use doubles");
     parse_args.Parse(true);
-    
+
     if(use_double) Draw_Bubble<VECTOR<double,2> >(parse_args);
-    else  Draw_Bubble<VECTOR<float,2> >(parse_args);
+    else Draw_Bubble<VECTOR<float,2> >(parse_args);
 }
 
