996b032a2c6ad987ff21aff5ab41c7383be96caf
Craig Schroeder <snubdodecahedron@gmail.com>
2013-07-26 10:00:41 -0700

    Avoid multiple instantiations, allow defauld initialization value, remove redundant routines.

--- Public_Library/Tools/Arrays/ARRAY.h
+++ Public_Library/Tools/Arrays/ARRAY.h
@@ -43,18 +43,18 @@ public:
         :base_pointer(0),buffer_size(0),m(0)
     {}
 
-    explicit ARRAY(const ID m_input,const bool initialize_using_default_constructor=true)
+    explicit ARRAY(const ID m_input,const bool initialize_new_elements=true,const T& initialization_value=T())
         :base_pointer(0),buffer_size(m_input),m(m_input)
     {
         assert(m>=ID());base_pointer=new T[Value(m)];
-        if(!IS_CLASS<T>::value && initialize_using_default_constructor){ID m=Size();for(ID i(0);i<m;i++) (*this)(i)=T();}
+        if(!IS_CLASS<T>::value && initialize_new_elements){ID m=Size();for(ID i(0);i<m;i++) (*this)(i)=initialization_value;}
     }
 
-    explicit ARRAY(const INITIAL_SIZE m_input,const bool initialize_using_default_constructor=true)
+    explicit ARRAY(const INITIAL_SIZE m_input,const bool initialize_new_elements=true,const T& initialization_value=T())
         :base_pointer(0),buffer_size(Value(m_input)),m(Value(m_input))
     {
         assert(Value(m)>=0);base_pointer=new T[Value(m)];
-        if(!IS_CLASS<T>::value && initialize_using_default_constructor){ID m=Size();for(ID i(0);i<m;i++) (*this)(i)=T();}
+        if(!IS_CLASS<T>::value && initialize_new_elements){ID m=Size();for(ID i(0);i<m;i++) (*this)(i)=initialization_value;}
     }
 
     ARRAY(const ARRAY& array)
@@ -120,17 +120,7 @@ public:
     {if(m<buffer_size) Exact_Resize(m);}
 
 private:
-    void Resize_Helper(const ID buffer_new,const bool initialize_new_elements=true,const bool copy_existing_elements=true)
-    {if(buffer_size==buffer_new) return;assert(m<=buffer_size);
-    T* p=new T[Value(buffer_new)];
-    int m_end=Value(PhysBAM::min(m,buffer_new));
-    if(copy_existing_elements) for(int i=0;i<m_end;i++) p[i]=base_pointer[i];
-    if(!IS_CLASS<T>::value && initialize_new_elements) for(int i=m_end;i<Value(buffer_new);i++) p[i]=T();
-    delete[] base_pointer;
-    base_pointer=p;
-    buffer_size=buffer_new;}
-
-    void Resize_Helper(const ID buffer_new,const bool initialize_new_elements,const bool copy_existing_elements,const T& initialization_value)
+    void Resize_Helper(const ID buffer_new,const bool initialize_new_elements=true,const bool copy_existing_elements=true,const T& initialization_value=T())
     {if(buffer_size==buffer_new) return;assert(m<=buffer_size);
     T* p=new T[Value(buffer_new)];
     int m_end=Value(PhysBAM::min(m,buffer_new));
@@ -147,12 +137,9 @@ public:
     void Preallocate(const ID max_size)
     {if(buffer_size<max_size) Resize_Helper(max_size,false);}
 
-    void Resize(const ID m_new,const bool initialize_new_elements=true,const bool copy_existing_elements=true)
-    {Ensure_Enough_Space(m_new,copy_existing_elements);if(initialize_new_elements && m_new>m) for(int i=Value(m);i<Value(m_new);i++) base_pointer[i]=T();
-    m=m_new;}
-
-    void Resize(const ID m_new,const bool initialize_new_elements,const bool copy_existing_elements,const T& initialization_value)
-    {Ensure_Enough_Space(m_new,copy_existing_elements);if(initialize_new_elements && m_new>m) for(int i=Value(m);i<Value(m_new);i++) base_pointer[i]=initialization_value;
+    void Resize(const ID m_new,const bool initialize_new_elements=true,const bool copy_existing_elements=true,const T& initialization_value=T())
+    {Ensure_Enough_Space(m_new,copy_existing_elements);
+    if(initialize_new_elements && m_new>m) for(int i=Value(m);i<Value(m_new);i++) base_pointer[i]=initialization_value;
     m=m_new;}
 
     void Exact_Resize(const ID m_new,const bool initialize_new_elements=true) // zero elbow room
@@ -256,5 +243,11 @@ public:
     Write_Binary<RW>(output,Value(prefix));Write_Binary_Array<RW>(output,Get_Array_Pointer(),Value(prefix));}
 //#####################################################################
 };
+extern template ARRAY<int>::~ARRAY();
+extern template ARRAY<float>::~ARRAY();
+extern template ARRAY<double>::~ARRAY();
+extern template void ARRAY<int>::Resize_Helper(int, bool, bool, const int&);
+extern template void ARRAY<float>::Resize_Helper(int, bool, bool, const float&);
+extern template void ARRAY<double>::Resize_Helper(int, bool, bool, const double&);
 }
 #endif
