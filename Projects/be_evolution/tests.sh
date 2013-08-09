7b93d8b790945763f70f27c7dc923ce2efd9bcf4
Craig Schroeder <snubdodecahedron@gmail.com>
2013-08-09 15:16:14 -0700

    fixes

--- Projects/be_evolution/tests.sh
+++ Projects/be_evolution/tests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 OUT_LIST=""
-(
-    for r in 6 11 16 21 ; do
-        for t in 1 2 3 ; do
-            O="out-$t-$r.txt"
-            OUT_LIST="$OUT_LIST $O"
-            echo "( time ./be_evolution -resolution $r $t -newton_cd_tol 1e-3 -o out-$t-$r $@ ) >& $O"
-        done
+rm -f tmp-sim-list.txt
+for r in 6 11 16 21 ; do
+    for t in 1 2 3 ; do
+        OUT_LIST="$OUT_LIST out-$t-$r"
+        echo "( time ./be_evolution -resolution $r $t -newton_cd_tol 1e-3 -o out-$t-$r $@ ) >& /dev/null" >> tmp-sim-list.txt
     done
-) | xargs -n 1 -d '\n' -P 12 bash -c
+done
+xargs -n 1 -d '\n' -P 12 bash -c < tmp-sim-list.txt
+rm -f tmp-sim-list.txt
 
 ./make-table.pl $OUT_LIST
