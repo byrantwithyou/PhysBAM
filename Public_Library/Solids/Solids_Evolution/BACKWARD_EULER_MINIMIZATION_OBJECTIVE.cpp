e05f60faad44aec95595bdf41f4d9f5b98e5e19a
Craig Schroeder <snubdodecahedron@gmail.com>
2014-09-08 17:53:43 -0700

    Bug fix for projections.

--- Public_Library/Solids/Solids_Evolution/BACKWARD_EULER_MINIMIZATION_OBJECTIVE.cpp
+++ Public_Library/Solids/Solids_Evolution/BACKWARD_EULER_MINIMIZATION_OBJECTIVE.cpp
@@ -99,9 +99,12 @@ Compute_Unconstrained(const KRYLOV_VECTOR_BASE<T>& Bdv,KRYLOV_SYSTEM_BASE<T>* h,
     if(e) *e=energy;
 
     if(g){
-        tmp1*=0;
+        GENERALIZED_VELOCITY<TV>& gg=debug_cast<GENERALIZED_VELOCITY<TV>&>(*g);
+        tmp1.V.array.Fill(TV());
+        tmp1.rigid_V.array.Fill(TWIST<TV>());
         solid_body_collection.Add_Velocity_Independent_Forces(tmp1.V.array,tmp1.rigid_V.array,time);
-        g->Copy(-dt,tmp1,tmp0);}
+        gg.V.array.Copy(-dt,tmp1.V.array,tmp0.V.array);
+        gg.rigid_V.array.Copy(-dt,tmp1.rigid_V.array,tmp0.rigid_V.array);}
 }
 //#####################################################################
 // Function Update_Position_Based_State
