cd6cc25079d69198423ba2bd5594365f75fd2715
Greg Klar <gklar@ucla.edu>
2015-04-16 13:45:48 -0700

    mpm2partio export for 2d sims

--- Tools/mpm2partio/main.cpp
+++ Tools/mpm2partio/main.cpp
@@ -11,8 +11,8 @@
 
 using namespace PhysBAM;
 
-template<class T>
-void writePartio(const std::string& output_filename,const MPM_PARTICLES<VECTOR<T,3> >& particles,bool dump_valid)
+template<class T,int N>
+void writePartio(const std::string& output_filename,const MPM_PARTICLES<VECTOR<T,N> >& particles,bool dump_valid)
 {
     Partio::ParticlesDataMutable* parts=Partio::create();
 
@@ -37,15 +37,20 @@ void writePartio(const std::string& output_filename,const MPM_PARTICLES<VECTOR<T
         float *vol=parts->dataWrite<float>(volH,idx);
         float *m=parts->dataWrite<float>(mH,idx);
 
-        MATRIX<T,3> F=particles.F(i);
-        for(int k=0;k<3;++k) Fx[k]=F(k,0);
-        for(int k=0;k<3;++k) Fy[k]=F(k,1);
-        for(int k=0;k<3;++k) Fz[k]=F(k,2);
+        VECTOR<T,3> pFx(particles.F(i).Column(0));
+        VECTOR<T,3> pFy(particles.F(i).Column(1));
+        VECTOR<T,3> pFz(N==3?VECTOR<T,3>(particles.F(i).Column(N-1)):VECTOR<T,3>(0,0,1));
+        VECTOR<T,3> pX(particles.X(i));
+        VECTOR<T,3> pV(particles.V(i));
+        for(int k=0;k<3;++k){
+            Fx[k]=pFx(k);
+            Fy[k]=pFy(k);
+            Fz[k]=pFz(k);
+
+            p[k]=pX(k);
+            v[k]=pV(k);}
+
         vol[0]=particles.volume(i);
-
-        for(int k=0;k<3;++k) p[k]=particles.X(i)(k);
-        for(int k=0;k<3;++k) v[k]=particles.V(i)(k);
-
         m[0]=particles.mass(i);
 
         if(dump_valid) *parts->dataWrite<int>(validH,idx)=particles.valid(i);}
@@ -56,35 +61,39 @@ void writePartio(const std::string& output_filename,const MPM_PARTICLES<VECTOR<T
     LOG::printf("Exported %d particles to %s\n",particles.number,output_filename);
 }
 
-template<class T>
+template<class T,int N>
 void Convert(const std::string& input_directory,const std::string& output_filename_pattern,bool dump_valid)
 {
-    MPM_PARTICLES<VECTOR<T,3> > particles;
+    MPM_PARTICLES<VECTOR<T,N> > particles;
     int last_frame;
     FILE_UTILITIES::Read_From_Text_File(input_directory+"/common/last_frame",last_frame);
     for(int i=0;i<=last_frame;++i){
         FILE_UTILITIES::Read_From_File<T>(LOG::sprintf("%s/%d/mpm_particles.gz",input_directory,i),particles);
-        writePartio<T>(LOG::sprintf(output_filename_pattern.c_str(),i),particles,dump_valid);}
+        writePartio<T,N>(LOG::sprintf(output_filename_pattern.c_str(),i),particles,dump_valid);}
 }
 
 int main(int argc,char *argv[])
 {
     bool type_double=true;
     bool dump_valid=false;
+    bool use_3d=false;
     std::string input_directory,output_filename_pattern;
 
     PARSE_ARGS parse_args(argc,argv);
     parse_args.Add_Not("-float",&type_double,"Use floats");
     parse_args.Add("-double",&type_double,"Use doubles");
     parse_args.Add("-dump_valid_flag",&dump_valid,"Dump all particles along with a valid flag; don't prune invalid particles");
+    parse_args.Add("-3d",&use_3d,"Convert 3D examples");
     parse_args.Extra(&input_directory,"sim-dir","simulation output directory");
     parse_args.Extra(&output_filename_pattern,"output-pattern","output partio file name pattern, use %d");
     parse_args.Parse();
 
-    if(type_double){
-        Convert<double>(input_directory,output_filename_pattern,dump_valid);}
-    else{
-        Convert<float>(input_directory,output_filename_pattern,dump_valid);}
+    if(type_double)
+        if(use_3d) Convert<double,3>(input_directory,output_filename_pattern,dump_valid);
+        else Convert<double,2>(input_directory,output_filename_pattern,dump_valid);
+    else
+        if(use_3d) Convert<float,3>(input_directory,output_filename_pattern,dump_valid);
+        else Convert<float,2>(input_directory,output_filename_pattern,dump_valid);
 
     return 0;
 }
