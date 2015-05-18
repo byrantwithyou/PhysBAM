6508a141d6cfd4630be8393063f4993301142937
fanfu <chenfanfu.jiang@gmail.com>
2015-05-18 15:15:58 -0700

    indexing bug fix.

--- Projects/Smoke/SMOKE_EXAMPLE.cpp
+++ Projects/Smoke/SMOKE_EXAMPLE.cpp
@@ -15,7 +15,7 @@ SMOKE_EXAMPLE(const STREAM_TYPE stream_type_input,int number_of_threads)
     restart(0),write_debug_data(false),output_directory("output"),cfl(.9),mac_grid(TV_INT(),RANGE<TV>::Unit_Box(),true),mpi_grid(0),
     thread_queue(number_of_threads>1?new THREAD_QUEUE(number_of_threads):0),projection(mac_grid,false,false,thread_queue),advection_scalar(thread_queue),boundary(0)
 {
-    for(int i=0;i<TV::dimension;i++){domain_boundary(i)(1)=true;domain_boundary(i)(2)=true;}
+    for(int i=0;i<TV::dimension;i++){domain_boundary(i)(0)=true;domain_boundary(i)(1)=true;}
     pthread_mutex_init(&lock,0);    
 }
 //#####################################################################
