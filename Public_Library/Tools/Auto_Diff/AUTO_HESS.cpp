cee06b31dd782711d946f1184d150bf87e2267a7
Craig Schroeder <snubdodecahedron@gmail.com>
2015-06-23 15:13:56 -0700

    Bug fix.

--- Public_Library/Tools/Auto_Diff/AUTO_HESS.cpp
+++ Public_Library/Tools/Auto_Diff/AUTO_HESS.cpp
@@ -12,7 +12,7 @@ Principal_Curvatures(const AUTO_HESS<T,VECTOR<T,3> >& h)
 {
     SYMMETRIC_MATRIX<T,3> P=(T)1-SYMMETRIC_MATRIX<T,3>::Outer_Product(h.dx),M=SYMMETRIC_MATRIX<T,3>::Conjugate(P,h.ddx);
     T trace=M.Trace();
-    QUADRATIC<T> quadratic(-1,trace,sqr(M(1,1))-M(0,1)*M(1,2)+sqr(M(2,1))-M(0,1)*M(2,3)+sqr(M(2,2))-M(1,2)*M(2,3));
+    QUADRATIC<T> quadratic(-1,trace,sqr(M(0,2))+sqr(M(0,1))+sqr(M(1,2))-M(1,1)*M(0,0)-M(2,2)*M(0,0)-M(2,2)*M(1,1));
     quadratic.Compute_Roots();
     if(quadratic.roots == 0) (T).5*VECTOR<T,2>(trace,trace);
     else if(quadratic.roots == 1) return VECTOR<T,2>(quadratic.root1,quadratic.root1);
