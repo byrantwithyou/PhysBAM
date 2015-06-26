484ba232ba8e918ed570bebec5d1573c3242fa4c
Craig Schroeder <snubdodecahedron@gmail.com>
2015-06-26 11:06:54 -0700

    accelerate compilation

--- Projects/fluids_color/FLUIDS_COLOR_BASE.cpp
+++ Projects/fluids_color/FLUIDS_COLOR_BASE.cpp
@@ -2,34 +2,22 @@
 // Copyright 2012.
 // This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
 //#####################################################################
-//#include <Tools/Grids_Uniform/CELL_ITERATOR.h>
+#include <Tools/Auto_Diff/AUTO_HESS_EXT.h>
 #include <Tools/Grids_Uniform/FACE_ITERATOR.h>
 #include <Tools/Grids_Uniform_Interpolation/CUBIC_MN_INTERPOLATION_UNIFORM.h>
-//#include <Tools/Grids_Uniform_Interpolation/QUADRATIC_INTERPOLATION_UNIFORM.h>
 #include <Tools/Log/DEBUG_SUBSTEPS.h>
-//#include <Tools/Matrices/MATRIX.h>
-//#include <Tools/Parsing/PARSE_ARGS.h>
-//#include <Tools/Random_Numbers/RANDOM_NUMBERS.h>
 #include <Geometry/Analytic_Tests/ANALYTIC_LEVELSET_BOX.h>
 #include <Geometry/Analytic_Tests/ANALYTIC_LEVELSET_CONST.h>
 #include <Geometry/Analytic_Tests/ANALYTIC_LEVELSET_ELLIPSOID.h>
 #include <Geometry/Analytic_Tests/ANALYTIC_LEVELSET_LINE.h>
 #include <Geometry/Analytic_Tests/ANALYTIC_LEVELSET_NEST.h>
 #include <Geometry/Analytic_Tests/ANALYTIC_LEVELSET_ROTATE.h>
-//#include <Geometry/Analytic_Tests/ANALYTIC_LEVELSET_SCALE.h>
-//#include <Geometry/Analytic_Tests/ANALYTIC_LEVELSET_SIGNED.h>
 #include <Geometry/Analytic_Tests/ANALYTIC_LEVELSET_SPHERE.h>
 #include <Geometry/Analytic_Tests/ANALYTIC_LEVELSET_TRANSLATE.h>
-//#include <Geometry/Analytic_Tests/ANALYTIC_LEVELSET_VORTEX.h>
-//#include <Geometry/Basic_Geometry/BASIC_GEOMETRY_POLICY.h>
-//#include <Geometry/Basic_Geometry/CYLINDER.h>
 #include <Geometry/Basic_Geometry/LINE_2D.h>
 #include <Geometry/Basic_Geometry/PLANE.h>
-//#include <Geometry/Basic_Geometry/SPHERE.h>
 #include <Geometry/Geometry_Particles/DEBUG_PARTICLES.h>
 #include <Geometry/Geometry_Particles/GEOMETRY_PARTICLES_FORWARD.h>
-//#include <Incompressible/Forces/VORTICITY_CONFINEMENT.h>
-//#include <Dynamics/Fluids_Color_Driver/PLS_FC_EXAMPLE.h>
 #include <Dynamics/Level_Sets/PARTICLE_LEVELSET_EVOLUTION_MULTIPLE_UNIFORM.h>
 #include "ANALYTIC_POLYMER_STRESS.h"
 #include "ANALYTIC_VELOCITY.h"
@@ -901,8 +889,4 @@ Velocity_Jump(const TV& X,int color0,int color1,T time)
     return TV();
 }
 //#####################################################################
-template class FLUIDS_COLOR_BASE<VECTOR<float,2> >;
-template class FLUIDS_COLOR_BASE<VECTOR<float,3> >;
-template class FLUIDS_COLOR_BASE<VECTOR<double,2> >;
-template class FLUIDS_COLOR_BASE<VECTOR<double,3> >;
 }
---
+++ Projects/fluids_color/INSTANTIATION_HELPER_1.cpp
@@ -0,0 +1,4 @@
+#include "FLUIDS_COLOR_BASE.cpp"
+namespace PhysBAM{
+template class FLUIDS_COLOR_BASE<VECTOR<float,3> >;
+}
