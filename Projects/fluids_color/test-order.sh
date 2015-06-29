a4b1b6ccf9779e36822fe1d9d6c5765da2b65034
Craig Schroeder <snubdodecahedron@gmail.com>
2015-06-29 13:26:15 -0700

    More tests.

--- Projects/fluids_color/test-order.sh
+++ Projects/fluids_color/test-order.sh
@@ -21,5 +21,16 @@ for t in 00 01 08 09 12 13 14 20 21  250 251 252 253 254 255 256 257 258 259  ; 
     ./test-order-one.sh -b 2 $max new_test_order/conv-$t.png nice ./fluids_color -resolution 8 -last_frame 1 -s 1.3 -m .8 -kg 1.2 -dt .05 $t
 done
 
+U=("u=1;v=1;" "u=t+1;v=t+1;")
+P=("p=0;")
+S=("s00=1;s10=0;s11=1;" "s00=x*x;s10=x*y;s11=y*y;")
+for u in {0..1} ; do
+    for p in 0 ; do
+        for s in {0..1} ; do
+            ./test-order-one.sh -b 2 $max new_test_order/conv-custom-$u-$p-$s.png nice ./fluids_color -resolution 8 -last_frame 1 -s 1.3 -m .8 -kg 1.2 -dt .05 250 -set_u0 "${U[$u]}" -set_S0 "${S[$s]}" -set_p0 "${P[$p]}"
+        done
+    done
+done
+
 $SLAVE -k
 wait
