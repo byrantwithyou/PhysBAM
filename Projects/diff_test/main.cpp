ce8db661758b7d1b7ff23f586821abb2c7042066
Craig Schroeder <snubdodecahedron@gmail.com>
2015-07-29 10:14:29 -0700

    tweak skeleton

--- Projects/diff_test/main.cpp
+++ Projects/diff_test/main.cpp
@@ -18,22 +18,23 @@ typedef double T;
 typedef VECTOR<T,3> TV;
 typedef VECTOR<int,TV::m> TV_INT;
 RANDOM_NUMBERS<T> rnd;
+static const int N=2;
 
 T eps=1e-6;
 
 struct HELPER
 {
     SYMMETRIC_MATRIX<T,TV::m> A;
-    MATRIX<T,TV::m> db[2],ddb[2];
-    TV u,v,x[2],b,dc[2],ddc[2];
+    MATRIX<T,TV::m> db[N],ddb[N];
+    TV u,v,x[N],b,dc[N],ddc[N];
     T c;
 
-    VEC_ID_SYM_TENSOR<T,2,TV::m> dA[2],ddA[2];
+    VEC_ID_SYM_TENSOR<T,2,TV::m> dA[N],ddA[N];
 };
 
 void Init(HELPER& z)
 {
-    for(int i=0;i<2;i++) rnd.Fill_Uniform(z.x[i],-1,1);
+    for(int i=0;i<N;i++) rnd.Fill_Uniform(z.x[i],-1,1);
 
     rnd.Fill_Uniform(z.u,-1,1);
     rnd.Fill_Uniform(z.v,-1,1);
@@ -41,7 +42,7 @@ void Init(HELPER& z)
 
 void Fill(HELPER& z)
 {
-    for(int i=0;i<2;i++) z.A+=SYMMETRIC_MATRIX<T,TV::m>::Outer_Product(z.x[i]);
+    for(int i=0;i<N;i++) z.A+=SYMMETRIC_MATRIX<T,TV::m>::Outer_Product(z.x[i]);
     z.b=z.A*z.u;
     z.c=z.b.Dot(z.v);
 }
@@ -50,32 +51,32 @@ void Fill_Diff(HELPER& z)
 {
     IDENTITY_MATRIX<T,TV::m> id;
 
-    for(int i=0;i<2;i++){
+    for(int i=0;i<N;i++){
         z.dA[i].v=z.x[i];
         z.db[i]=Contract<1>(z.dA[i],z.u);
         z.dc[i]=z.db[i].Transpose_Times(z.v);
     }
 }
 
-void Fill_Hess(HELPER& z,TV dx[2])
+void Fill_Hess(HELPER& z,TV dx[N])
 {
     IDENTITY_MATRIX<T,TV::m> id;
 
-    for(int i=0;i<2;i++){
+    for(int i=0;i<N;i++){
         z.ddA[i].v=dx[i];
         z.ddb[i]=Contract<1>(z.ddA[i],z.u);
         z.ddc[i]=z.ddb[i].Transpose_Times(z.v);
     }
 }
 
-void Test(const char* name,TV dx[2],T a,T b,TV da[2],TV db[2])
+void Test(const char* name,TV dx[N],T a,T b,TV da[N],TV db[N])
 {
     T x=b-a;
     T y=(db[0]+da[0]).Dot(dx[0])/2+(db[1]+da[1]).Dot(dx[1])/2;
     LOG::printf("DIFF %s: %.16g %.16g -> %.16g\n",name,x/eps,y/eps,(x-y)/max(max(abs(x),abs(y)),1e-30));
 }
 
-void Test(const char* name,TV dx[2],TV a,TV b,MATRIX<T,TV::m> da[2],MATRIX<T,TV::m> db[2])
+void Test(const char* name,TV dx[N],TV a,TV b,MATRIX<T,TV::m> da[N],MATRIX<T,TV::m> db[N])
 {
     TV x=b-a;
     TV y=(db[0]+da[0])*dx[0]/2+(db[1]+da[1])*dx[1]/2;
@@ -83,36 +84,36 @@ void Test(const char* name,TV dx[2],TV a,TV b,MATRIX<T,TV::m> da[2],MATRIX<T,TV:
 }
 
 template<class T_TENSOR>
-void Test(const char* name,TV dx[2],MATRIX<T,TV::m> a,MATRIX<T,TV::m> b,T_TENSOR da[2],T_TENSOR db[2])
+void Test(const char* name,TV dx[N],MATRIX<T,TV::m> a,MATRIX<T,TV::m> b,T_TENSOR da[N],T_TENSOR db[N])
 {
     MATRIX<T,TV::m> x=b-a;
-    MATRIX<T,TV::m> y=Contract<2>(db[0]+da[0],dx[0])/2+Contract<2>(db[1]+da[1],dx[1])/2;
+    MATRIX<T,TV::m> y=Contract<N>(db[0]+da[0],dx[0])/2+Contract<N>(db[1]+da[1],dx[1])/2;
     LOG::printf("DIFF %s: %.16g %.16g -> %.16g\n",name,x.Frobenius_Norm()/eps,y.Frobenius_Norm()/eps,(x-y).Frobenius_Norm()/max(max(x.Frobenius_Norm(),y.Frobenius_Norm()),1e-30));
 }
 
-void Test(const char* name,TV dx[2],TV a[2],TV b[2],MATRIX<T,TV::m> da[2][2],MATRIX<T,TV::m> db[2][2])
+void Test(const char* name,TV dx[N],TV a[N],TV b[N],MATRIX<T,TV::m> da[N][N],MATRIX<T,TV::m> db[N][N])
 {
     VECTOR<TV,2> x,y;
-    for(int i=0;i<2;i++){
+    for(int i=0;i<N;i++){
         x(i)=b[i]-a[i];
         y(i)=(db[i][0]+da[i][0])*dx[0]/2+(db[i][1]+da[i][1])*dx[1]/2;}
     LOG::printf("DIFF %s: %.16g %.16g -> %.16g\n",name,x.Flattened().Magnitude()/eps,y.Flattened().Magnitude()/eps,(x-y).Flattened().Magnitude()/max(max(x.Flattened().Magnitude(),y.Flattened().Magnitude()),1e-30));
 }
 
-void Test(const char* name,TV dx[2],TV a[2],TV b[2],TV da[2],TV db[2])
+void Test(const char* name,TV dx[N],TV a[N],TV b[N],TV da[N],TV db[N])
 {
     VECTOR<TV,2> x,y;
-    for(int i=0;i<2;i++){
+    for(int i=0;i<N;i++){
         x(i)=b[i]-a[i];
         y(i)=(db[i]+da[i])/2;}
     LOG::printf("DIFF %s: %.16g %.16g -> %.16g\n",name,x.Flattened().Magnitude()/eps,y.Flattened().Magnitude()/eps,(x-y).Flattened().Magnitude()/max(max(x.Flattened().Magnitude(),y.Flattened().Magnitude()),1e-30));
 }
 
 template<class T_MATRIX0,class T_MATRIX1> typename enable_if<IS_MATRIX<T_MATRIX0>::value && IS_MATRIX<T_MATRIX1>::value>::type
-Test(const char* name,TV dx[2],T_MATRIX0 a[2],T_MATRIX0 b[2],T_MATRIX1 da[2],T_MATRIX1 db[2])
+Test(const char* name,TV dx[N],T_MATRIX0 a[N],T_MATRIX0 b[N],T_MATRIX1 da[N],T_MATRIX1 db[N])
 {
-    MATRIX<T,TV::m> x[2],y[2],z[2];
-    for(int i=0;i<2;i++){
+    MATRIX<T,TV::m> x[N],y[N],z[N];
+    for(int i=0;i<N;i++){
         x[i]=b[i]-a[i];
         y[i]=(db[i]+da[i])/2;
         z[i]=x[i]-y[i];}
@@ -123,15 +124,15 @@ Test(const char* name,TV dx[2],T_MATRIX0 a[2],T_MATRIX0 b[2],T_MATRIX1 da[2],T_M
 }
 
 template<class T_TENSOR0,class T_TENSOR1> typename enable_if<IS_TENSOR<T_TENSOR0>::value && IS_TENSOR<T_TENSOR1>::value>::type
-Test(const char* name,TV dx[2],T_TENSOR0 a[2],T_TENSOR0 b[2],T_TENSOR1 da[2],T_TENSOR1 db[2])
+Test(const char* name,TV dx[N],T_TENSOR0 a[N],T_TENSOR0 b[N],T_TENSOR1 da[N],T_TENSOR1 db[N])
 {
-    TENSOR<T,TV::m> x[2],y[2],z[2];
-    for(int i=0;i<2;i++){
+    TENSOR<T,TV::m> x[N],y[N],z[N];
+    for(int i=0;i<N;i++){
         x[i]+=b[i]-a[i];
         y[i]+=(db[i]+da[i])/2;
         z[i]=x[i]-y[i];}
     T Mx=0,My=0,Mz=0;
-    for(int i=0;i<2;i++)
+    for(int i=0;i<N;i++)
         for(int j=0;j<TV::m;j++){
             Mx+=x[i].x[j].Frobenius_Norm_Squared();
             My+=y[i].x[j].Frobenius_Norm_Squared();
@@ -152,10 +153,10 @@ int main(int argc, char* argv[])
     HELPER z0;
     Init(z0);
 
-    TV dx[2];
-    for(int i=0;i<2;i++) rnd.Fill_Uniform(dx[i],-eps,eps);
+    TV dx[N];
+    for(int i=0;i<N;i++) rnd.Fill_Uniform(dx[i],-eps,eps);
     HELPER z1=z0;
-    for(int i=0;i<2;i++) z1.x[i]+=dx[i];
+    for(int i=0;i<N;i++) z1.x[i]+=dx[i];
         
     Fill(z0);
     Fill(z1);
