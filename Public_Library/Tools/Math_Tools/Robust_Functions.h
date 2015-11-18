204350b2a8c53220fdbf495ba4dba12042ddc38b
Craig Schroeder <snubdodecahedron@gmail.com>
2015-11-17 19:18:43 -0800

    Add robust routine.

--- Public_Library/Tools/Math_Tools/Robust_Functions.h
+++ Public_Library/Tools/Math_Tools/Robust_Functions.h
@@ -30,5 +30,13 @@ inline T one_minus_cos_x_over_x(const T x) // (1-cos(x))/x
 template<class T>
 inline T atan2_y_x_over_y(const T y,const T x) // atan2(y,x)/y
 {if(abs(y)<1e-8) return 1;return atan2(y,x)/y;}
+
+template<class T>
+inline T log1p_x_over_x(const T x) // log(x+1)/x
+{if(fabs(x)<1e-18) return 1;return log1p(x)/x;}
+
+template<class T>
+inline T diff_log_over_diff(const T x,const T y) // (log(x)-log(y))/(x-y)
+{return log1p_x_over_x((y-x)/x)/x;}
 }
 #endif
