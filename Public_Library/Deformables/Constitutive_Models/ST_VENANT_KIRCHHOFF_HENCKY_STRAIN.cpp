bb6eacb7b613c61a7310999d1b1149f78033dbc4
apradhana <andre.pradhana@gmail.com>
2015-11-22 22:16:10 -0800

    Make Hencky more robust.

--- Public_Library/Deformables/Constitutive_Models/ST_VENANT_KIRCHHOFF_HENCKY_STRAIN.cpp
+++ Public_Library/Deformables/Constitutive_Models/ST_VENANT_KIRCHHOFF_HENCKY_STRAIN.cpp
@@ -2,11 +2,13 @@
 // Copyright 2015, Andre Pradhana, Greg Klar
 // This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
 //#####################################################################
+#include <Tools/Math_Tools/Robust_Functions.h>
 #include <Tools/Matrices/DIAGONAL_MATRIX.h>
 #include <Tools/Matrices/MATRIX.h>
 #include <Tools/Matrices/SYMMETRIC_MATRIX.h>
 #include <Deformables/Constitutive_Models/DIAGONALIZED_ISOTROPIC_STRESS_DERIVATIVE.h>
 #include <Deformables/Constitutive_Models/ST_VENANT_KIRCHHOFF_HENCKY_STRAIN.h>
+#include <cfloat>
 using namespace PhysBAM;
 using ::std::log;
 //#####################################################################
@@ -46,10 +48,13 @@ Zero_Out_Mu()
 template<class T,int d> T ST_VENANT_KIRCHHOFF_HENCKY_STRAIN<T,d>::
 Energy_Density(const DIAGONAL_MATRIX<T,d>& F,const int id) const
 {
-    T id_mu=(mu.m?mu(id):constant_mu),id_lambda=(lambda.m?lambda(id):constant_lambda);
-    DIAGONAL_MATRIX<T,d> log_F=log(F.Abs());
-    T sum_sqr=sqr(log_F.Trace());
-    return id_mu*log_F.Frobenius_Norm_Squared()+(T)0.5*id_lambda*sum_sqr;
+    if(F.x(d-1)<0)
+        return FLT_MAX;
+    else{
+        T id_mu=(mu.m?mu(id):constant_mu),id_lambda=(lambda.m?lambda(id):constant_lambda);
+        DIAGONAL_MATRIX<T,d> log_F=log(F);
+        T sum_sqr=sqr(log_F.Trace());
+        return id_mu*log_F.Frobenius_Norm_Squared()+(T)0.5*id_lambda*sum_sqr;}
 }
 //#####################################################################
 // Function P_From_Strain
@@ -57,10 +62,13 @@ Energy_Density(const DIAGONAL_MATRIX<T,d>& F,const int id) const
 template<class T,int d> DIAGONAL_MATRIX<T,d> ST_VENANT_KIRCHHOFF_HENCKY_STRAIN<T,d>::
 P_From_Strain(const DIAGONAL_MATRIX<T,d>& F,const int id) const
 {
-    T id_mu=(mu.m?mu(id):constant_mu),id_lambda=(lambda.m?lambda(id):constant_lambda);
-    DIAGONAL_MATRIX<T,d> log_E=log(F.Abs());
-    DIAGONAL_MATRIX<T,d> F_Inv=F.Inverse();
-    return ((T)2*id_mu*log_E+id_lambda*log_E.Trace())*F_Inv;
+    if(F.x(d-1)<0)
+        return DIAGONAL_MATRIX<T,d>();
+    else{
+        T id_mu=(mu.m?mu(id):constant_mu),id_lambda=(lambda.m?lambda(id):constant_lambda);
+        DIAGONAL_MATRIX<T,d> log_E=log(F);
+        DIAGONAL_MATRIX<T,d> F_Inv=F.Inverse();
+        return ((T)2*id_mu*log_E+id_lambda*log_E.Trace())*F_Inv;}
 }
 //#####################################################################
 // Function P_From_Strain_Rate
@@ -68,7 +76,6 @@ P_From_Strain(const DIAGONAL_MATRIX<T,d>& F,const int id) const
 template<class T,int d> MATRIX<T,d> ST_VENANT_KIRCHHOFF_HENCKY_STRAIN<T,d>::
 P_From_Strain_Rate(const DIAGONAL_MATRIX<T,d>& F,const MATRIX<T,d>& F_dot,const int id) const
 {
-    PHYSBAM_NOT_IMPLEMENTED();
     return DIAGONAL_MATRIX<T,d>();
 }
 //#####################################################################
@@ -77,16 +84,20 @@ P_From_Strain_Rate(const DIAGONAL_MATRIX<T,d>& F,const MATRIX<T,d>& F_dot,const 
 template<class T> static void
 Isotropic_Stress_Derivative_Helper(const DIAGONAL_MATRIX<T,2>& F,DIAGONALIZED_ISOTROPIC_STRESS_DERIVATIVE<T,2>& dPi_dF,T failure_threshold,T mu,T lambda)
 {
-    DIAGONAL_MATRIX<T,2> F_threshold=F.Clamp_Min(failure_threshold);
-    T lambda_plus_two_mu=lambda+2*mu,two_mu=2*mu,two_lambda_plus_two_mu=2*(lambda+mu),s0=F_threshold.x.x,s1=F_threshold.x.y;
-    T l0=log(s0),l1=log(s1);
-    T s0_sqr=sqr(s0),s1_sqr=sqr(s1);
-    T eps=s1-s0;
-    dPi_dF.x0000=(lambda_plus_two_mu*(1-l0)-lambda*l1)/s0_sqr;
-    dPi_dF.x1111=(lambda_plus_two_mu*(1-l1)-lambda*l0)/s1_sqr;
-    dPi_dF.x1100=lambda/(s0*s1);
-    dPi_dF.x1010=(abs(eps)<1e-8)?mu/s0_sqr:two_mu*(l0-l1)/(s0_sqr-s1_sqr);
-    dPi_dF.x1001=(abs(eps)<1e-8)?(mu-two_lambda_plus_two_mu*l0)/s0_sqr:((-lambda*s0_sqr+lambda_plus_two_mu*s1_sqr)*l0+(lambda*s1_sqr-lambda_plus_two_mu*s0_sqr)*l1)/(s0*s1*(s0_sqr-s1_sqr));
+    if(F.x.y<0)
+        dPi_dF.x0000=dPi_dF.x1111=dPi_dF.x1100=dPi_dF.x1010=dPi_dF.x1001=(T)0;
+    else{
+        DIAGONAL_MATRIX<T,2> F_threshold=F.Clamp_Min(failure_threshold);
+        T lambda_plus_two_mu=lambda+2*mu,two_mu=2*mu,s0=F_threshold.x.x,s1=F_threshold.x.y;
+        T l0=log(s0),l1=log(s1),sum_log=l0+l1;
+        SYMMETRIC_MATRIX<T,2> F_outer=SYMMETRIC_MATRIX<T,2>::Outer_Product(VECTOR<T,2>(F_threshold.x.x,F_threshold.x.y));
+        T Psi_0_minus_Psi_1=(two_mu/s0)*(diff_log_over_diff(s0,s1)-l1/s1)-lambda*sum_log/F_outer.x10;
+        T Psi_0_plus_Psi_1=((lambda*s0+lambda_plus_two_mu*s1)*l0+(lambda*s1+lambda_plus_two_mu*s0)*l1)/(F_outer.x10*(s0+s1));
+        dPi_dF.x0000=(lambda_plus_two_mu*(1-l0)-lambda*l1)/F_outer.x00;
+        dPi_dF.x1111=(lambda_plus_two_mu*(1-l1)-lambda*l0)/F_outer.x11;
+        dPi_dF.x1100=lambda/F_outer.x10;
+        dPi_dF.x1010=(T)0.5*(Psi_0_minus_Psi_1+Psi_0_plus_Psi_1);
+        dPi_dF.x1001=(T)0.5*(Psi_0_minus_Psi_1-Psi_0_plus_Psi_1);}
 }
 //#####################################################################
 // Function Isotropic_Stress_Derivative_Helper
@@ -94,26 +105,34 @@ Isotropic_Stress_Derivative_Helper(const DIAGONAL_MATRIX<T,2>& F,DIAGONALIZED_IS
 template<class T> static void
 Isotropic_Stress_Derivative_Helper(const DIAGONAL_MATRIX<T,3>& F,DIAGONALIZED_ISOTROPIC_STRESS_DERIVATIVE<T,3>& dPi_dF,T failure_threshold,T mu,T lambda)
 {
-    DIAGONAL_MATRIX<T,3> F_threshold=F.Clamp_Min(failure_threshold);
-    T lambda_plus_two_mu=lambda+2*mu,two_mu=2*mu,two_lambda_plus_two_mu=2*(lambda+mu),s0=F_threshold.x.x,s1=F_threshold.x.y,s2=F_threshold.x.y;
-    T s0_sqr=sqr(s0),s1_sqr=sqr(s1),s2_sqr=sqr(s2);
-    T eps_10=s1-s0,eps_20=s2-s0,eps_21=s2-s1;
-    T l0=log(s0),l1=log(s1),l2=log(s2),lt=log(s0*s1*s2);
-    //Hessian variables
-    dPi_dF.x0000=(lambda_plus_two_mu-two_mu*l0-lambda*lt)/s0_sqr;
-    dPi_dF.x1111=(lambda_plus_two_mu-two_mu*l1-lambda*lt)/s1_sqr;
-    dPi_dF.x2222=(lambda_plus_two_mu-two_mu*l2-lambda*lt)/s2_sqr;
-    dPi_dF.x1100=lambda/(s0*s1);
-    dPi_dF.x2200=lambda/(s0*s2);
-    dPi_dF.x2211=lambda/(s1*s2);
-    //ijij
-    dPi_dF.x1010=abs(eps_10<1e-8)?mu/s0_sqr:two_mu*(l0-l1)/(s0_sqr-s1_sqr);
-    dPi_dF.x2020=abs(eps_20<1e-8)?mu/s0_sqr:two_mu*(l0-l2)/(s0_sqr-s2_sqr);
-    dPi_dF.x2121=abs(eps_21<1e-8)?mu/s1_sqr:two_mu*(l1-l2)/(s1_sqr-s2_sqr);
-    //ijji
-    dPi_dF.x1001=abs(eps_10<1e-8)?(mu-two_lambda_plus_two_mu*l0-lambda*l2)/s0_sqr:((-s0_sqr*lambda+s1_sqr*lambda_plus_two_mu)*l0+(s1_sqr*lambda-s0_sqr*lambda_plus_two_mu)*l1+(-s0_sqr+s1_sqr)*lambda*l2)/(s0*s1*(s0_sqr-s1_sqr));
-    dPi_dF.x2002=abs(eps_20<1e-8)?(mu-two_lambda_plus_two_mu*l0-lambda*l1)/s0_sqr:((-s0_sqr*lambda+s2_sqr*lambda_plus_two_mu)*l0+(-s0_sqr+s2_sqr)*lambda*l1+(s2_sqr*lambda-s0_sqr*lambda_plus_two_mu)*l2)/(s0*s2*(s0_sqr-s2_sqr));
-    dPi_dF.x2112=abs(eps_21<1e-8)?(mu-two_lambda_plus_two_mu*l1-lambda*l0)/s1_sqr:((-s1_sqr+s2_sqr)*lambda*l0+(-s1_sqr*lambda+s2_sqr*lambda_plus_two_mu)*l1+(s2_sqr*lambda-s1_sqr*lambda_plus_two_mu)*l2)/(s1*s2*(s1_sqr-s2_sqr));
+    if(F.x.z<0){
+        dPi_dF.x0000=dPi_dF.x1111=dPi_dF.x2222=dPi_dF.x1100=dPi_dF.x2200=dPi_dF.x2211=dPi_dF.x1010=dPi_dF.x2020=dPi_dF.x2121=dPi_dF.x1001=dPi_dF.x2002=dPi_dF.x2112=(T)0;}
+    else{
+        DIAGONAL_MATRIX<T,3> F_threshold=F.Clamp_Min(failure_threshold);
+        T lambda_plus_two_mu=lambda+2*mu,two_mu=2*mu,s0=F_threshold.x.x,s1=F_threshold.x.y,s2=F_threshold.x.z;
+        T l0=log(abs(s0)),l1=log(abs(s1)),l2=log(abs(s2)),lt=log(abs(s0*s1*s2)),sum_log=l0+l1+l2;
+        SYMMETRIC_MATRIX<T,3> F_outer=SYMMETRIC_MATRIX<T,3>::Outer_Product(VECTOR<T,3>(s0,s1,s2));
+        MATRIX<T,3> sl_outer=MATRIX<T,3>::Outer_Product(VECTOR<T,3>(s0,s1,s2),VECTOR<T,3>(l0,l1,l2));
+        //Hessian variables
+        dPi_dF.x0000=(lambda_plus_two_mu-two_mu*l0-lambda*lt)/F_outer.x00;
+        dPi_dF.x1111=(lambda_plus_two_mu-two_mu*l1-lambda*lt)/F_outer.x11;
+        dPi_dF.x2222=(lambda_plus_two_mu-two_mu*l2-lambda*lt)/F_outer.x22;
+        dPi_dF.x1100=lambda/F_outer.x10;
+        dPi_dF.x2200=lambda/F_outer.x20;
+        dPi_dF.x2211=lambda/F_outer.x21;
+        //2x2 block-matrices
+        T Psi_0_minus_Psi_1=(two_mu/s0)*(diff_log_over_diff(s0,s1)-l1/s1)-lambda*sum_log/F_outer.x10;
+        T Psi_0_minus_Psi_2=(two_mu/s0)*(diff_log_over_diff(s0,s2)-l2/s2)-lambda*sum_log/F_outer.x20;
+        T Psi_1_minus_Psi_2=(two_mu/s1)*(diff_log_over_diff(s1,s2)-l2/s2)-lambda*sum_log/F_outer.x21;
+        T Psi_0_plus_Psi_1=(two_mu*(s1*l0+s0*l1)+lambda*(sl_outer.Row(0).Sum()+sl_outer.Row(1).Sum()))/(F_outer.x10*(s0+s1));
+        T Psi_0_plus_Psi_2=(two_mu*(s2*l0+s0*l2)+lambda*(sl_outer.Row(0).Sum()+sl_outer.Row(2).Sum()))/(F_outer.x20*(s0+s2));
+        T Psi_1_plus_Psi_2=(two_mu*(s2*l1+s1*l2)+lambda*(sl_outer.Row(1).Sum()+sl_outer.Row(2).Sum()))/(F_outer.x21*(s1+s2));
+        dPi_dF.x1010=(T)0.5*(Psi_0_minus_Psi_1+Psi_0_plus_Psi_1);
+        dPi_dF.x1001=(T)0.5*(Psi_0_minus_Psi_1-Psi_0_plus_Psi_1); 
+        dPi_dF.x2020=(T)0.5*(Psi_0_minus_Psi_2+Psi_0_plus_Psi_2);
+        dPi_dF.x2002=(T)0.5*(Psi_0_minus_Psi_2-Psi_0_plus_Psi_2); 
+        dPi_dF.x2121=(T)0.5*(Psi_1_minus_Psi_2+Psi_1_plus_Psi_2);
+        dPi_dF.x2112=(T)0.5*(Psi_1_minus_Psi_2-Psi_1_plus_Psi_2);}
 }
 //#####################################################################
 // Function Isotropic_Stress_Derivative
