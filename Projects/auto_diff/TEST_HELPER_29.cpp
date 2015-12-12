77143ffa72908fc36ebc0a797c76dd5a34e8ddcb
Craig Schroeder <snubdodecahedron@gmail.com>
2015-12-11 17:34:20 -0800

    Add some tests.

--- Projects/auto_diff/TEST_HELPER_29.cpp
+++ Projects/auto_diff/TEST_HELPER_29.cpp
@@ -1,6 +1,15 @@
 #include "helper.h"
 
+TV o=TV::All_Ones_Vector();
 void test_29()
 {
     TEST(sin(v));
+    TEST(cos(v));
+    TEST(exp(v.Dot(o)*o));
+    TEST(cos(v.Dot(o)*o));
+    TEST(sin(v.Dot(o)*o));
+    TEST(sin(v+o));
+    TEST(exp(v.Dot(o)*o));
+    TEST(exp(o*a));
+    TEST(exp(v+v));
 }
