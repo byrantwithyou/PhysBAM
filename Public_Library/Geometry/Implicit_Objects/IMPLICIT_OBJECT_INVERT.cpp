05228852e82b0c2821b33d04d06619c8dd8ded88
Craig Schroeder <snubdodecahedron@gmail.com>
2016-01-05 15:49:23 -0800

    Level set inverse.

---
+++ Public_Library/Geometry/Implicit_Objects/IMPLICIT_OBJECT_INVERT.cpp
@@ -0,0 +1,303 @@
+//#####################################################################
+// Copyright 2002-2006, Ronald Fedkiw, Sergey Koltakov, Eran Guendelman, Geoffrey Irving, Neil Molino, Andrew Selle, Rachel Weinstein.
+// This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
+//#####################################################################
+#include <Tools/Math_Tools/RANGE.h>
+#include <Tools/Matrices/MATRIX_3X3.h>
+#include <Tools/Matrices/SYMMETRIC_MATRIX.h>
+#include <Tools/Nonlinear_Equations/ITERATIVE_SOLVER.h>
+#include <Tools/Random_Numbers/RANDOM_NUMBERS.h>
+#include <Geometry/Basic_Geometry/RAY.h>
+#include <Geometry/Implicit_Objects/IMPLICIT_OBJECT.h>
+#include <Geometry/Implicit_Objects/IMPLICIT_OBJECT_INVERT.h>
+#include <Geometry/Intersections/RAY_BOX_INTERSECTION.h>
+#include <Geometry/Level_Sets/IMPLICIT_OBJECT_ON_A_RAY.h>
+#include <Geometry/Level_Sets/IMPLICIT_OBJECT_ON_A_RAY_SECONDARY_INTERPOLATION.h>
+#include <Geometry/Level_Sets/LEVELSET_UTILITIES.h>
+namespace PhysBAM{
+//#####################################################################
+// Destructor
+//#####################################################################
+template<class TV> IMPLICIT_OBJECT_INVERT<TV>::
+~IMPLICIT_OBJECT_INVERT()
+{
+    if(owns_io) delete &io;
+}
+//#####################################################################
+// Function Update_Box
+//#####################################################################
+template<class TV> void IMPLICIT_OBJECT_INVERT<TV>::
+Update_Box()
+{
+    box=RANGE<TV>::Full_Box();
+}
+//#####################################################################
+// Function Update_Minimum_Cell_Size
+//#####################################################################
+template<class TV> void IMPLICIT_OBJECT_INVERT<TV>::
+Update_Minimum_Cell_Size(const int maximum_depth)
+{
+    io.Update_Minimum_Cell_Size(maximum_depth);
+}
+//#####################################################################
+// Function Minimum_Cell_Size_Within_Box
+//#####################################################################
+template<class TV> typename TV::SCALAR IMPLICIT_OBJECT_INVERT<TV>::
+Minimum_Cell_Size_Within_Box(const RANGE<TV>& box) const
+{
+    return io.Minimum_Cell_Size_Within_Box(box);
+}
+//#####################################################################
+// Function operator
+//#####################################################################
+template<class TV> typename TV::SCALAR IMPLICIT_OBJECT_INVERT<TV>::
+operator()(const TV& X) const
+{
+    return -io(X);
+}
+//#####################################################################
+// Function Extended_Phi
+//#####################################################################
+template<class TV> typename TV::SCALAR IMPLICIT_OBJECT_INVERT<TV>::
+Extended_Phi(const TV& X) const
+{
+    return -io.Extended_Phi(X);
+}
+//#####################################################################
+// Function Phi_Secondary
+//#####################################################################
+template<class TV> typename TV::SCALAR IMPLICIT_OBJECT_INVERT<TV>::
+Phi_Secondary(const TV& X) const
+{
+    return -io.Phi_Secondary(X);
+}
+//#####################################################################
+// Function Normal
+//#####################################################################
+template<class TV> TV IMPLICIT_OBJECT_INVERT<TV>::
+Normal(const TV& X,const int aggregate) const
+{
+    return -io.Normal(X,aggregate);
+}
+//#####################################################################
+// Function Extended_Normal
+//#####################################################################
+template<class TV> TV IMPLICIT_OBJECT_INVERT<TV>::
+Extended_Normal(const TV& X,const int aggregate) const
+{
+    return -Extended_Normal(X,aggregate);
+}
+//#####################################################################
+// Function Compute_Normals
+//#####################################################################
+template<class TV> void IMPLICIT_OBJECT_INVERT<TV>::
+Compute_Normals()
+{
+    io.Compute_Normals();
+}
+//#####################################################################
+// Function Compute_Cell_Minimum_And_Maximum
+//#####################################################################
+template<class TV> void IMPLICIT_OBJECT_INVERT<TV>::
+Compute_Cell_Minimum_And_Maximum(const bool recompute_if_exists)
+{
+    io.Compute_Cell_Minimum_And_Maximum(recompute_if_exists);
+}
+//#####################################################################
+// Function Rescale
+//#####################################################################
+template<class TV> void IMPLICIT_OBJECT_INVERT<TV>::
+Rescale(const T scaling_factor)
+{
+    io.Rescale(scaling_factor);
+}
+//#####################################################################
+// Function Translate
+//#####################################################################
+template<class TV> void IMPLICIT_OBJECT_INVERT<TV>::
+Translate(const TV& translation)
+{
+    io.Translate(translation);
+}
+//#####################################################################
+// Function Inflate
+//#####################################################################
+template<class TV> void IMPLICIT_OBJECT_INVERT<TV>::
+Inflate(const T inflation_distance)
+{
+    io.Inflate(inflation_distance);
+}
+//#####################################################################
+// Function Inside
+//#####################################################################
+template<class TV> bool IMPLICIT_OBJECT_INVERT<TV>::
+Inside(const TV& X,const T thickness_over_two) const
+{
+    return io.Outside(X,thickness_over_two);
+}
+//#####################################################################
+// Function Outside
+//#####################################################################
+template<class TV> bool IMPLICIT_OBJECT_INVERT<TV>::
+Outside(const TV& X,const T thickness_over_two) const
+{
+    return io.Inside(X,thickness_over_two);
+}
+//#####################################################################
+// Function Boundary
+//#####################################################################
+template<class TV> bool IMPLICIT_OBJECT_INVERT<TV>::
+Boundary(const TV& X,const T thickness_over_two) const
+{
+    return io.Boundary(X,thickness_over_two);
+}
+//#####################################################################
+// Function Lazy_Inside
+//#####################################################################
+template<class TV> bool IMPLICIT_OBJECT_INVERT<TV>::
+Lazy_Inside(const TV& X,const T contour_value) const
+{
+    return io.Lazy_Outside(X,-contour_value);
+}
+//#####################################################################
+// Function Lazy_Inside_And_Value
+//#####################################################################
+template<class TV> bool IMPLICIT_OBJECT_INVERT<TV>::
+Lazy_Inside_And_Value(const TV& X,T& phi_value,const T contour_value) const
+{
+    bool b=io.Lazy_Inside_And_Value(X,phi_value,-contour_value);
+    phi_value=-phi_value;
+    return !b;
+}
+//#####################################################################
+// Function Lazy_Inside_Extended_Levelset
+//#####################################################################
+template<class TV> bool IMPLICIT_OBJECT_INVERT<TV>::
+Lazy_Inside_Extended_Levelset(const TV& X,const T contour_value) const
+{
+    return io.Lazy_Outside_Extended_Levelset(X,-contour_value);
+}
+//#####################################################################
+// Function Lazy_Inside_Extended_Levelset_And_Value
+//#####################################################################
+template<class TV> bool IMPLICIT_OBJECT_INVERT<TV>::
+Lazy_Inside_Extended_Levelset_And_Value(const TV& X,T& phi_value,const T contour_value) const
+{
+    bool b=io.Lazy_Outside_Extended_Levelset_And_Value(X,phi_value,-contour_value);
+    phi_value=-phi_value;
+    return b;
+}
+//#####################################################################
+// Function Lazy_Outside
+//#####################################################################
+template<class TV> bool IMPLICIT_OBJECT_INVERT<TV>::
+Lazy_Outside(const TV& X,const T contour_value) const
+{
+    return io.Lazy_Inside(X,-contour_value);
+}
+//#####################################################################
+// Function Lazy_Outside_Extended_Levelset
+//#####################################################################
+template<class TV> bool IMPLICIT_OBJECT_INVERT<TV>::
+Lazy_Outside_Extended_Levelset(const TV& X,const T contour_value) const
+{
+    return io.Lazy_Inside_Extended_Levelset(X,-contour_value);
+}
+//#####################################################################
+// Function Lazy_Outside_Extended_Levelset_And_Value
+//#####################################################################
+template<class TV> bool IMPLICIT_OBJECT_INVERT<TV>::
+Lazy_Outside_Extended_Levelset_And_Value(const TV& X,T& phi_value,const T contour_value) const
+{
+    bool b=io.Lazy_Inside_Extended_Levelset_And_Value(X,phi_value,-contour_value);
+    phi_value=-phi_value;
+    return b;
+}
+//#####################################################################
+// Function Min_Phi
+//#####################################################################
+template<class TV> typename TV::SCALAR IMPLICIT_OBJECT_INVERT<TV>::
+Min_Phi() const
+{
+    return io.Min_Phi();
+}
+//#####################################################################
+// Function Intersection
+//#####################################################################
+template<class TV> bool IMPLICIT_OBJECT_INVERT<TV>::
+Intersection(RAY<TV>& ray,const T thickness) const
+{
+    PHYSBAM_NOT_IMPLEMENTED();
+}
+//#####################################################################
+// Function Closest_Point_On_Boundary
+//#####################################################################
+template<class TV> TV IMPLICIT_OBJECT_INVERT<TV>::
+Closest_Point_On_Boundary(const TV& X,const T tolerance,const int max_iterations,T* distance) const
+{
+    return io.Closest_Point_On_Boundary(X,tolerance,max_iterations,distance);
+}
+//#####################################################################
+// Function Velocity
+//#####################################################################
+template<class TV> TV IMPLICIT_OBJECT_INVERT<TV>::
+Velocity(const TV& X) const
+{
+    return io.Velocity(X);
+}
+//#####################################################################
+// Function Velocity
+//#####################################################################
+template<class TV> SYMMETRIC_MATRIX<typename TV::SCALAR,TV::m> IMPLICIT_OBJECT_INVERT<TV>::
+Hessian(const TV& X) const
+{
+    return -io.Hessian(X);
+}
+//#####################################################################
+// Function Velocity
+//#####################################################################
+template<class TV> auto IMPLICIT_OBJECT_INVERT<TV>::
+Principal_Curvatures(const TV& X) const -> T_CURVATURES
+{
+    return -io.Principal_Curvatures(X);
+}
+//#####################################################################
+// Function Integration_Step
+//#####################################################################
+template<class TV> typename TV::SCALAR IMPLICIT_OBJECT_INVERT<TV>::
+Integration_Step(const T phi) const
+{
+    return io.Integration_Step(phi);
+}
+//#####################################################################
+// Function Minimum_Cell_Size
+//#####################################################################
+template<class TV> typename TV::SCALAR IMPLICIT_OBJECT_INVERT<TV>::
+Minimum_Cell_Size() const
+{
+    return io.Minimum_Cell_Size();
+}
+//#####################################################################
+// Function Read
+//#####################################################################
+template<class TV> void IMPLICIT_OBJECT_INVERT<TV>::
+Read(TYPED_ISTREAM& input)
+{
+    io.Read(input);
+}
+//#####################################################################
+// Function Write
+//#####################################################################
+template<class TV> void IMPLICIT_OBJECT_INVERT<TV>::
+Write(TYPED_OSTREAM& output) const
+{
+    io.Write(output);
+}
+template class IMPLICIT_OBJECT_INVERT<VECTOR<float,1> >;
+template class IMPLICIT_OBJECT_INVERT<VECTOR<float,2> >;
+template class IMPLICIT_OBJECT_INVERT<VECTOR<float,3> >;
+template class IMPLICIT_OBJECT_INVERT<VECTOR<double,1> >;
+template class IMPLICIT_OBJECT_INVERT<VECTOR<double,2> >;
+template class IMPLICIT_OBJECT_INVERT<VECTOR<double,3> >;
+
+}
---
+++ Public_Library/Geometry/Implicit_Objects/IMPLICIT_OBJECT_INVERT.h
@@ -0,0 +1,80 @@
+//#####################################################################
+// Copyright 2002-2008, Ronald Fedkiw, Eran Guendelman, Geoffrey Irving, Sergey Koltakov, Frank Losasso, Neil Molino, Andrew Selle, Eftychios Sifakis, Rachel Weinstein.
+// This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
+//#####################################################################
+// Class IMPLICIT_OBJECT_INVERT
+//#####################################################################
+#ifndef __IMPLICIT_OBJECT_INVERT__
+#define __IMPLICIT_OBJECT_INVERT__
+
+#include <Tools/Math_Tools/RANGE.h>
+#include <Tools/Matrices/MATRIX_POLICY.h>
+#include <Tools/Utilities/TYPE_UTILITIES.h>
+#include <Geometry/Basic_Geometry/RAY.h>
+#include <Geometry/Topology_Based_Geometry/STRUCTURE.h>
+namespace PhysBAM{
+
+template<class TV>
+class IMPLICIT_OBJECT_INVERT:public IMPLICIT_OBJECT<TV>
+{
+    typedef typename TV::SCALAR T;
+    enum WORKAROUND {d=TV::m};
+    typedef VECTOR<T,d-1> T_CURVATURES;
+public:
+    typedef int HAS_TYPED_READ_WRITE;
+    typedef TV VECTOR_T;
+    
+    using IMPLICIT_OBJECT<TV>::box;
+    using IMPLICIT_OBJECT<TV>::use_secondary_interpolation;
+
+    IMPLICIT_OBJECT<TV>& io;
+    bool owns_io;
+
+    IMPLICIT_OBJECT_INVERT(IMPLICIT_OBJECT<TV>* o)
+        :io(*o),owns_io(true)
+    {
+    }
+    virtual ~IMPLICIT_OBJECT_INVERT();
+
+    void Use_Secondary_Interpolation(const bool use_secondary_interpolation_input=true)
+    {use_secondary_interpolation=use_secondary_interpolation_input;}
+
+//#####################################################################
+    void Update_Box() override;
+    void Update_Minimum_Cell_Size(const int maximum_depth=0) override;
+    T Minimum_Cell_Size_Within_Box(const RANGE<TV>& box) const override;
+    T operator()(const TV& location) const override;
+    T Extended_Phi(const TV& location) const override;
+    T Phi_Secondary(const TV& location) const override;
+    TV Normal(const TV& location,const int aggregate=-1) const override;
+    TV Extended_Normal(const TV& location,const int aggregate=-1) const override;
+    void Compute_Normals() override;
+    void Compute_Cell_Minimum_And_Maximum(const bool recompute_if_exists=true) override;
+    void Rescale(const T scaling_factor) override;
+    void Translate(const TV& translation) override;
+    void Inflate(const T inflation_distance) override;
+    bool Inside(const TV& location,const T thickness_over_two=0) const override;
+    bool Outside(const TV& location,const T thickness_over_two=0) const override;
+    bool Boundary(const TV& location,const T thickness_over_two) const override;
+    bool Lazy_Inside(const TV& location,const T contour_value=0) const override;
+    bool Lazy_Inside_And_Value(const TV& location,T& phi_value,const T contour_value=0) const override;
+    bool Lazy_Inside_Extended_Levelset(const TV& unclamped_X,const T contour_value=0) const override;
+    bool Lazy_Inside_Extended_Levelset_And_Value(const TV& unclamped_X,T& phi_value,const T contour_value=0) const override;
+    bool Lazy_Outside(const TV& location,const T contour_value=0) const override;
+    bool Lazy_Outside_Extended_Levelset(const TV& unclamped_X,const T contour_value=0) const override;
+    bool Lazy_Outside_Extended_Levelset_And_Value(const TV& unclamped_X,T& phi_value,const T contour_value=0) const override;
+    T Min_Phi() const override;
+    bool Intersection(RAY<TV>& ray,const T thickness=0) const override;
+    TV Closest_Point_On_Boundary(const TV& location,const T tolerance=0,const int max_iterations=1,T* distance=0) const override;
+    TV Velocity(const TV& location) const override;
+    // the following only exist in 3d
+    SYMMETRIC_MATRIX<T,TV::m> Hessian(const TV& X) const override;
+    T_CURVATURES Principal_Curvatures(const TV& X) const override;
+    T Integration_Step(const T phi) const override;
+    T Minimum_Cell_Size() const override;
+    void Read(TYPED_ISTREAM& input) override;
+    void Write(TYPED_OSTREAM& output) const override;
+//#####################################################################
+};
+}
+#endif
