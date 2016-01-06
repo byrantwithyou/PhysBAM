4ba4d236ebec62aca1bfc04f40ebcaf2bf9b5a7f
fanfu <chenfanfu.jiang@gmail.com>
2016-01-05 22:37:11 -0800

    collision type tune

--- Projects/mpm/STANDARD_TESTS_3D.cpp
+++ Projects/mpm/STANDARD_TESTS_3D.cpp
@@ -914,7 +914,7 @@ Initialize()
             LOG::cout<<"GRID dx: "<<grid.dX<<std::endl;
             RANGE<TV> ground(TV(-10,-10,-10)*m,TV(10,0,10)*m);
             if(use_penalty_collisions) Add_Penalty_Collision_Object(ground);
-            else Add_Collision_Object(ground,COLLISION_TYPE::slip,10); //friction hard coded to be 10
+            else Add_Collision_Object(ground,COLLISION_TYPE::separate,0.6);
             T density=(T)2200*unit_rho*scale_mass;
             T E=35.37e6*unit_p*scale_E,nu=.3;
             if(!no_implicit_plasticity) use_implicit_plasticity=true;
