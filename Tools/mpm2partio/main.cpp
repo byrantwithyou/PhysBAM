0a21f9ff18fd7abb681243fc06734a7a7edd1af5
Greg Klar <gklar@ucla.edu>
2016-01-07 13:02:10 -0800

    mpm2partio: restart and attentive mode, parameter check

--- Tools/mpm2partio/main.cpp
+++ Tools/mpm2partio/main.cpp
@@ -8,6 +8,7 @@
 #include <Tools/Read_Write/FILE_UTILITIES.h>
 #include <Hybrid_Methods/Examples_And_Drivers/MPM_PARTICLES.h>
 #include <Partio.h>
+#include <regex>
 
 using namespace PhysBAM;
 
@@ -62,14 +63,17 @@ void writePartio(const std::string& output_filename,const MPM_PARTICLES<VECTOR<T
 }
 
 template<class T,int N>
-void Convert(const std::string& input_directory,const std::string& output_filename_pattern,bool dump_valid)
+void Convert(const std::string& input_directory,const std::string& output_filename_pattern,int start_at,bool dump_valid,bool attentive)
 {
     MPM_PARTICLES<VECTOR<T,N> > particles;
     int last_frame;
     FILE_UTILITIES::Read_From_Text_File(input_directory+"/common/last_frame",last_frame);
-    for(int i=0;i<=last_frame;++i){
+    PHYSBAM_ASSERT(start_at>0 && start_at<=last_frame);
+    for(int i=start_at;i<=last_frame;++i){
         FILE_UTILITIES::Read_From_File<T>(LOG::sprintf("%s/%d/mpm_particles.gz",input_directory,i),particles);
-        writePartio<T,N>(LOG::sprintf(output_filename_pattern.c_str(),i),particles,dump_valid);}
+        writePartio<T,N>(LOG::sprintf(output_filename_pattern.c_str(),i),particles,dump_valid);
+        if(attentive)
+            FILE_UTILITIES::Read_From_Text_File(input_directory+"/common/last_frame",last_frame);}
 }
 
 int main(int argc,char *argv[])
@@ -77,6 +81,8 @@ int main(int argc,char *argv[])
     bool type_double=true;
     bool dump_valid=false;
     bool use_3d=false;
+    bool attentive=true;
+    int start_at=0;
     std::string input_directory,output_filename_pattern;
 
     PARSE_ARGS parse_args(argc,argv);
@@ -84,16 +90,22 @@ int main(int argc,char *argv[])
     parse_args.Add("-double",&type_double,"Use doubles");
     parse_args.Add("-dump_valid_flag",&dump_valid,"Dump all particles along with a valid flag; don't prune invalid particles");
     parse_args.Add("-3d",&use_3d,"Convert 3D examples");
+    parse_args.Add("-start",&start_at,"start_at","Start conversion from this frame");
+    parse_args.Add_Not("-no_attentive",&attentive,"Don't look for new frames during conversion");
     parse_args.Extra(&input_directory,"sim-dir","simulation output directory");
     parse_args.Extra(&output_filename_pattern,"output-pattern","output partio file name pattern, use %d");
     parse_args.Parse();
 
+    if(!std::regex_match(output_filename_pattern,std::regex(".*%[0-9]*d.*"))){
+        LOG::printf("Format string missing from filename! Eg. %%04d\n");
+        return -1;}
+
     if(type_double)
-        if(use_3d) Convert<double,3>(input_directory,output_filename_pattern,dump_valid);
-        else Convert<double,2>(input_directory,output_filename_pattern,dump_valid);
+        if(use_3d) Convert<double,3>(input_directory,output_filename_pattern,start_at,dump_valid,attentive);
+        else Convert<double,2>(input_directory,output_filename_pattern,start_at,dump_valid,attentive);
     else
-        if(use_3d) Convert<float,3>(input_directory,output_filename_pattern,dump_valid);
-        else Convert<float,2>(input_directory,output_filename_pattern,dump_valid);
+        if(use_3d) Convert<float,3>(input_directory,output_filename_pattern,start_at,dump_valid,attentive);
+        else Convert<float,2>(input_directory,output_filename_pattern,start_at,dump_valid,attentive);
 
     return 0;
 }
