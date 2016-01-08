55c3a2b03e14b9c4671f2fb59998eee3ce2be5af
fanfu <chenfanfu.jiang@gmail.com>
2016-01-07 22:35:13 -0800

    endless tuning

--- Projects/mpm/STANDARD_TESTS_3D.cpp
+++ Projects/mpm/STANDARD_TESTS_3D.cpp
@@ -1082,12 +1082,12 @@ Initialize()
 
          case 45:{ // sand castle
             particles.Store_Fp(true);
-            grid.Initialize(TV_INT(11,7,10)*resolution,RANGE<TV>(TV(-1.0,-0.1,-1.0)*m,TV(1.2,1.3,1.0)*m),true);
+            grid.Initialize(TV_INT(15,7,10)*resolution,RANGE<TV>(TV(-1.0,-0.1,-1.0)*m,TV(2.0,1.3,1.0)*m),true);
             LOG::cout<<"GRID dx: "<<grid.dX<<std::endl;
             RANGE<TV> boxymin(TV(-10,-10,-10)*m,TV(10,0,10)*m);
             RANGE<TV> boxymax(TV(-10,1.2,-10)*m,TV(10,10,10)*m);
             RANGE<TV> boxxmin(TV(-10,-10,-10)*m,TV(-0.9,10,10)*m);
-            RANGE<TV> boxxmax(TV(1.1,-10,-10)*m,TV(10,10,10)*m);
+            RANGE<TV> boxxmax(TV(1.9,-10,-10)*m,TV(10,10,10)*m);
             RANGE<TV> boxzmin(TV(-10,-10,-10)*m,TV(10,10,-0.9)*m);
             RANGE<TV> boxzmax(TV(-10,-10,0.9)*m,TV(10,10,10)*m);
             IMPLICIT_OBJECT_UNION<TV>* box=new IMPLICIT_OBJECT_UNION<TV>(
@@ -1125,7 +1125,7 @@ Initialize()
             SPHERE<TV> sphere1(TV(-0.64,0.52,0)*m,.15*m);
             T density_sphere=1e4*unit_rho*scale_mass;
             VECTOR<T,3> angular_velocity1(TV(0,0,foo_T2));
-            Seed_Particles(sphere1,[=](const TV& X){return angular_velocity1.Cross(X-sphere1.center)+TV(10,-3,0)*(m/s);},
+            Seed_Particles(sphere1,[=](const TV& X){return angular_velocity1.Cross(X-sphere1.center)+TV(5,-1.5,0)*(m/s);},
                 [=](const TV&){return MATRIX<T,3>::Cross_Product_Matrix(angular_velocity1);},density_sphere,particles_per_cell);
             Add_Fixed_Corotated(40e5*unit_p,0.3);
             Add_Gravity(m/(s*s)*TV(0,-9.80665,0));
