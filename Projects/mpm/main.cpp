3686407bb0e1e35eda500cffbc9e273f862bc28c
Theodore Gast <tfg@math.ucla.edu>
2016-01-12 17:31:26 -0800

    Allow runnning with floats

--- Projects/mpm/main.cpp
+++ Projects/mpm/main.cpp
@@ -21,21 +21,27 @@ void Run_Test(PARSE_ARGS& parse_args,STREAM_TYPE stream_type)
 
 int main(int argc,char *argv[])
 {
-    typedef double T;
-    typedef double RW;
-    STREAM_TYPE stream_type((RW()));
+    bool type_double=true;
     PROCESS_UTILITIES::Set_Floating_Point_Exception_Handling(true);
     PROCESS_UTILITIES::Set_Backtrace(true);
 
     bool use_3d=false;
     PARSE_ARGS parse_args(argc,argv);
+    parse_args.Add_Not("-float",&type_double,"Use floats");
+    parse_args.Add("-double",&type_double,"Use doubles");
     parse_args.Add("-3d",&use_3d,"run 3D examples");
     LOG::Initialize_Logging(false,false,1<<30,true);
     LOG::cout<<parse_args.Print_Arguments()<<std::endl;
 
     parse_args.Parse(true);
-    if(use_3d) Run_Test<VECTOR<T,3> >(parse_args,stream_type);
-    else Run_Test<VECTOR<T,2> >(parse_args,stream_type);
+    if(type_double){
+        STREAM_TYPE stream_type((double()));
+        if(use_3d) Run_Test<VECTOR<double,3> >(parse_args,stream_type);
+        else Run_Test<VECTOR<double,2> >(parse_args,stream_type);}
+    else{
+        STREAM_TYPE stream_type((float()));
+        if(use_3d) Run_Test<VECTOR<float,3> >(parse_args,stream_type);
+        else Run_Test<VECTOR<float,2> >(parse_args,stream_type);}
 
     LOG::Finish_Logging();
     return 0;
