141de7ea83587b4e6201840bf6997488b5fc93e5
fanfu <chenfanfu.jiang@gmail.com>
2016-01-15 20:13:33 -0800

    sticky letter ground

--- Projects/mpm/STANDARD_TESTS_3D.cpp
+++ Projects/mpm/STANDARD_TESTS_3D.cpp
@@ -1269,7 +1269,8 @@ Initialize()
             LOG::cout<<"GRID dx: "<<grid.dX<<std::endl;
             RANGE<TV> ground(TV(-10,-10,-10)*m,TV(10,0.05,10)*m);
             if(use_penalty_collisions) Add_Penalty_Collision_Object(ground);
-            else Add_Collision_Object(ground,COLLISION_TYPE::slip,10);
+            // else Add_Collision_Object(ground,COLLISION_TYPE::slip,10);
+            else Add_Collision_Object(ground,COLLISION_TYPE::stick,0);
             T density=(T)2200*unit_rho*scale_mass;
             T E=35.37e6*unit_p*scale_E,nu=.3;
             if(!no_implicit_plasticity) use_implicit_plasticity=true;
