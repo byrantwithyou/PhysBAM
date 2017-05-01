d6fa578e6215d8616739ccf3f01a564f9b5783b1
Craig Schroeder <snubdodecahedron@gmail.com>
2017-05-01 09:35:42 -0700

    Cleanup

--- Public_Library/Grid_PDE/Boundaries/BOUNDARY_MAC_GRID_PERIODIC.cpp
+++ Public_Library/Grid_PDE/Boundaries/BOUNDARY_MAC_GRID_PERIODIC.cpp
@@ -47,7 +47,7 @@ Fill_Ghost_Cells(const GRID<TV>& grid,const ARRAYS_ND_BASE<T2,TV_INT>& u,ARRAYS_
 // Function Fill_Ghost_Faces
 //#####################################################################
 template<class TV,class T2> void BOUNDARY_MAC_GRID_PERIODIC<TV,T2>::
-Fill_Ghost_Faces(const GRID<TV>& grid,const T_FACE_ARRAYS_T2& u,T_FACE_ARRAYS_T2& u_ghost,const T time,const int number_of_ghost_cells) const
+Fill_Ghost_Faces(const GRID<TV>& grid,const ARRAY<T2,FACE_INDEX<TV::m> >& u,ARRAY<T2,FACE_INDEX<TV::m> >& u_ghost,const T time,const int number_of_ghost_cells) const
 {
     assert(grid.Is_MAC_Grid());
     for(int axis=0;axis<TV::m;axis++){
@@ -57,12 +57,15 @@ Fill_Ghost_Faces(const GRID<TV>& grid,const T_FACE_ARRAYS_T2& u,T_FACE_ARRAYS_T2
         ARRAYS_ND_BASE<T2,TV_INT>::Put(u_axis,u_ghost_axis); // interior
         GRID<TV> face_grid=grid.Get_Face_Grid(axis);
         TV_INT periods=grid.Domain_Indices().Maximum_Corner();
-        VECTOR<RANGE<TV_INT>,2*TV::m> regions;Find_Ghost_Regions(face_grid,regions,number_of_ghost_cells);
-        for(int face_axis=0;face_axis<TV::m;face_axis++)for(int axis_side=0;axis_side<2;axis_side++){
-            int side=2*face_axis+axis_side;
-            TV_INT period=(axis_side==0?1:-1)*periods[face_axis]*TV_INT::Axis_Vector(face_axis);
-            for(NODE_ITERATOR<TV> iterator(face_grid,regions(side));iterator.Valid();iterator.Next()){TV_INT node=iterator.Node_Index();
-                u_ghost_axis(node)=u_ghost_axis(node+period);}}}
+        VECTOR<RANGE<TV_INT>,2*TV::m> regions;
+        Find_Ghost_Regions(face_grid,regions,number_of_ghost_cells);
+        for(int face_axis=0;face_axis<TV::m;face_axis++)
+            for(int axis_side=0;axis_side<2;axis_side++){
+                int side=2*face_axis+axis_side;
+                TV_INT period=(axis_side==0?1:-1)*periods[face_axis]*TV_INT::Axis_Vector(face_axis);
+                for(NODE_ITERATOR<TV> iterator(face_grid,regions(side));iterator.Valid();iterator.Next()){
+                    TV_INT node=iterator.Node_Index();
+                    u_ghost_axis(node)=u_ghost_axis(node+period);}}}
 }
 //#####################################################################
 namespace PhysBAM{
--- Public_Library/Grid_PDE/Boundaries/BOUNDARY_MAC_GRID_PERIODIC.h
+++ Public_Library/Grid_PDE/Boundaries/BOUNDARY_MAC_GRID_PERIODIC.h
@@ -14,8 +14,6 @@ template<class TV,class T2>
 class BOUNDARY_MAC_GRID_PERIODIC:public BOUNDARY<TV,T2>
 {
     typedef typename TV::SCALAR T;typedef VECTOR<int,TV::m> TV_INT;
-    typedef ARRAYS_ND_BASE<T,TV_INT> T_ARRAYS_BASE;
-    typedef ARRAY<T2,FACE_INDEX<TV::m> > T_FACE_ARRAYS_T2;
 public:
     using BOUNDARY<TV,T2>::Find_Ghost_Regions;
 
@@ -24,7 +22,7 @@ public:
 
 //#####################################################################
     void Fill_Ghost_Cells(const GRID<TV>& grid,const ARRAYS_ND_BASE<T2,TV_INT>& u,ARRAYS_ND_BASE<T2,TV_INT>& u_ghost,const T dt,const T time,const int number_of_ghost_cells=3) const override;
-    void Fill_Ghost_Faces(const GRID<TV>& grid,const T_FACE_ARRAYS_T2& u,T_FACE_ARRAYS_T2& u_ghost,const T time,const int number_of_ghost_cells=3) const override;
+    void Fill_Ghost_Faces(const GRID<TV>& grid,const ARRAY<T2,FACE_INDEX<TV::m> >& u,ARRAY<T2,FACE_INDEX<TV::m> >& u_ghost,const T time,const int number_of_ghost_cells=3) const override;
     void Apply_Boundary_Condition(const GRID<TV>& grid,ARRAYS_ND_BASE<T2,TV_INT>& u,const T time) const override {} // do nothing
     void Apply_Boundary_Condition_Face(const GRID<TV>& grid,ARRAY<T2,FACE_INDEX<TV::m> >& u,const T time) const override;
 //#####################################################################
