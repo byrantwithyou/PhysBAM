6fcf764dddb9c1fe899f59073944b7ec0926d98e
Ounan Ding <oding001@ucr.edu>
2017-07-17 17:34:11 -0700

    add scripts for irregular seeding, particle location experiment.

--- Projects/mpm_mac/2017-jcp-apic-scripts/run_irregular_seeding.sh
+++ Projects/mpm_mac/2017-jcp-apic-scripts/run_irregular_seeding.sh
@@ -5,19 +5,36 @@ NAME=eig-irregular-seeding
 RES=64
 FULL=1 # Set to 1 for a full rebuild; 0 to skip rerunning the simulations
 
-ARGS="../../fourier-apic/fourier_mac -resolution $RES -size $RES -dump_particles -dump_eigenvalues"
+SEED=42
 
-default_seed=0
+ARGS="../../fourier-apic/fourier_mac -resolution $RES -size $RES -dump_particles -dump_eigenvalues -seed $SEED"
 
-np=("-irreg 1" "-irreg 4" "-irreg 9" "-irreg 25" "-irreg 64")
-np_name=("ppc1" "ppc4" "ppc9" "ppc25" "ppc64")
+
+np=("-irreg 1" "-irreg 4" "-irreg 9" "-irreg 16" "-irreg 25" "-irreg 64")
+np_name=("ppc1" "ppc4" "ppc9" "ppc16" "ppc25" "ppc64")
 
 order=("-order 2" "-order 3")
 order_name=("quadratic" "cubic")
 
+px=($(seq 0 0.1 0.5))
+#py=($(seq 0 0.1 0.5))
+py=($(seq 0.1 0.1 0.1))
+
 if [ "X$FULL" = "X1" ] ; then
     rm -rf $NAME
     mkdir -p $NAME
+    for x in ${px[@]}; do
+        for y in ${py[@]}; do
+            for o in `seq 0 $((${#order[@]}-1))` ; do
+                coord=$x-$y
+                pic_folder=$NAME/pic-single-$coord-${order_name[$o]}
+                apic_folder=$NAME/apic-single-$coord-${order_name[$o]}
+                echo $ARGS -px $x -py $y ${order[$o]} -v $pic_folder -o $pic_folder/eigen.png
+                echo $ARGS -affine -px $x -py $y ${order[$o]} -v $apic_folder -o $apic_folder/eigen.png
+            done
+        done
+    done | xargs -P 8 -n 1 -d '\n' bash -c
+
     for p in `seq 0 $((${#np[@]}-1))` ; do
         for o in `seq 0 $((${#order[@]}-1))` ; do
             pic_folder=$NAME/pic-${np_name[$p]}-${order_name[$o]}
@@ -28,7 +45,25 @@ if [ "X$FULL" = "X1" ] ; then
     done | xargs -P 8 -n 1 -d '\n' bash -c
 fi
 
+plot_template='\\addplot [mark=none,solid] table[x=waven,y=eig]'
+node_template=' node[above right,pos=0.8,scale=0.5] '
+content=''
+for x in ${px[@]}; do
+    for y in ${py[@]}; do
+        line=$plot_template'{apic-single-'$x-$y-'xxx\/eig-aaa.txt}'$node_template'{('$x,$y')};'
+        content=$content$line'\n'
+    done
+done
+
+sed -e "s/XXXXXX/$content/g" eig_irregular_seeding_ppc1_plot.tex > $NAME/eig_irregular_seeding_apic_ppc1.template
+sed -e "s/apic/pic/g" $NAME/eig_irregular_seeding_apic_ppc1.template > $NAME/eig_irregular_seeding_pic_ppc1.template
+
 for a in x y xy ; do
+    sed -e "s/aaa/$a/g; s/xxx/quadratic/g" $NAME/eig_irregular_seeding_apic_ppc1.template > $NAME/eig-irregular-seeding-apic-ppc1-quadratic-$a.tex
+    sed -e "s/aaa/$a/g; s/xxx/cubic/g" $NAME/eig_irregular_seeding_apic_ppc1.template > $NAME/eig-irregular-seeding-apic-ppc1-cubic-$a.tex
+    sed -e "s/aaa/$a/g; s/xxx/quadratic/g" $NAME/eig_irregular_seeding_pic_ppc1.template > $NAME/eig-irregular-seeding-pic-ppc1-quadratic-$a.tex
+    sed -e "s/aaa/$a/g; s/xxx/cubic/g" $NAME/eig_irregular_seeding_pic_ppc1.template > $NAME/eig-irregular-seeding-pic-ppc1-cubic-$a.tex
+
     sed -e "s/aaa/$a/g; s/xxx/quadratic/g" eig_irregular_seeding_plot.tex  > $NAME/eig-irregular-seeding-quadratic-$a.tex
     sed -e "s/aaa/$a/g; s/xxx/cubic/g" eig_irregular_seeding_plot.tex  > $NAME/eig-irregular-seeding-cubic-$a.tex
 done
