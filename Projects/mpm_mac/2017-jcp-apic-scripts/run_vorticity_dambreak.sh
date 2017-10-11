e684305a363d558ace6d76f0f11485fc6115bb37
Ounan Ding <oding001@ucr.edu>
2017-10-11 13:41:29 -0700

    show velocity and vorticity of dambreak test under refinement.

--- Projects/mpm_mac/2017-jcp-apic-scripts/run_vorticity_dambreak.sh
+++ Projects/mpm_mac/2017-jcp-apic-scripts/run_vorticity_dambreak.sh
@@ -2,41 +2,67 @@
 
 NAME=vort-dambreak
 
-RES=64
-FULL=1 # Set to 1 for a full rebuild; 0 to skip rerunning the simulations
+FULL=0 # Set to 1 for a full rebuild; 0 to skip rerunning the simulations
 
-dt=`perl -e "print (1.0/$RES)"`
-DT="-max_dt $dt -min_dt $dt"
-ARGS="../mpm_mac 25 -last_frame 10 -frame_dt 1 -clamp -mu 0 -scale_mass 3 -resolution $RES -analyze_u_modes -dump_modes_freq $RES $DT -max_ke 15"
+ARGS="../mpm_mac 25 -last_frame 10 -frame_dt 1 -clamp -mu 0 -scale_mass 3 -analyze_u_modes -max_ke 15"
 
 opt=("" "-regular_seeding" "-order 3" "-regular_seeding -order 3")
 opt_name=("default" "regular" "default-cubic" "regular-cubic")
+
+LO=32
+LO_REG_RES=64
+HI=256
+SKIP=32
 
 if [ "X$FULL" = "X1" ] ; then
     rm -rf $NAME
     mkdir -p $NAME
     for o in `seq 0 $((${#opt[@]}-1))` ; do
-        echo $ARGS $DT -no_affine ${opt[$o]} -o $NAME/pic-${opt_name[$o]}
-        echo $ARGS $DT -affine ${opt[$o]} -o $NAME/apic-${opt_name[$o]}
-        echo $ARGS $DT -no_affine ${opt[$o]} -flip 1 -o $NAME/flip-${opt_name[$o]}
-    done | xargs -P 8 -n 1 -d '\n' bash -c
+        for r in `seq $HI -$SKIP $LO` ; do
+            dt=`perl -e "print (1.0/$r)"`
+            DT="-max_dt $dt -min_dt $dt"
+            echo $ARGS $DT -no_affine ${opt[$o]} -resolution $r -dump_modes_freq $r -o $NAME/pic-${opt_name[$o]}-$r
+            echo $ARGS $DT -affine ${opt[$o]} -resolution $r -dump_modes_freq $r -o $NAME/apic-${opt_name[$o]}-$r
+            #echo $ARGS $DT -no_affine ${opt[$o]} -flip 1 -resolution $r -dump_modes_freq $r -o $NAME/flip-${opt_name[$o]}-$r
+        done
+    done | xargs -P 16 -n 1 -d '\n' bash -c
 fi
 
-for s in pic apic flip ; do
-    for o in `seq 0 $((${#opt[@]}-1))` ; do
-        ./vort_parse_data.pl < $NAME/$s-${opt_name[$o]}/common/log.txt > $NAME/$s-${opt_name[$o]}-norms.txt
+for s in pic apic ; do
+    for r in `seq $HI -$SKIP $LO` ; do
+        for o in `seq 0 $((${#opt[@]}-1))` ; do
+            ./vort_parse_data.pl < $NAME/$s-${opt_name[$o]}-$r/common/log.txt > $NAME/$s-${opt_name[$o]}-$r/norms.txt
+        done
     done
 done
 
+RES=64
 for o in ${opt_name[@]} ; do
-    sed -e "s/xxx/$o/" vort_dambreak_plot.tex  > $NAME/plot-dambreak-vort-$o.tex
-    sed -e "s/xxx/$o/" vel_dambreak_plot.tex  > $NAME/plot-dambreak-vel-$o.tex
+    sed -e "s/xxx/$o/" -e "s/rrr/$RES/" vort_dambreak_plot.tex  > $NAME/plot-dambreak-vort-$o.tex
+    sed -e "s/xxx/$o/" -e "s/rrr/$RES/" vel_dambreak_plot.tex  > $NAME/plot-dambreak-vel-$o.tex
 done
 
 for i in {pic,apic}-{default,regular} ; do
     for j in {0..10} ; do
-        convert -scale 512 $NAME/$i/modes-$(($j*$RES))-x.png `printf $NAME/$i-dambreak-vort-%02d.png $j`
-        convert -scale 512 $NAME/$i/modes-$(($j*$RES))-x.png `printf $NAME/$i-dambreak-vort-%02d.eps $j`
+        convert -scale 512 $NAME/$i-$RES/modes-$(($j*$RES))-x.png `printf $NAME/$i-dambreak-vort-%02d.png $j`
+        convert -scale 512 $NAME/$i-$RES/modes-$(($j*$RES))-x.png `printf $NAME/$i-dambreak-vort-%02d.eps $j`
+    done
+done
+
+colors=('black' 'red' 'blue' 'green' 'cyan' 'purple' 'yellow' 'orange')
+template='\\addplot [mark=none,solid,color=COLOR] table[x=time,y=VVV]{ALGO-RES\/norms.txt};'
+for i in {pic,apic}-{default,regular} ; do
+    for v in {vel,vort} ; do
+        content=''
+        legend_content=''
+        nline=0
+        for r in `seq $HI -$SKIP $LO` ; do
+            line=`echo "$template" | sed -e "s/VVV/$v/g; s/ALGO/$i/g; s/RES/$r/g; s/COLOR/${colors[$nline]}/g"`
+            content="$content$line\n"
+            legend_content="$legend_content$r,"
+            nline=$((nline+1))
+        done
+        sed -e "s/XXXXXX/$content/g; s/LLLLLL/$legend_content/g" ${v}_dambreak_refinement_plot.tex > $NAME/${i}-${v}-refinement-plot.tex
     done
 done
 
