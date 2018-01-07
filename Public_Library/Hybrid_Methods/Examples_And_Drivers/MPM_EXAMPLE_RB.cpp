ab9fe0b7e186fa01898dd5ab674fa29f5b03d137
Craig Schroeder <snubdodecahedron@gmail.com>
2018-01-06 20:11:13 -0800

    Do not need extra padding anymore.

--- Public_Library/Hybrid_Methods/Examples_And_Drivers/MPM_EXAMPLE_RB.cpp
+++ Public_Library/Hybrid_Methods/Examples_And_Drivers/MPM_EXAMPLE_RB.cpp
@@ -410,7 +410,7 @@ Update_Collision_Detection_Structures()
             cell_particles.Insert(index,p);}
 
     RIGID_BODY_PARTICLES<TV>& rigid_body_particles=solid_body_collection.rigid_body_collection.rigid_body_particles;
-    T padding=grid.dX.Max()*2;
+    T padding=grid.dX.Max()*.5;
     for(int b=0;b<rigid_body_particles.frame.m;b++){
         RIGID_BODY<TV>& rigid_body=*rigid_body_particles.rigid_body(b);
         rigid_body.Update_Bounding_Box_From_Implicit_Geometry();
