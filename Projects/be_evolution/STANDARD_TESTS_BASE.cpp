a32bc6a154fb4b19f8dc9b50fa8859188aedbaad
Ounan Ding <oding001@ucr.edu>
2018-01-30 17:05:37 -0800

    Use float stream for SOLIDS_STANDARD_TESTS to read data.

--- Projects/be_evolution/STANDARD_TESTS_BASE.cpp
+++ Projects/be_evolution/STANDARD_TESTS_BASE.cpp
@@ -34,7 +34,7 @@
 namespace PhysBAM{
 template<class TV> STANDARD_TESTS_BASE<TV>::
 STANDARD_TESTS_BASE(const STREAM_TYPE stream_type_input,PARSE_ARGS& parse_args)
-    :BASE(stream_type_input,parse_args),tests(stream_type_input,data_directory,solid_body_collection),
+    :BASE(stream_type_input,parse_args),tests(STREAM_TYPE(.0f),data_directory,solid_body_collection),
     backward_euler_evolution(new BACKWARD_EULER_EVOLUTION<TV>(solids_parameters,solid_body_collection,*this))
 {
     this->fixed_dt=1./240;
