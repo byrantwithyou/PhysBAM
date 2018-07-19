ece4a03da6d25d70c798a61a058685989fb96a93
Craig Schroeder <snubdodecahedron@gmail.com>
2018-07-19 16:35:50 -0700

    add missing files

---
+++ Projects/microfluidics-test/FREQUENCY_TRACKER.cpp
@@ -0,0 +1,73 @@
+//#####################################################################
+// Copyright 2018.
+// This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
+//#####################################################################
+#include <cassert>
+#include "FREQUENCY_TRACKER.h"
+namespace PhysBAM{
+void FREQUENCY_TRACKER::Add(int id, int row)
+{
+    auto it=id_map.find(id);
+    pr p;
+    if(it!=id_map.end())
+    {
+        p=it->second->second;
+        freq_map.erase(it->second);
+    }
+    else p=std::make_pair(new std::set<int>,id);
+    p.first->insert(row);
+    id_map[id]=freq_map.insert(std::make_pair(p.first->size(),p));
+}
+void FREQUENCY_TRACKER::Remove(int id, int row)
+{
+    auto it=id_map.find(id);
+    if(it==id_map.end()) return;
+    pr p=it->second->second;
+    freq_map.erase(it->second);
+    p.first->erase(row);
+    if(p.first->empty())
+    {
+        delete p.first;
+        id_map.erase(it);
+    }
+    else id_map[id]=freq_map.insert(std::make_pair(p.first->size(),p));
+}
+int FREQUENCY_TRACKER::Most_Frequent(std::vector<int>& v) const
+{
+    v.clear();
+    auto it=freq_map.end();
+    it--;
+    for(auto a:*it->second.first)
+        v.push_back(a);
+    return it->second.second;
+}
+bool FREQUENCY_TRACKER::Empty() const
+{
+    return freq_map.empty();
+}
+void FREQUENCY_TRACKER::Dump() const
+{
+    auto dump_mm=[](mm::const_iterator m)
+        {
+            printf("freq: %i  rows:",m->first);
+            for(auto a:*m->second.first) printf(" %i",a);
+            printf(" id: %i",m->second.second);
+        };
+
+    printf("id map:\n");
+    for(auto a:id_map)
+    {
+        printf("%i -> [",a.first);
+        dump_mm(a.second);
+        printf("]\n");
+    }
+
+    printf("freq map:\n");
+    for(auto it=freq_map.begin();it!=freq_map.end();it++)
+    {
+        dump_mm(it);
+    printf("\n");
+    }
+
+}
+}
---
+++ Projects/microfluidics-test/FREQUENCY_TRACKER.h
@@ -0,0 +1,28 @@
+//#####################################################################
+// Copyright 2018.
+// This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
+//#####################################################################
+#ifndef __FREQUENCY_TRACKER__
+#define __FREQUENCY_TRACKER__
+#include <set>
+#include <map>
+#include <vector>
+
+namespace PhysBAM{
+
+struct FREQUENCY_TRACKER
+{
+    typedef std::pair<std::set<int>*,int> pr;
+    typedef std::multimap<int,pr> mm;
+    std::map<int,mm::iterator> id_map;
+    mm freq_map;
+
+    void Add(int id, int row);
+    void Remove(int id, int row);
+    int Most_Frequent(std::vector<int>& v) const;
+    bool Empty() const;
+    void Dump() const;
+};
+
+}
+#endif
