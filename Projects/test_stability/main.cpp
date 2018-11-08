4327190abb4dd0b10cff5f6449f054e60950bff4
Ounan Ding <oding001@ucr.edu>
2018-11-08 11:53:17 -0800

    fix 2d case.

--- Projects/test_stability/main.cpp
+++ Projects/test_stability/main.cpp
@@ -9,6 +9,7 @@
 #include <Core/Matrices/SYSTEM_MATRIX_HELPER.h>
 #include <Core/Utilities/PROCESS_UTILITIES.h>
 #include <Core/Vectors/VECTOR.h>
+#include <Tools/Interpolation/INTERPOLATED_COLOR_MAP.h>
 #include <Tools/Parsing/PARSE_ARGS.h>
 #include <Tools/Read_Write/OCTAVE_OUTPUT.h>
 #include <Grid_Tools/Grids/GRID.h>
@@ -16,6 +17,7 @@
 #include <Geometry/Geometry_Particles/VIEWER_OUTPUT.h>
 #include <Geometry/Topology/SEGMENT_MESH.h>
 #include <Geometry/Topology_Based_Geometry/TETRAHEDRALIZED_VOLUME.h>
+#include <Geometry/Topology_Based_Geometry/TRIANGULATED_AREA.h>
 #include <Rigids/Rigid_Bodies/RIGID_BODY_COLLECTION.h>
 #include <Deformables/Constitutive_Models/COROTATED_FIXED.h>
 #include <Deformables/Constitutive_Models/DIAGONALIZED_ISOTROPIC_STRESS_DERIVATIVE.h>
@@ -63,23 +65,28 @@ struct EXPLICIT_EXAMPLE
     typedef typename TV::SCALAR T;
     std::string out_dir;
     STREAM_TYPE stream_type;
-    int last_frame,frame,step;
+    int last_frame,frame,step,resolution;
     T time,default_dt,frame_dt,dt;
     T alpha;
     T damping,poissons_ratio,stiffness;
-    bool test_matrix,dump_matrix;
+    bool test_matrix,dump_matrix,compute_max_sound_speed,dump_sound_speed;
 
-    EXPLICIT_EXAMPLE():stream_type((T())),last_frame(100),default_dt(1e-3),frame_dt(1.0/24),alpha(1),
+    EXPLICIT_EXAMPLE():stream_type((T())),last_frame(100),resolution(8),default_dt(1e-3),frame_dt(1.0/24),alpha(1),
         damping(0),poissons_ratio(0.45),stiffness(1e4),
-        test_matrix(false),dump_matrix(false)
+        test_matrix(false),dump_matrix(false),compute_max_sound_speed(false),dump_sound_speed(false)
     {}
 };
 
 template<class T,int d>
-T Max_Sound_Speed(const FINITE_VOLUME<VECTOR<T,d>,d>& fvm)
+T Max_Sound_Speed(const FINITE_VOLUME<VECTOR<T,d>,d>& fvm,SOLID_BODY_COLLECTION<VECTOR<T,d> >& sbc,bool dump)
 {
     typedef VECTOR<T,d> TV;
     typedef typename FORCE_ELEMENTS::ITERATOR FORCE_ITERATOR;
+    typedef typename TOPOLOGY_BASED_SIMPLEX_POLICY<TV,d>::OBJECT T_OBJECT;
+    auto st=sbc.deformable_body_collection.template Find_Structure<T_OBJECT*>(0);
+    DEFORMABLE_PARTICLES<TV>& particles=sbc.deformable_body_collection.particles;
+    INTERPOLATED_COLOR_MAP<T> icm;
+    icm.Initialize_Colors((T)1e-8,1e3,false,false,false);
     T max_c=0;
     //SYMMETRIC_MATRIX<T,TV::m> H; // H(i,k) = x_iikk
     //typename TV::SPIN B,C; // B = x_ikik; C = x_ikki; order: (1D: none; 2D: 01; 3D: 12 20 01)
@@ -97,7 +104,14 @@ T Max_Sound_Speed(const FINITE_VOLUME<VECTOR<T,d>,d>& fvm)
             B(1,1)=dPdF.B(j)*sqr(F(l));
             B(0,1)=dPdF.C(j)*F(i)*F(l);
             k=max(k,B.Eigenvalues().x.Max_Abs());}
-        max_c=max(max_c,sqrt(k/(J*rho)));
+        T c=sqrt(k/(J*rho));
+        if(dump){
+            VECTOR<int,d+1>& elem=st->mesh.elements(t);
+            TV X=particles.X.Subset(elem).Average();
+            VECTOR<T,3> color=icm(c);
+            Add_Debug_Particle(X,color);
+            Debug_Particle_Set_Attribute<TV>("V",c+TV());}
+        max_c=max(max_c,c);
     }
     return max_c;
 }
@@ -107,9 +121,9 @@ void Step(SOLID_BODY_COLLECTION<TV>& sbc,EXPLICIT_EXAMPLE<TV>& example)
 {
     typedef typename TV::SCALAR T;
     sbc.Update_Position_Based_State(example.time,false,false);
-    for(int i=0;i<sbc.deformable_body_collection.deformables_forces.m;i++){
+    for(int i=0;example.compute_max_sound_speed && i<sbc.deformable_body_collection.deformables_forces.m;i++){
         FINITE_VOLUME<TV,TV::m>* fvm=dynamic_cast<FINITE_VOLUME<TV,TV::m>*>(sbc.deformable_body_collection.deformables_forces(i));
-        if(fvm) LOG::printf("Max c: %f\n",Max_Sound_Speed(*fvm));}
+        if(fvm) LOG::printf("Max c: %f\n",Max_Sound_Speed(*fvm,sbc,example.dump_sound_speed));}
     DEFORMABLE_PARTICLES<TV>& particles=sbc.deformable_body_collection.particles;
     ARRAY<TV> col(particles.number);
     auto idx=[](int p,int dim){return p*TV::m+dim;};
@@ -155,8 +169,8 @@ void Step(SOLID_BODY_COLLECTION<TV>& sbc,EXPLICIT_EXAMPLE<TV>& example)
             T r=0;
             for(int j=0;j<v1.m;j++) r=max(r,abs(v1(j)));
             if(r<1e-12){
-                ++n;
-                LOG::printf("limiting eigenvector: %P\n",v);}}
+                //LOG::printf("limiting eigenvector: %P\n",v);
+                ++n;}}
         LOG::printf("largest eigenvalue: %P magnitude: %f multiplicity: %d\n",eig(0),abs(eig(0)),n);}
 
     ARRAY<T> U,U1;
@@ -186,21 +200,23 @@ void Step(SOLID_BODY_COLLECTION<TV>& sbc,EXPLICIT_EXAMPLE<TV>& example)
         LOG::printf("L-inf error x: %P v: %P\n",x_error,v_error);}
 }
 
-template<class TV,class T>
-void Add_Constitutive_Model(SOLID_BODY_COLLECTION<TV>& sbc,EXPLICIT_EXAMPLE<TV>& example,TETRAHEDRALIZED_VOLUME<T>& st)
+template<class T,int d,class T_OBJECT>
+void Add_Constitutive_Model(SOLID_BODY_COLLECTION<VECTOR<T,d> >& sbc,EXPLICIT_EXAMPLE<VECTOR<T,d> >& example,T_OBJECT& st)
 {
-    sbc.Add_Force(Create_Finite_Volume(st,new COROTATED_FIXED<T,3>(example.stiffness,example.poissons_ratio,example.damping)));
+    typedef VECTOR<T,d> TV;
+    sbc.Add_Force(Create_Finite_Volume(st,new COROTATED_FIXED<T,d>(example.stiffness,example.poissons_ratio,example.damping)));
     DEFORMABLE_PARTICLES<TV>& particles=sbc.deformable_body_collection.particles;
     if(example.damping){
-        DEFORMABLES_FORCES<TV>* force=Create_Finite_Volume(st,new COROTATED_FIXED<T,3>(
+        DEFORMABLES_FORCES<TV>* force=Create_Finite_Volume(st,new COROTATED_FIXED<T,d>(
             example.stiffness,example.poissons_ratio,example.damping));
         force->Update_Position_Based_State(0,true,true);
         sbc.Add_Force(new RALEIGH_DAMPING_FORCE<TV>(particles,force,example.damping,1,example.dt));}
 }
 
-template<class T,class TV>
-T Min_Edge(const TETRAHEDRALIZED_VOLUME<T>& st,ARRAY_VIEW<TV> X)
+template<class T_OBJECT,class TV>
+typename TV::SCALAR Min_Edge(const T_OBJECT& st,ARRAY_VIEW<TV> X)
 {
+    typedef typename TV::SCALAR T;
     SEGMENT_MESH& segment_mesh=st.mesh.Get_Segment_Mesh();
     T len=1e6;
     for(int i=0;i<segment_mesh.elements.m;i++)
@@ -208,11 +224,12 @@ T Min_Edge(const TETRAHEDRALIZED_VOLUME<T>& st,ARRAY_VIEW<TV> X)
     return len;
 }
 
-template<class TV>
-void Setup(SOLID_BODY_COLLECTION<TV>& sbc,EXPLICIT_EXAMPLE<TV>& example,SOLIDS_STANDARD_TESTS<TV>& tests,int test_case)
+template<class T>
+void Setup(SOLID_BODY_COLLECTION<VECTOR<T,3> >& sbc,EXPLICIT_EXAMPLE<VECTOR<T,3> >& example,
+    SOLIDS_STANDARD_TESTS<VECTOR<T,3> >& tests,int test_case)
 {
-    typedef typename TV::SCALAR T;
-    typedef VECTOR<int,TV::m> TV_INT;
+    typedef VECTOR<int,3> TV_INT;
+    typedef VECTOR<T,3> TV;
     T density=1;
     RIGID_BODY_STATE<TV> initial_state;
     std::string data_directory="../../Public_Data";
@@ -226,8 +243,27 @@ void Setup(SOLID_BODY_COLLECTION<TV>& sbc,EXPLICIT_EXAMPLE<TV>& example,SOLIDS_S
             LOG::printf("Min edge: %f\n",Min_Edge(st,sbc.deformable_body_collection.particles.X));}
         break;
     case 1:{
-            GRID<TV> mattress_grid(TV_INT()+8,RANGE<TV>(TV(),TV()+1),true);
+            GRID<TV> mattress_grid(TV_INT()+example.resolution,RANGE<TV>(TV(),TV()+1),true);
             TETRAHEDRALIZED_VOLUME<T>& st=tests.Create_Mattress(mattress_grid,true,&initial_state,density);
+            Add_Constitutive_Model(sbc,example,st);
+            sbc.deformable_body_collection.particles.X.template Project<T,&TV::y>()*=0.9;
+            LOG::printf("Min edge: %f\n",Min_Edge(st,sbc.deformable_body_collection.particles.X));}
+        break;
+    default:PHYSBAM_FATAL_ERROR();}
+}
+template<class T>
+void Setup(SOLID_BODY_COLLECTION<VECTOR<T,2> >& sbc,EXPLICIT_EXAMPLE<VECTOR<T,2> >& example,
+    SOLIDS_STANDARD_TESTS<VECTOR<T,2> >& tests,int test_case)
+{
+    typedef VECTOR<int,2> TV_INT;
+    typedef VECTOR<T,2> TV;
+    T density=1;
+    RIGID_BODY_STATE<TV> initial_state;
+    std::string data_directory="../../Public_Data";
+    switch(test_case){
+    case 1:{
+            GRID<TV> mattress_grid(TV_INT()+example.resolution,RANGE<TV>(TV(),TV()+1),true);
+            TRIANGULATED_AREA<T>& st=tests.Create_Mattress(mattress_grid,true,&initial_state,density);
             Add_Constitutive_Model(sbc,example,st);
             sbc.deformable_body_collection.particles.X.template Project<T,&TV::y>()*=0.9;
             LOG::printf("Min edge: %f\n",Min_Edge(st,sbc.deformable_body_collection.particles.X));}
@@ -244,11 +280,16 @@ void Run(PARSE_ARGS& parse_args,STREAM_TYPE stream_type,const std::string& outpu
     EXPLICIT_EXAMPLE<TV> example;
     example.out_dir=output_dir;
     int test_number;
+    bool write_substep=false;
     parse_args.Add("-test_matrix",&example.test_matrix,"Test matrix");
     parse_args.Add("-dump_matrix",&example.dump_matrix,"Dump transit matrix");
+    parse_args.Add("-substep",&write_substep,"Write substep");
+    parse_args.Add("-dump_c",&example.dump_sound_speed,"Dump sound speed");
+    parse_args.Add("-compute_max_c",&example.compute_max_sound_speed,"Compute sound speed");
     parse_args.Add("-frame_dt",&example.frame_dt,"rate","Number of frames per second");
     parse_args.Add("-dt",&example.default_dt,"time","dt");
     parse_args.Add("-last_frame",&example.last_frame,"frame","number of frames to simulate");
+    parse_args.Add("-resolution",&example.resolution,"resolution","resolution");
     parse_args.Add("-a",&example.alpha,"alpha","alpha");
     parse_args.Add("-damping",&example.damping,"damping","damping");
     parse_args.Add("-stiffness",&example.stiffness,"stiffness","stiffness");
@@ -257,7 +298,8 @@ void Run(PARSE_ARGS& parse_args,STREAM_TYPE stream_type,const std::string& outpu
     parse_args.Parse();
 
     std::string data_directory="../../Public_Data";
-    VIEWER_OUTPUT<TV> vo(stream_type,GRID<TV>(),output_dir);
+    GRID<TV> grid;
+    VIEWER_OUTPUT<TV> vo(stream_type,grid,output_dir);
     SOLID_BODY_COLLECTION<TV> sbc;
     SOLIDS_STANDARD_TESTS<TV> tests(stream_type,data_directory,sbc);
     Setup(sbc,example,tests,test_number);
@@ -276,6 +318,9 @@ void Run(PARSE_ARGS& parse_args,STREAM_TYPE stream_type,const std::string& outpu
                 example.dt=frame_end-example.time;
                 end_time=frame_end;}
             Step(sbc,example);
+            if(write_substep){
+                Flush_Frame<TV>("Substep");
+                sbc.Write(stream_type,output_dir,vo.frame-1,0,true,true,true,true,true);}
             example.time=end_time;
             example.step++;}
         Flush_Frame<TV>("Frame");
@@ -284,13 +329,14 @@ void Run(PARSE_ARGS& parse_args,STREAM_TYPE stream_type,const std::string& outpu
 
 int main(int argc, char* argv[])
 {
-    bool type_double=true;
+    bool type_double=true,use_3d=false;
     std::string output_dir="output";
     PROCESS_UTILITIES::Set_Floating_Point_Exception_Handling(true);
 
     PARSE_ARGS parse_args(argc,argv);
     parse_args.Add("-o",&output_dir,"dir","Output directory");
     parse_args.Add_Not("-float",&type_double,"Use floats");
+    parse_args.Add("-3d",&use_3d,"Run 3D examples");
     parse_args.Parse(true);
 
     Create_Directory(output_dir+"/common");
@@ -298,10 +344,12 @@ int main(int argc, char* argv[])
     LOG::printf("%s\n",parse_args.Print_Arguments());
     if(type_double){
         STREAM_TYPE stream_type((double()));
-        Run<VECTOR<double,3> >(parse_args,stream_type,output_dir);}
+        if(use_3d) Run<VECTOR<double,3> >(parse_args,stream_type,output_dir);
+        else Run<VECTOR<double,2> >(parse_args,stream_type,output_dir);}
     else{
         STREAM_TYPE stream_type((float()));
-        Run<VECTOR<float,3> >(parse_args,stream_type,output_dir);}
+        if(use_3d) Run<VECTOR<float,3> >(parse_args,stream_type,output_dir);
+        else Run<VECTOR<float,2> >(parse_args,stream_type,output_dir);}
     LOG::Finish_Logging();
     return 0;
 }
