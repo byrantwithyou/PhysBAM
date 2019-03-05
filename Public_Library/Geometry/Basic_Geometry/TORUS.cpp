be7f5101fa02ebac6676eabe6dd0e912c08ce4a7
Craig Schroeder <snubdodecahedron@gmail.com>
2019-03-05 12:19:09 -0800

    Compute Hessian explicitly

--- Public_Library/Geometry/Basic_Geometry/TORUS.cpp
+++ Public_Library/Geometry/Basic_Geometry/TORUS.cpp
@@ -2,22 +2,63 @@
 // Copyright 2006-2007, Geoffrey Irving, Craig Schroeder, Tamar Shinar.
 // This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
 //#####################################################################
-#include <Tools/Auto_Diff/AUTO_HESS_EXT.h>
 #include <Geometry/Basic_Geometry/TORUS.h>
 namespace PhysBAM{
+//#####################################################################
+// Function Signed_Distance
+//#####################################################################
+template<class T> T TORUS<T>::
+Signed_Distance(const TV& X) const
+{
+    TV x=X-center;
+    T axial=Dot_Product(x,axis);
+    T radial=(x-axial*axis).Magnitude();
+    return sqrt(sqr(radial-outer_radius)+sqr(axial))-inner_radius;
+}
+//#####################################################################
+// Function Normal
+//#####################################################################
+template<class T> VECTOR<T,3> TORUS<T>::
+Normal(const TV& X) const
+{
+    TV x=X-center;
+    T axial=Dot_Product(x,axis);
+    TV inplane=x-axial*axis;
+    VECTOR<T,2> plane_normal(axial,inplane.Normalize()-outer_radius);
+    plane_normal.Normalize();
+    return plane_normal.x*axis+plane_normal.y*inplane;
+}
 //#####################################################################
 // Function Hessian
 //#####################################################################
 template<class T> SYMMETRIC_MATRIX<T,3> TORUS<T>::
 Hessian(const TV& X) const
 {
-    typedef DIFF_LAYOUT<T,TV::m> LAYOUT;
-    auto x=Hess_From_Var<LAYOUT,0>(X)-center;
-    auto axial=x.Dot(axis);
-    auto ret=hypot((x-axial*axis).Magnitude()-outer_radius,axial)-inner_radius;
-    SYMMETRIC_MATRIX<T,3> sm;
-    Get<0,0>(sm,ret.ddx);
-    return sm;
+    TV Y=X-center;
+    T a=Y.Dot(axis);
+    TV dw=Y-a*axis;
+    T w=dw.Normalize();
+    T z=w-outer_radius,p=hypot(z,a),e=z/p;
+    return e/w*((T)1-Outer_Product(axis)-Outer_Product(dw))+Outer_Product(a/p*dw-e*axis)/p;
+}
+//#####################################################################
+// Function Principal_Curvatures
+//#####################################################################
+template<class T> VECTOR<T,2> TORUS<T>::
+Principal_Curvatures(const TV& X) const
+{
+    T inplane=(X-TV::Dot_Product(X,axis)*axis).Magnitude();
+    return VECTOR<T,2>(-1/inner_radius,-(inplane-outer_radius)/(inplane*inner_radius));
+}
+//#####################################################################
+// Function Bounding_Box
+//#####################################################################
+template<class T> RANGE<VECTOR<T,3> > TORUS<T>::
+Bounding_Box() const
+{
+    T x=sqr(axis.x),y=sqr(axis.y),z=sqr(axis.z);
+    TV corner_offset=sqrt(TV(y+z,x+z,x+y))*outer_radius+inner_radius;
+    return RANGE<TV>(center-corner_offset,center+corner_offset);
 }
 template class TORUS<float>;
 template class TORUS<double>;
--- Public_Library/Geometry/Basic_Geometry/TORUS.h
+++ Public_Library/Geometry/Basic_Geometry/TORUS.h
@@ -28,23 +28,11 @@ public:
         :axis(TV(0,0,1)),inner_radius(1),outer_radius(2)
     {}
 
-    TORUS(const TV& center_input,const TV& axis_input,const T inner_radius_input,const T outer_radius_input)
-        :center(center_input),axis(axis_input.Normalized()),inner_radius(inner_radius_input),outer_radius(outer_radius_input)
+    TORUS(const TV& center_input,const TV& axis_input,const T inner_radius_input,
+        const T outer_radius_input)
+        :center(center_input),axis(axis_input.Normalized()),
+        inner_radius(inner_radius_input),outer_radius(outer_radius_input)
     {}
-
-    T Signed_Distance(const TV& X) const
-    {TV x=X-center;T axial=Dot_Product(x,axis),radial=(x-axial*axis).Magnitude();return sqrt(sqr(radial-outer_radius)+sqr(axial))-inner_radius;}
-
-    RANGE<TV> Bounding_Box() const
-    {T x=sqr(axis.x),y=sqr(axis.y),z=sqr(axis.z);TV corner_offset=sqrt(TV(y+z,x+z,x+y))*outer_radius+inner_radius;
-    return RANGE<TV>(center-corner_offset,center+corner_offset);}
-
-    TV Normal(const TV& X) const
-    {TV x=X-center;T axial=Dot_Product(x,axis);TV inplane=x-axial*axis;VECTOR<T,2> plane_normal(axial,inplane.Normalize()-outer_radius);plane_normal.Normalize();
-    return plane_normal.x*axis+plane_normal.y*inplane;}
-
-    VECTOR<T,2> Principal_Curvatures(const TV& X) const
-    {T inplane=(X-TV::Dot_Product(X,axis)*axis).Magnitude();return VECTOR<T,2>(-1/inner_radius,-(inplane-outer_radius)/(inplane*inner_radius));}
 
     static std::string Name()
     {return "TORUS<T>";}
@@ -57,7 +45,11 @@ public:
     void Write(std::ostream& output) const
     {Write_Binary<RW>(output,center,axis,inner_radius,outer_radius);}
 
+    T Signed_Distance(const TV& X) const;
+    TV Normal(const TV& X) const;
     SYMMETRIC_MATRIX<T,3> Hessian(const TV& X) const PHYSBAM_FLATTEN;
+    VECTOR<T,2> Principal_Curvatures(const TV& X) const;
+    RANGE<TV> Bounding_Box() const;
 //#####################################################################
 };   
 }
