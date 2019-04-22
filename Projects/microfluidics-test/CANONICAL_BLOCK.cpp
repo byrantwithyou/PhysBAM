82b113384695c934681cbcf2c9e8cd18b240d555
Ounan Ding <oding001@ucr.edu>
2019-04-22 14:00:37 -0700

    fix element_edges.

--- Projects/microfluidics-test/CANONICAL_BLOCK.cpp
+++ Projects/microfluidics-test/CANONICAL_BLOCK.cpp
@@ -16,9 +16,9 @@ template<class T> void CANONICAL_BLOCK<T>::Compute_Element_Edges()
         h.Set(S(i),{i,0});
         h.Set(S(i).Reversed(),{i,1});
     }
-    for(int i=0;i<S.m;i++)
+    for(int i=0;i<E.m;i++)
         for(int j=0;j<3;j++)
-            element_edges(i)(j)=h.Get({(j+1)%3,(j+2)%3});
+            element_edges(i)(j)=h.Get({E(i)((j+1)%3),E(i)((j+2)%3)});
 }
 template class CANONICAL_BLOCK<double>;
 }
