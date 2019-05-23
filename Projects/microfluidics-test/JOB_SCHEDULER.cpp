b652768c4967d93d6d648ffe3e324c3e1657d367
Ounan Ding <oding001@ucr.edu>
2019-05-22 17:29:08 -0700

    implement work stealing

--- Projects/microfluidics-test/JOB_SCHEDULER.cpp
+++ Projects/microfluidics-test/JOB_SCHEDULER.cpp
@@ -13,7 +13,67 @@
 namespace PhysBAM{
 bool use_job_timing;
 
-void Thread_Function(JOB_SCHEDULER_CORE* s, int tid)
+template<class T,class P> bool PRIORITY_QUEUE<T,P>::
+Pop(T& data)
+{
+    std::unique_lock<std::mutex> lck(mtx);
+    if(!a.m) return false;
+    data=a(0).x;
+    std::pop_heap(a.begin(),a.end(),cmp);
+    a.Pop();
+    return true;
+}
+template<class T,class P> int PRIORITY_QUEUE<T,P>::
+Push(const T& data,P priority)
+{
+    std::unique_lock<std::mutex> lck(mtx);
+    a.Append({data,priority});
+    std::push_heap(a.begin(),a.end(),cmp);
+    return a.m;
+}
+
+int THREAD_STATE::Get_Next_Job()
+{
+    int job=-1;
+    if(priority_queue.Pop(job)) return job;
+
+    while(1)
+    {
+        for(int i=0;i<core->threads.m;i++)
+        {
+            int t=(i+tid)%core->threads.m;
+            if(core->threads(t)->priority_queue.Pop(job)) return job;
+        }
+
+        std::unique_lock<std::mutex> lck(core->mtx);
+        if(++core->waiting==core->threads.m)
+        {
+            core->done.store(true);
+            core->cv.notify_all();
+            return -1;
+        }
+        core->cv.wait(lck);
+        int w=--core->waiting;
+        PHYSBAM_ASSERT(w>=0);
+        if(core->done.load()) return -1;
+    }
+}
+
+void THREAD_STATE::Release_Job(int job)
+{
+    int n=priority_queue.Push(job,core->jobs(job)->priority);
+    if(n>1 && core->waiting.load())
+        core->cv.notify_one();
+}
+
+void THREAD_STATE::Release_Dependencies(int job)
+{
+    for(auto a:core->jobs(job)->release_list)
+        if(!--core->jobs(a)->missing_deps)
+            Release_Job(a);
+}
+
+void Thread_Function(THREAD_STATE* ts)
 {
     auto dur=[](auto a,auto b){return std::chrono::duration_cast<std::chrono::duration<double> >(b-a).count();};
     bool use_timing=use_job_timing;
@@ -26,102 +86,77 @@ void Thread_Function(JOB_SCHEDULER_CORE* s, int tid)
     std::chrono::steady_clock::time_point t0,t1;
     if(use_timing) t0=std::chrono::steady_clock::now();
     
-    int job=-1;
-    
     while(1)
     {
-        job=s->Finish_Job(job);
+        int job=ts->Get_Next_Job();
         if(job<0) break;
 
         std::chrono::steady_clock::time_point ta;
         if(use_timing) ta=std::chrono::steady_clock::now();
 
-        s->execute_job(s->jobs(job).job,s->data);
+        ts->core->execute_job(ts->core->jobs(job)->job,ts->core->data);
+
         if(use_timing)
         {
             std::chrono::steady_clock::time_point tb=std::chrono::steady_clock::now();
             timing_data.Append({dur(t0,ta),dur(ta,tb),job});
             t0=tb;
         }
+
+        ts->Release_Dependencies(job);
     }
+
     if(use_timing)
     {
         t1=std::chrono::steady_clock::now();
-        std::ofstream fout(LOG::sprintf("timing-%i.txt",tid));
+        std::ofstream fout(LOG::sprintf("timing-%i.txt",ts->tid));
         for(auto d:timing_data) LOG::fprintf(fout,"%i %.1f %.1f\n",d.job,d.t0*1e6,d.t1*1e6);
         LOG::fprintf(fout,"%.1f\n",dur(t0,t1)*1e6);
     }
 
 }
 
-int JOB_SCHEDULER_CORE::Finish_Job(int job)
-{
-    std::unique_lock<std::mutex> lck(mtx);
-    if(job>=0){
-        if(!--jobs_left){
-            cv.notify_all();
-            return -1;}
-        int best_priority=INT_MIN;
-        int best_job=-1;
-        if(priority_queue.m){
-            best_job=-1;
-            best_priority=jobs(priority_queue(0)).priority;}
-
-        for(auto a:jobs(job).release_list){
-            if(--jobs(a).missing_deps>0) continue;
-            if(jobs(a).priority>=best_priority){
-                best_priority=jobs(a).priority;
-                std::swap(best_job,a);
-                if(a<0) continue;}
-            priority_queue.Append(a);
-            std::push_heap(priority_queue.begin(),priority_queue.end(),
-                [this](int r,int s){return jobs(r).priority<jobs(s).priority;});
-            cv.notify_one();}
-
-        if(best_job>=0) return best_job;}
-
-    while(!priority_queue.m && jobs_left) cv.wait(lck);
-    if(!jobs_left) return -1;
-    int j=priority_queue(0);
-    std::pop_heap(priority_queue.begin(),priority_queue.end(),[this](int r,int s){return jobs(r).priority<jobs(s).priority;});
-    priority_queue.Pop();
-    return j;
-}
-
 void JOB_SCHEDULER_CORE::Execute_Jobs(int num_threads)
 {
     if(num_threads<0) num_threads=std::thread::hardware_concurrency();
-    printf("threads: %i\n",num_threads);
-    jobs_left=jobs.m;
-    for(int i=0;i<jobs.m;i++)
-        if(!jobs(i).missing_deps)
-            priority_queue.Append(i);
-    
-    std::make_heap(priority_queue.begin(),priority_queue.end(),
-        [this](int r,int s){return jobs(r).priority<jobs(s).priority;});
+    LOG::printf("threads: %i\n",num_threads);
+    LOG::printf("jobs: %i\n",jobs.m);
 
-    ARRAY<std::thread*> threads;
-    for(int i=0;i<num_threads;i++) threads.Append(new std::thread(Thread_Function,this,i));
-    
-    for(int i=0;i<threads.m;i++){
-        threads(i)->join();
-        delete threads(i);}
+    threads.Resize(num_threads);
+    for(int i=0;i<num_threads;i++)
+    {
+        threads(i)=new THREAD_STATE;
+        threads(i)->core=this;
+        threads(i)->tid=i;
+    }
+
+    for(int i=0,k=0;i<jobs.m;i++)
+        if(!jobs(i)->missing_deps)
+            threads(k++%num_threads)->Release_Job(i);
+
+    for(auto& t:threads) t->th=new std::thread(Thread_Function,t);
+
+    for(auto& t:threads)
+    {
+        t->th->join();
+        delete t->th;
+    }
 }
 
 int JOB_SCHEDULER_CORE::Compute_Priority_By_Paths_Rec(int j)
 {
-    if(jobs(j).priority>=0)
-        return jobs(j).priority;
+    if(jobs(j)->priority>=0)
+        return jobs(j)->priority;
     int p=0;
-    for(auto k:jobs(j).release_list)
+    for(auto k:jobs(j)->release_list)
         p=std::max(p,Compute_Priority_By_Paths_Rec(k));
-    jobs(j).priority=p;
+    jobs(j)->priority=p;
     return p;
 }
 
 void JOB_SCHEDULER_CORE::Compute_Priority_By_Paths()
 {
-    for(auto& j:jobs) j.priority=-1;
+    for(auto& j:jobs) j->priority=-1;
      for(int i=jobs.m-1;i>=0;i--)
         Compute_Priority_By_Paths_Rec(i);
 }
--- Projects/microfluidics-test/JOB_SCHEDULER.h
+++ Projects/microfluidics-test/JOB_SCHEDULER.h
@@ -5,41 +5,89 @@
 #ifndef __JOB_SCHEDULER__
 #define __JOB_SCHEDULER__
 #include <Core/Arrays/ARRAY.h>
-#include <mutex>
+#include <Core/Data_Structures/PAIR.h>
+#include <atomic>
 #include <condition_variable>
+#include <mutex>
+#include <thread>
 
 namespace PhysBAM{
 
+struct JOB_SCHEDULER_CORE;
+
+struct alignas(64) JOB_INFO
+{
+    void* job;
+    int priority;
+    std::atomic<int> missing_deps;
+    ARRAY<int> release_list;
+
+    JOB_INFO(void* job,int priority)
+        :job(job),priority(priority)
+    {
+        missing_deps.store(0);
+    }
+};
+
+template<class T,class P>
+struct PRIORITY_QUEUE
+{
+    ARRAY<PAIR<T,P> > a;
+    std::mutex mtx;
+    static bool cmp(const PAIR<T,P>& r,const PAIR<T,P>& s){return r.y<s.y;}
+    
+    bool Pop(T& data);
+    int Push(const T& data,P priority);
+};
+
+struct THREAD_STATE
+{
+    JOB_SCHEDULER_CORE * core=0;
+
+    PRIORITY_QUEUE<int,int> priority_queue;
+    std::thread* th=0;
+    int tid=-1;
+
+    int Get_Next_Job();
+    void Release_Dependencies(int job);
+    void Release_Job(int job);
+};
+
 struct JOB_SCHEDULER_CORE
 {
-    struct JOB_INFO
-    {
-        void* job;
-        int priority;
-        int missing_deps;
-        ARRAY<int> release_list;
-    };
-    ARRAY<JOB_INFO> jobs;
-    ARRAY<int> priority_queue;
+    ARRAY<THREAD_STATE*> threads;
+    ARRAY<JOB_INFO*> jobs;
+
+    std::mutex mtx;
+    std::condition_variable cv;
+    std::atomic<int> waiting;
+    std::atomic<bool> done;
 
     void (*execute_job)(void* job,void* data);
     void* data;
 
-    std::mutex mtx;
-    std::condition_variable cv;
-    int jobs_left;
+    JOB_SCHEDULER_CORE()
+    {
+        waiting.store(0);
+        done.store(false);
+    }
+
+    ~JOB_SCHEDULER_CORE()
+    {
+        jobs.Delete_Pointers_And_Clean_Memory();
+        threads.Delete_Pointers_And_Clean_Memory();
+    }
     
     int Add_Job(void* job,int priority)
-    {return jobs.Append({job,priority,0});}
+    {return jobs.Append(new JOB_INFO(job,priority));}
 
     // user depends on dep.
     void Register_Dependency(int dep,int user)
     {
-        jobs(dep).release_list.Append(user);
-        jobs(user).missing_deps++;
+        jobs(dep)->release_list.Append(user);
+        jobs(user)->missing_deps++;
     }
 
-    int Finish_Job(int job);
     void Execute_Jobs(int num_threads);
     void Compute_Priority_By_Paths();
     int Compute_Priority_By_Paths_Rec(int j);
