5ee3f2e592730ddfffb9f5ec28b50b19c39e131d
Ounan Ding <oding001@ucr.edu>
2019-06-12 18:54:58 -0700

    self convergence test 2d

---
+++ Projects/microfluidics-test/2019-jcp-cem-scripts/run_self_conv_2d.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+NAME=self-conv-2d
+
+export OPENBLAS_NUM_THREADS=1
+ARGS="../fem -q -threads 16"
+FULL=1 # Set to 1 for a full rebuild; 0 to skip rerunning the simulations
+
+tests=(comp-0 comp-1 simple grid20 rgrid0 rgrid1 voronoi-s4 voronoi-s15)
+LO=2
+HI=15
+SOL=16
+
+if [ "X$FULL" = "X1" ] ; then
+    rm -rf $NAME
+    mkdir -p $NAME
+    for c in ${tests[@]} ; do
+        $ARGS -o $NAME/$c-r$SOL -refine $SOL -dump_sol ../$c.txt > /dev/null
+    done
+    for c in ${tests[@]} ; do
+        for r in `seq $HI -1 $LO` ; do
+            echo $ARGS -o $NAME/$c-r$r -refine $r -check_sol $NAME/$c-r$SOL/sol.gz ../$c.txt;
+        done
+    done | xargs -P 4 -n 1 -d '\n' bash -c > /dev/null
+fi
+
+names=("" "" "ul-inf" "ul-2" "pl-inf" "pl-2")
+for c in ${tests[@]} ; do
+    cat <<EOF > $NAME/$c.txt
+EOF
+    for r in `seq $LO 1 $HI` ; do
+        grep 'l-2' $NAME/$c-r$r/common/log.txt |\
+            sed "s/.*l-inf \([^ ]*\).*l-2 \([^ ]*\).*l-inf \([^ ]*\).*l-2 \([^<]*\).*/$r \1 \2 \3 \4/g" >> $NAME/$c.txt
+    done
+
+    for i in 2 3 4 5 ; do
+        gnuplot -e 'set terminal pdf' \
+            -e "set output \"$NAME/$c-${names[$i]}.pdf\"" \
+            -e "fit a*x+b \"$NAME/$c.txt\" u (log10(\$1)):(log10(\$$i)) via a,b" \
+            -e "plot \"$NAME/$c.txt\" u (log10(\$1)):(log10(\$$i)),a*x+b title sprintf(\"%.2f*x+%.2f\",a,b)" \
+            2>/dev/null
+    done
+done
