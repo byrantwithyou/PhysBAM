4f9b9e41d8559726deca9e67e052bef0e79143a1
Ounan Ding <oding001@ucr.edu>
2019-06-24 07:56:46 -0700

    3D analytic convergence tests

--- Projects/microfluidics-test/2019-jcp-cem-scripts/run_conv_3d.sh
+++ Projects/microfluidics-test/2019-jcp-cem-scripts/run_conv_3d.sh
@@ -3,13 +3,13 @@
 NAME=conv-3d
 
 export OPENBLAS_NUM_THREADS=1
-ARGS="../fem -3d -q -threads 12"
+ARGS="../fem -3d -q -threads 8"
 FULL=1 # Set to 1 for a full rebuild; 0 to skip rerunning the simulations
 
-tests=(grid2 comp-1 comp-2 comp-3 comp-13 comp-14)
+tests=(simple grid20 rgrid0 rgrid1 voronoi-s4 voronoi-s15)
 LO=2
-HI=6
-ANA="-u 'u=sin(x)*y+cos(y)*z+x*y,v=cos(x*y)+sin(y)*x+x*x+y*z-1,w=sin(z)*y+cos(x)*z' -p 'p=sin(x+y+1)+cos(z)'"
+HI=5
+ANA="-u 'u=sin(x)*y+cos(y)*z+x*y,v=cos(x)*cos(y)+sin(y)*x+x*x+y*z-1,w=sin(z)*y+cos(x)*z' -p 'p=sin(x+y+1)+cos(z)'"
 
 if [ "X$FULL" = "X1" ] ; then
     rm -rf $NAME
@@ -21,20 +21,45 @@ if [ "X$FULL" = "X1" ] ; then
     done | xargs -P 1 -n 1 -d '\n' bash -c > /dev/null
 fi
 
-names=("" "" "ul-inf" "ul-2" "pl-inf" "pl-2")
 for c in ${tests[@]} ; do
-    cat <<EOF > $NAME/$c.txt
+    cat <<EOF > $NAME/$c-p.txt
+res linf l2
+EOF
+    cat <<EOF > $NAME/$c-v.txt
+res linf l2
 EOF
     for r in `seq $LO 1 $HI` ; do
+        res=`bc <<< "2*$r"`
         grep 'l-2' $NAME/$c-r$r/common/log.txt |\
-            sed "s/.*l-inf \([^ ]*\).*l-2 \([^ ]*\).*l-inf \([^ ]*\).*l-2 \([^<]*\).*/$r \1 \2 \3 \4/g" >> $NAME/$c.txt
+            sed "s/.*l-inf \([^ ]*\).*l-2 \([^ ]*\).*l-inf \([^ ]*\).*l-2 \([^<]*\).*/$res \3 \4/g" >> $NAME/$c-p.txt
+        grep 'l-2' $NAME/$c-r$r/common/log.txt |\
+            sed "s/.*l-inf \([^ ]*\).*l-2 \([^ ]*\).*l-inf \([^ ]*\).*l-2 \([^<]*\).*/$res \1 \2/g" >> $NAME/$c-v.txt
     done
 
-    for i in 2 3 4 5 ; do
-        gnuplot -e 'set terminal pdf' \
-            -e "set output \"$NAME/$c-${names[$i]}.pdf\"" \
-            -e "fit a*x+b \"$NAME/$c.txt\" u (log10(\$1)):(log10(\$$i)) via a,b" \
-            -e "plot \"$NAME/$c.txt\" u (log10(\$1)):(log10(\$$i)),a*x+b title sprintf(\"%.2f*x+%.2f\",a,b)" \
-            2>/dev/null
-    done
+    sed -e 's/LLLL/p/g' -e "s/XXXX/$c-p/g" -e "s/TTTT/$c pressure/g" conv_plot.tex  > $NAME/plot-$c-p.tex
+    sed -e 's/LLLL/\\mathbf{v}/g' -e "s/XXXX/$c-v/g" -e "s/TTTT/$c velocity/g" conv_plot.tex  > $NAME/plot-$c-v.tex
+
+    V=`./conv_regression.pl $NAME/$c-v.txt $NAME/$c-v-regres.txt`
+    read ova ovb <<< "$V"
+    sed -i -e "s/IIII/$ova/g" -e "s/EEEE/$ovb/g" $NAME/plot-$c-v.tex
+
+    P=`./conv_regression.pl $NAME/$c-p.txt $NAME/$c-p-regres.txt`
+    read opa opb <<< "$P"
+    sed -i -e "s/IIII/$opa/g" -e "s/EEEE/$opb/g" $NAME/plot-$c-p.tex
 done
+
+cat <<EOF > $NAME/SConstruct
+import os
+import re
+env=Environment(ENV = os.environ)
+env['PSSUFFIX']=".eps"
+r=re.compile(".*\.tex$")
+for f in [x for x in os.listdir(".") if r.match(x)]:
+    t=env.DVI(f)
+    env.PostScript(t)
+    env.PDF(t)
+EOF
+(
+    cd $NAME
+    scons
+)
