dae1edf64761c925819221bda208ccfce604d742
Ounan Ding <oding001@ucr.edu>
2019-08-09 14:35:34 -0700

    fix umfpack

--- Projects/microfluidics-test/umfpack/ldivide.cpp
+++ Projects/microfluidics-test/umfpack/ldivide.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <cstdio>
 #include <cmath>
 #include <chrono>
@@ -17,6 +18,42 @@ void check(const vector<T>& x,const vector<T>& ref)
     for(int i=0;i<x.size();i++)
         err_linf=max(err_linf,fabs(x[i]-ref[i]));
     printf("error %e\n",err_linf);
+}
+
+vector<T> residue(int dim,const vector<int>& offsets,const vector<int>& col,const vector<T>& entries,
+    const vector<T>& x,const vector<T>& b)
+{
+    vector<T> result(dim);
+    int index=offsets[0];
+    for(int i=0;i<dim;i++){
+        int end=offsets[i+1];
+        T sum=(T)0;
+        for(;index<end;index++)
+            sum+=entries[index]*x[col[index]];
+        result[i]=sum-b[i];
+    }
+    return result;
+}
+
+T l2(const vector<T>& x)
+{
+    T s=0;
+    for(const auto& a:x)
+    {
+        s+=a*a;
+    }
+    s/=x.size();
+    return sqrt(s);
+}
+
+T linf(const vector<T>& x)
+{
+    T s=abs(x[0]);
+    for(const auto& a:x)
+    {
+        s=max(s,abs(a));
+    }
+    return s;
 }
 
 // ./prog <output_dir>
@@ -58,21 +95,49 @@ int main(int argc,char* argv[])
     fread(&entries[0],sizeof(T),nnz,file);
     fclose(file);
     printf("name %s dim %d nnz %d threads %d\n",dir.c_str(),dim,nnz,threads);
+
+    vector<long int> col_l,offsets_l;
+    col_l.resize(col.size());
+    offsets_l.resize(offsets.size());
+    for(int i=0;i<col.size();i++) col_l[i]=(long int)col[i];
+    for(int i=0;i<offsets.size();i++) offsets_l[i]=(long int)offsets[i];
     timer("import");
 
+    double ctrl[UMFPACK_CONTROL];
+    double info[UMFPACK_INFO];
+    umfpack_dl_defaults(ctrl);
+    //ctrl[UMFPACK_STRATEGY]=UMFPACK_STRATEGY_SYMMETRIC;
+    //ctrl[UMFPACK_PIVOT_TOLERANCE]=0.1;
     void* symbolic;
-    umfpack_di_symbolic(dim,dim,&offsets[0],&col[0],&entries[0],&symbolic,0,0);
+    long int ret;
+    ret=umfpack_dl_symbolic((long)dim,(long)dim,&offsets_l[0],&col_l[0],&entries[0],&symbolic,ctrl,info);
+    //printf("symbolic %ld\n",ret);
+    assert(ret==UMFPACK_OK);
     void* numeric;
-    umfpack_di_numeric(&offsets[0],&col[0],&entries[0],symbolic,&numeric,0,0);
+    ret=umfpack_dl_numeric(&offsets_l[0],&col_l[0],&entries[0],symbolic,&numeric,ctrl,info);
+    //printf("numeric %ld\n",ret);
+    assert(ret==UMFPACK_OK);
     vector<T> x(dim);
-    umfpack_di_solve(UMFPACK_A,&offsets[0],&col[0],&entries[0],&x[0],&rhs[0],numeric,0,0);
+    ret=umfpack_dl_solve(UMFPACK_A,&offsets_l[0],&col_l[0],&entries[0],&x[0],&rhs[0],numeric,ctrl,info);
+    //printf("ret %ld\n",ret);
+    assert(ret==UMFPACK_OK);
     timer("solve");
 
+    printf("INFO NZ %.16f\n",info[UMFPACK_NZ]);
+    printf("INFO RCOND %.16f\n",info[UMFPACK_RCOND]);
+    printf("INFO RSMIN %.16f\n",info[UMFPACK_RSMIN]);
+    printf("INFO RSMAX %.16f\n",info[UMFPACK_RSMAX]);
+    printf("INFO NOFF_DIAG %.16f\n",info[UMFPACK_NOFF_DIAG]);
+    printf("INFO NZDROPPED %.16f\n",info[UMFPACK_NZDROPPED]);
+    timer("info");
+
     check(x,ref_sol);
+    auto res=residue(dim,offsets,col,entries,x,rhs);
+    printf("residue linf %e l2 %e\n",linf(res),l2(res));
     timer("check");
 
-    umfpack_di_free_symbolic(&symbolic);
-    umfpack_di_free_numeric(&numeric);
+    umfpack_dl_free_symbolic(&symbolic);
+    umfpack_dl_free_numeric(&numeric);
 
     for(int i=1;i<tm.size();i++)
         printf("%20s %5.0f ms\n",tm[i].second,
