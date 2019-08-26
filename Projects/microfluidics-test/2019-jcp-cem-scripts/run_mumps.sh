73f3d207d227d1ac3ae632d2c3eb3961ababde9a
Ounan Ding <oding001@ucr.edu>
2019-08-26 10:33:50 -0700

    test our method without caching

--- Projects/microfluidics-test/2019-jcp-cem-scripts/run_mumps.sh
+++ Projects/microfluidics-test/2019-jcp-cem-scripts/run_mumps.sh
@@ -38,6 +38,20 @@ if [ "X$FULL" = "X1" ] ; then
     for th in 1 2 4 8 16 ; do
         for p in `seq 0 3` ; do
             c=${tests[$p]}
+            r=${res2[$p]}
+            $ARGS -o $NAME/$c-nc-t$th -refine $r -threads $th -force_blk_ref ../$c.txt;
+        done
+
+        for p in `seq 0 3` ; do
+            c=${tests[$p]}
+            r=${res3[$p]}
+            $ARGS -3d -o $NAME/$c-3d-nc-t$th -refine $r -threads $th -force_blk_ref ../$c.txt;
+        done
+    done
+
+    for th in 1 2 4 8 16 ; do
+        for p in `seq 0 3` ; do
+            c=${tests[$p]}
             mkdir -p $NAME/$c-mumps-t$th/common
             mpirun -n $th $MUMPS $NAME/$c-t1 $th > $NAME/$c-mumps-t$th/common/log.txt
         done
@@ -114,7 +128,15 @@ for th in 1 2 4 8 16 ; do
             exec_jobs=`grep "exec jobs" $NAME/$c$dim-t$th/common/log.txt | sed 's/.*jobs *\(.*\) ms.*/\1/g'`
             sol=`perl -e "{printf('%f',$comp_matrix+$elim_irreg+$elim_non_sep+$elim_3+$back+$exec_jobs);}"`
 
-            echo $name$dim $th $sol $mumps_time $umfpack_time >> $NAME/result.txt
+            nc_comp_matrix=`grep "compute matrix" $NAME/$c$dim-nc-t$th/common/log.txt | sed 's/.*matrix *\(.*\) ms.*/\1/g'`
+            nc_elim_irreg=`grep "elim irreg" $NAME/$c$dim-nc-t$th/common/log.txt | sed 's/.*irreg *\(.*\) ms.*/\1/g'`
+            nc_elim_non_sep=`grep "elim non sep" $NAME/$c$dim-nc-t$th/common/log.txt | sed 's/.*sep *\(.*\) ms.*/\1/g'`
+            nc_elim_3=`grep "elim 3" $NAME/$c$dim-nc-t$th/common/log.txt | sed 's/.*elim 3 *\(.*\) ms.*/\1/g'`
+            nc_back=`grep "back solve" $NAME/$c$dim-nc-t$th/common/log.txt | sed 's/.*solve *\(.*\) ms.*/\1/g'`
+            nc_exec_jobs=`grep "exec jobs" $NAME/$c$dim-nc-t$th/common/log.txt | sed 's/.*jobs *\(.*\) ms.*/\1/g'`
+            nc_sol=`perl -e "{printf('%f',$nc_comp_matrix+$nc_elim_irreg+$nc_elim_non_sep+$nc_elim_3+$nc_back+$nc_exec_jobs);}"`
+
+            echo $name$dim $th $sol $mumps_time $umfpack_time $nc_sol >> $NAME/result.txt
         done
     done
 done
@@ -127,7 +149,9 @@ for i in `seq 0 $((${#tests[@]}-1))` ; do
         grep "$name$dim " $NAME/result.txt | awk '{print $2,$3}' > $NAME/$c$dim.txt
         grep "$name$dim " $NAME/result.txt | awk '{print $2,$4}' > $NAME/$c$dim-mumps.txt
         grep "$name$dim " $NAME/result.txt | awk '{print $2,$5}' > $NAME/$c$dim-umfpack.txt
-        sed -e "s/CCCC/$c$dim/g" -e "s/MMMM/$c$dim-mumps/g" -e "s/FFFF/$c$dim-umfpack/g" -e "s/TTTT/$name$dim/g" \
+        grep "$name$dim " $NAME/result.txt | awk '{print $2,$6}' > $NAME/$c$dim-nc.txt
+        sed -e "s/CCCC/$c$dim/g" -e "s/MMMM/$c$dim-mumps/g" -e "s/FFFF/$c$dim-umfpack/g" -e "s/NNNN/$c$dim-nc/g" \
+            -e "s/TTTT/$name$dim/g" \
             comparison_timing_plot.tex > $NAME/plot-$c$dim.tex
     done
 done
