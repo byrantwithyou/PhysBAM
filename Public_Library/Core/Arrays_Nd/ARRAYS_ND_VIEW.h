f7b4e5ee4d5ffcb6adc438448a26983ff7e9ac1d
Craig Schroeder <snubdodecahedron@gmail.com>
2019-09-11 17:48:49 -0700

    cleanup in ARRAYS_ND_VIEW

--- Public_Library/Core/Arrays_Nd/ARRAYS_ND_VIEW.h
+++ Public_Library/Core/Arrays_Nd/ARRAYS_ND_VIEW.h
@@ -22,10 +22,10 @@ class ARRAY_VIEW<T,VECTOR<int,d> >:public ARRAYS_ND_BASE<typename remove_const<T
     typedef VECTOR<int,d> TV_INT;
     struct UNUSABLE{};
     template<class S> struct COPY_CONST{typedef typename conditional<is_const<T>::value,typename add_const<S>::type,S>::type TYPE;};
-    typedef ARRAYS_ND_BASE<typename remove_const<T>::type,VECTOR<int,d> > BASE;
 public:
     typedef int HAS_UNTYPED_READ_WRITE;
     typedef typename remove_const<T>::type ELEMENT;typedef TV_INT INDEX;
+    typedef ARRAYS_ND_BASE<ELEMENT,VECTOR<int,d> > BASE;
     typedef T& RESULT_TYPE;
 
     using BASE::domain;using BASE::array;
@@ -34,18 +34,18 @@ private:
 
     using BASE::Calculate_Acceleration_Constants;
 
-    void Initialize(typename remove_const<T>::type* raw_data)
-    {ARRAY_VIEW<typename remove_const<T>::type> new_array(raw_data,domain.Size());new_array.Exchange(array);}
+    void Initialize(ELEMENT* raw_data)
+    {ARRAY_VIEW<ELEMENT> new_array(raw_data,domain.Size());new_array.Exchange(array);}
 
 public:
 
-    ARRAY_VIEW(const RANGE<TV_INT>& domain=RANGE<TV_INT>::Empty_Box(),typename remove_const<T>::type* raw_data=0)
+    ARRAY_VIEW(const RANGE<TV_INT>& domain=RANGE<TV_INT>::Empty_Box(),ELEMENT* raw_data=0)
         :BASE(domain)
     {Initialize(raw_data);}
 
-    ARRAY_VIEW(const ARRAY_VIEW<typename remove_const<T>::type,TV_INT>& array_input)
+    ARRAY_VIEW(const ARRAY_VIEW<ELEMENT,TV_INT>& array_input)
         :BASE(array_input.domain)
-    {array.Set((typename remove_const<T>::type*)array_input.array.Get_Array_Pointer(),domain.Size());} // TODO: why do I need this cast?
+    {array.Set((ELEMENT*)array_input.array.Get_Array_Pointer(),domain.Size());} // TODO: why do I need this cast?
 
     template<class T_ARRAY>
     ARRAY_VIEW(T_ARRAY& array_input,typename enable_if<is_same<ELEMENT,typename T_ARRAY::ELEMENT>::value && !IS_ARRAY_VIEW<T_ARRAY>::value,UNUSABLE>::type unusable=UNUSABLE())
@@ -67,10 +67,10 @@ public:
     static void Exchange(ARRAY_VIEW& array0,ARRAY_VIEW& array1)
     {STATIC_ASSERT(!is_const<T>::value);array0.Exchange(array1);}
 
-    void Set(const RANGE<TV_INT>& domain=RANGE<TV_INT>::Empty_Box(),typename remove_const<T>::type* raw_data=0)
+    void Set(const RANGE<TV_INT>& domain=RANGE<TV_INT>::Empty_Box(),ELEMENT* raw_data=0)
     {Calculate_Acceleration_Constants(domain);Initialize(raw_data);}
 
-    void Set(const ARRAY_VIEW<typename remove_const<T>::type,TV_INT>& array)
+    void Set(const ARRAY_VIEW<ELEMENT,TV_INT>& array)
     {Calculate_Acceleration_Constants(array.domain);Initialize(array.base_pointer);}
 
     template<class T_ARRAY>
@@ -80,9 +80,6 @@ public:
     template<class T_ARRAY>
     void Set(T_ARRAY array,typename enable_if<is_same<ELEMENT,typename T_ARRAY::ELEMENT>::value && IS_ARRAY_VIEW<T_ARRAY>::value,UNUSABLE>::type unusable=UNUSABLE())
     {Calculate_Acceleration_Constants(array.Domain_Indices());Initialize(array.Get_Array_Pointer());}
-
-    ARRAY_VIEW<typename remove_const<T>::type>& Const_Cast() const // return reference to allow Exchange
-    {return reinterpret_cast<ARRAY_VIEW<typename remove_const<T>::type>&>(const_cast<ARRAY_VIEW&>(*this));}
 
     static bool Same_Array(const ARRAY_VIEW& array0,const ARRAY_VIEW& array1)
     {return array0.Get_Array_Pointer()==array1.Get_Array_Pointer();}
