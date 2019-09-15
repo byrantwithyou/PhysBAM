6978da395df2deda44ee3d89c09091b85616d106
Craig Schroeder <snubdodecahedron@gmail.com>
2019-09-15 10:55:41 -0700

    Extend interface and avoid data copies.

--- Public_Library/Core/Matrices/LAPACK.h
+++ Public_Library/Core/Matrices/LAPACK.h
@@ -7,17 +7,55 @@
 #ifndef __LAPACK__
 #define __LAPACK__
 
-#include <complex>
 #include <Core/Arrays/ARRAY.h>
 #include <Core/Arrays/ARRAY_VIEW.h>
+#include <Core/Matrices/Make_View.h>
 #include <Core/Matrices/MATRIX_BASE.h>
-#include <cblas.h>
+#include <Core/Matrices/MATRIX_VIEW.h>
+#include <complex>
 #define lapack_complex_double std::complex<double>
 #define lapack_complex_float std::complex<float>
+#include <cblas.h>
 #include <lapacke.h>
 namespace PhysBAM{
 
-// Overloads
+//#####################################################################
+// Forward Declarations
+//#####################################################################
+
+// Least_Squares_Solve_Raw and Least_Squares_Solve work with non-square an singular M.
+
+// Destroys M
+template<class T,class T_MATRIX1,class T_MATRIX2,class T_MATRIX3>
+void Least_Squares_Solve_Raw(MATRIX_BASE<T,T_MATRIX1>& M,
+    const MATRIX_BASE<T,T_MATRIX2>& rhs,
+    MATRIX_BASE<T,T_MATRIX3>& sol,
+    T rcond=(std::numeric_limits<T>::epsilon()), int * rank=0);
+
+// Preserves M
+template<class T,class T_MATRIX1,class T_MATRIX2,class T_MATRIX3>
+void Least_Squares_Solve(const MATRIX_BASE<T,T_MATRIX1>& M,
+    const MATRIX_BASE<T,T_MATRIX2>& rhs,
+    MATRIX_BASE<T,T_MATRIX3>& sol,
+    T rcond=(std::numeric_limits<T>::epsilon()), int * rank=0);
+
+// Destroys M
+template<class T,class T_MATRIX,class T_ARRAY1,class T_ARRAY2>
+void Least_Squares_Solve_Raw(MATRIX_BASE<T,T_MATRIX>& M,
+    const ARRAY_BASE<T,T_ARRAY1>& rhs,
+    ARRAY_BASE<T,T_ARRAY2>& sol,
+    T rcond=(std::numeric_limits<T>::epsilon()), int * rank=0);
+
+// Preserves M
+template<class T,class T_MATRIX,class T_ARRAY1,class T_ARRAY2>
+void Least_Squares_Solve(const MATRIX_BASE<T,T_MATRIX>& M,
+    const ARRAY_BASE<T,T_ARRAY1>& rhs,
+    ARRAY_BASE<T,T_ARRAY2>& sol,
+    T rcond=(std::numeric_limits<T>::epsilon()), int * rank=0);
+
+//#####################################################################
+// LAPACK Templatizations Wrappers
+//#####################################################################
 
 inline int xgelsy(int matrix_layout, int m, int n, int nrhs, double* a,
     int lda, double* b, int ldb, int* jpvt, double rcond, int* rank)
@@ -43,60 +81,95 @@ inline int xgelsy(int matrix_layout, int m, int n, int nrhs, std::complex<float>
     return LAPACKE_cgelsy( matrix_layout, m, n, nrhs, a, lda, b, ldb, jpvt, rcond, rank );
 }
 
-// Uses
+//#####################################################################
+// Definitions
+//#####################################################################
 
+// Destroys M
 template<class T,class T_MATRIX1,class T_MATRIX2,class T_MATRIX3>
-void Least_Squares_Solve(const MATRIX_BASE<T,T_MATRIX1>& M,
-    const MATRIX_BASE<T,T_MATRIX2>& rhs,MATRIX_BASE<T,T_MATRIX3>& sol,
-    T rcond=(std::numeric_limits<T>::epsilon()), int * rank=0)
+void Least_Squares_Solve_Raw(MATRIX_BASE<T,T_MATRIX1>& M,
+    const MATRIX_BASE<T,T_MATRIX2>& rhs,
+    MATRIX_BASE<T,T_MATRIX3>& sol,T rcond, int * rank)
 {
-    int m=M.Rows(),n=M.Columns(),nr=rhs.Columns(),ldb=max(m,n);
-    assert(m==rhs.Rows());
-    assert(n==sol.Rows());
-    assert(nr==sol.Columns());
-    ARRAY<T> A(m*n);
-    ARRAY<T> V(nr*ldb);
-    ARRAY<int> jpvt(n);
-    for(int j=0;j<n;j++)
-        for(int i=0;i<m;i++)
-            A(j*m+i)=M(i,j);
-    for(int j=0;j<nr;j++)
-        for(int i=0;i<m;i++)
-            V(j*ldb+i)=rhs(i,j);
+    assert(M.Rows()==rhs.Rows());
+    assert(M.Columns()==sol.Rows());
+    assert(rhs.Columns()==sol.Columns());
 
+    MATRIX_MXN<T> TS;
+    MATRIX_VIEW<T> VM(M.Derived()),VS(sol.Derived());
+
+    bool b=false;
+    if(sol.Rows()<rhs.Rows())
+    {
+        TS.Resize(rhs.Rows(),rhs.Columns());
+        VS.Set(TS);
+        b=true;
+    }
+    VS.m=rhs.Rows();
+    VS=rhs;
+    VS.m=sol.Rows();
+
+    ARRAY<int> jpvt(VM.n);
     int rnk = 0;
-    int ret = xgelsy( LAPACK_COL_MAJOR, m, n, nr, A.Get_Array_Pointer(), m,
-        V.Get_Array_Pointer(), ldb, jpvt.Get_Array_Pointer(), rcond, &rnk );
+    int ret = xgelsy( LAPACK_COL_MAJOR, VM.m, VM.n, rhs.Columns(), VM.x, VM.s,
+        VS.x, VS.s, jpvt.Get_Array_Pointer(), rcond, &rnk );
     PHYSBAM_ASSERT(!ret);
     if(rank) *rank=rnk;
-
-    for(int j=0;j<nr;j++)
-        for(int i=0;i<n;i++)
-            sol(i,j)=V(j*ldb+i);
+    if(b) sol.Derived()=VS;
 }
 
-template<class T,class T_MATRIX,class T_VECTOR1,class T_VECTOR2>
-void Least_Squares_Solve(const MATRIX_BASE<T,T_MATRIX>& M,
-    const ARRAY_BASE<T,T_VECTOR1>& rhs,ARRAY_BASE<T,T_VECTOR2>& sol,
-    T rcond=(std::numeric_limits<T>::epsilon()), int * rank=0)
+// Preserves M
+template<class T,class T_MATRIX1,class T_MATRIX2,class T_MATRIX3>
+void Least_Squares_Solve(const MATRIX_BASE<T,T_MATRIX1>& M,
+    const MATRIX_BASE<T,T_MATRIX2>& rhs,
+    MATRIX_BASE<T,T_MATRIX3>& sol,T rcond, int * rank)
 {
-    int m=M.Rows(),n=M.Columns(),ldb=max(m,n);
-    assert(m==rhs.Size());
-    assert(n==sol.Size());
-    ARRAY<T> A(m*n);
-    ARRAY<T> V(rhs);
-    ARRAY<int> jpvt(n);
-    for(int j=0;j<n;j++)
-        for(int i=0;i<m;i++)
-            A(j*m+i)=M(i,j);
+    MATRIX_MXN<T> MM(M);
+    Least_Squares_Solve_Raw(MM,rhs,sol,rcond,rank);
+}
 
+// Destroys M
+template<class T,class T_MATRIX,class T_ARRAY1,class T_ARRAY2>
+void Least_Squares_Solve_Raw(MATRIX_BASE<T,T_MATRIX>& M,
+    const ARRAY_BASE<T,T_ARRAY1>& rhs,
+    ARRAY_BASE<T,T_ARRAY2>& sol,T rcond, int * rank)
+{
+    assert(M.Rows()==rhs.Size());
+    assert(M.Columns()==sol.Size());
+
+    ARRAY<T> TS;
+    MATRIX_VIEW<T> VM(M.Derived());
+    ARRAY_VIEW<T> VS;
+
+    bool b=true;
+    if(sol.Size()>=rhs.Size()) b=Make_View(VS,sol,TS);
+    else
+    {
+        TS.Resize(rhs.Size());
+        VS.Set(TS);
+    }
+    VS.m=rhs.Size();
+    VS=rhs;
+    VS.m=sol.Size();
+
+    int ldb=max(VM.m,VM.n);
+    ARRAY<int> jpvt(VM.n);
     int rnk = 0;
-    int ret = xgelsy( LAPACK_COL_MAJOR, m, n, 1, A.Get_Array_Pointer(), m,
-        V.Get_Array_Pointer(), ldb, jpvt.Get_Array_Pointer(), rcond, &rnk );
+    int ret = xgelsy( LAPACK_COL_MAJOR, VM.m, VM.n, 1, VM.x, VM.s,
+        VS.base_pointer, ldb, jpvt.Get_Array_Pointer(), rcond, &rnk );
     PHYSBAM_ASSERT(!ret);
     if(rank) *rank=rnk;
+    if(b) sol.Derived()=VS;
+}
 
-    for(int i=0;i<V.m;i++) sol(i)=V(i);
+// Preserves M
+template<class T,class T_MATRIX,class T_ARRAY1,class T_ARRAY2>
+void Least_Squares_Solve(const MATRIX_BASE<T,T_MATRIX>& M,
+    const ARRAY_BASE<T,T_ARRAY1>& rhs,
+    ARRAY_BASE<T,T_ARRAY2>& sol,T rcond, int * rank)
+{
+    MATRIX_MXN<T> MM(M);
+    Least_Squares_Solve_Raw(MM,rhs,sol,rcond,rank);
 }
 
 }
---
+++ Public_Library/Core/Matrices/Make_View.h
@@ -0,0 +1,51 @@
+//#####################################################################
+// Copyright 2019, Craig Schroeder.
+// This file is part of PhysBAM whose distribution is governed by the license contained in the accompanying file PHYSBAM_COPYRIGHT.txt.
+//#####################################################################
+// Class Make_View
+//#####################################################################
+#ifndef __Make_View__
+#define __Make_View__
+
+#include <Core/Arrays/ARRAY_VIEW.h>
+#include <Core/Matrices/MATRIX_VIEW.h>
+namespace PhysBAM{
+
+// Makes a view from a, copying data to tmp if necessary.
+// Returns true if a copy was made
+template<class T,class T_ARRAY>
+typename enable_if<HAS_POINTER<T_ARRAY>::value,bool>::type
+Make_View(ARRAY_VIEW<T>& v,ARRAY_BASE<T,T_ARRAY>& a,ARRAY<T>& tmp)
+{
+    v.Set(a.Derived());
+    return false;
+}
+
+template<class T,class T_ARRAY>
+typename enable_if<HAS_POINTER<T_ARRAY>::value,bool>::type
+Make_View(ARRAY_VIEW<const T>& v,const ARRAY_BASE<T,T_ARRAY>& a,ARRAY<T>& tmp)
+{
+    v.Set(a.Derived());
+    return false;
+}
+
+template<class T,class T_ARRAY>
+typename enable_if<!HAS_POINTER<T_ARRAY>::value,bool>::type
+Make_View(ARRAY_VIEW<T>& v,ARRAY_BASE<T,T_ARRAY>& a,ARRAY<T>& tmp)
+{
+    tmp=a;
+    v.Set(tmp);
+    return true;
+}
+
+template<class T,class T_ARRAY>
+typename enable_if<!HAS_POINTER<T_ARRAY>::value,bool>::type
+Make_View(ARRAY_VIEW<const T>& v,const ARRAY_BASE<T,T_ARRAY>& a,ARRAY<T>& tmp)
+{
+    tmp=a;
+    v.Set(tmp);
+    return true;
+}
+
+}
+#endif
