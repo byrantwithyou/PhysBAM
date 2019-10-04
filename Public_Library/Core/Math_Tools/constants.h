538c3383d40bc96ac53622b44867bd9c2c7bfb27
Craig Schroeder <snubdodecahedron@gmail.com>
2019-10-03 17:15:11 -0700

    Make constants be compile-time constants.

--- Public_Library/Core/Math_Tools/constants.h
+++ Public_Library/Core/Math_Tools/constants.h
@@ -8,13 +8,13 @@
 #include <cmath>
 namespace PhysBAM{
 
-const double pi=M_PI;
+static const constexpr double pi=M_PI;
 
-const double speed_of_light=2.99792458e8; // m/s
-const double plancks_constant=6.6260755e-34; // J*s
-const double boltzmanns_constant=1.380658e-23; // J/K
+static const constexpr double speed_of_light=2.99792458e8; // m/s
+static const constexpr double plancks_constant=6.6260755e-34; // J*s
+static const constexpr double boltzmanns_constant=1.380658e-23; // J/K
 
-const double unit_sphere_size[4]={0,2,pi,pi*4/3};
+static const constexpr double unit_sphere_size[4]={0,2,pi,pi*4/3};
 
 }
 #endif
