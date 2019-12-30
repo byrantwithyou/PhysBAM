acd5d67670c36f2ef59551da6080abb59672c8ed
Craig Schroeder <snubdodecahedron@gmail.com>
2019-12-30 15:06:06 -0800

    update sand dam break test

--- Projects/mpm/siggraph-2020/run_sand_dam_break.sh
+++ Projects/mpm/siggraph-2020/run_sand_dam_break.sh
@@ -2,4 +2,11 @@
 
 DIR="out_sand_dam_break"
 
-../mpm -3d 34 -strong_cfl -sound_cfl -symplectic_euler -use_exp_F -max_dt 1 -use_reflect -threads 8 -last_frame 30 -o $DIR -cfl_F 0.2 -no_implicit_plasticity -friction 1 -fooT1 10 -fooT2 1000 -fooT3 3 -scale_E .1 -use_reflect_friction -resolution 128
+RES=128
+
+CMD="../mpm -3d 34 -strong_cfl -sound_cfl -symplectic_euler -use_exp_F -max_dt 1 -use_reflect -threads 8 -last_frame 30 -cfl_F 0.2 -no_implicit_plasticity -fooT1 10 -fooT2 1000 -fooT3 3 -scale_E .1 -resolution $RES -use_reflect_friction"
+
+$CMD -o $DIR"_hi_mu" -friction 1 >& /dev/null
+$CMD -o $DIR"_lo_mu" -friction .3 >& /dev/null
+$CMD -o $DIR"_slip" -slip >& /dev/null
+$CMD -o $DIR"_stick" -stick >& /dev/null
