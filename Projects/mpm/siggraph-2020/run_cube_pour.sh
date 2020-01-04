663abf8d28bd50ce7d747f46f1dc6feb0f1cfaec
Craig Schroeder <snubdodecahedron@gmail.com>
2020-01-04 14:24:09 -0800

    Don't run with this option.

--- Projects/mpm/siggraph-2020/run_cube_pour.sh
+++ Projects/mpm/siggraph-2020/run_cube_pour.sh
@@ -2,4 +2,4 @@
 
 DIR="out_cube_pour"
 
-../mpm -3d 73 -strong_cfl -sound_cfl -symplectic_euler -use_exp_F -max_dt 1 -use_reflect -threads 8 -last_frame 200 -o $DIR -cfl_F 0.2 -friction .1 -dump_collisions -use_reflect_friction
+../mpm -3d 73 -strong_cfl -sound_cfl -symplectic_euler -max_dt 1 -use_reflect -threads 8 -last_frame 200 -o $DIR -cfl_F 0.2 -friction .1 -dump_collisions -use_reflect_friction
--- Projects/mpm/siggraph-2020/run_fluid_pour.sh
+++ Projects/mpm/siggraph-2020/run_fluid_pour.sh
@@ -2,4 +2,4 @@
 
 DIR="out_fluid_pour"
 
-../mpm -3d 49 -strong_cfl -sound_cfl -symplectic_euler -use_exp_F -max_dt 1 -use_reflect -threads 8 -last_frame 280 -o $DIR -cfl_F 0.2 -dilation_only -separate
+../mpm -3d 49 -strong_cfl -sound_cfl -symplectic_euler -max_dt 1 -use_reflect -threads 8 -last_frame 280 -o $DIR -cfl_F 0.2 -dilation_only -separate
