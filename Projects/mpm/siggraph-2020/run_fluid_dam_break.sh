501754634ff8782d7e77a3d73e22577908f11e35
Craig Schroeder <snubdodecahedron@gmail.com>
2020-01-10 22:37:05 -0800

    add single particle cfl

--- Projects/mpm/siggraph-2020/run_fluid_dam_break.sh
+++ Projects/mpm/siggraph-2020/run_fluid_dam_break.sh
@@ -2,4 +2,10 @@
 
 DIR="out_fluid_dam_break"
 
-../mpm 22 -strong_cfl -cfl_F 0.2 -sound_cfl -symplectic_euler -use_reflect -last_frame 300 -dilation_only -friction 0 -separate -use_reflect_friction -o $DIR"_not_stable" -max_dt 1
+CMD="../mpm 22 -strong_cfl -cfl_F 0.2 -sound_cfl -symplectic_euler -use_reflect -last_frame 400 -dilation_only -friction 0 -separate -use_reflect_friction -scale_E 10 -single_particle_cfl -cfl_c .9"
+
+$CMD -cfl_p 0.9 -o $DIR"_ours"
+$CMD -cfl_p 1.3 -o $DIR"_max_stable"
+$CMD -cfl_p 1.4 -o $DIR"_min_unstable"
+$CMD -cfl_p 2.0 -o $DIR"_unstable"
+
