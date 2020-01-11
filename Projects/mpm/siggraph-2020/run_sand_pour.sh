de31b82de83d757b05977f93ee717540781ec92f
YunxinSun <martin.yunxin.sun@gmail.com>
2020-01-11 11:04:12 -0800

    tweak test case sand pour

--- Projects/mpm/siggraph-2020/run_sand_pour.sh
+++ Projects/mpm/siggraph-2020/run_sand_pour.sh
@@ -2,5 +2,5 @@
 
 DIR="out_sand_pour"
 
-../mpm -3d 44 -strong_cfl -sound_cfl -symplectic_euler -max_dt 1 -use_reflect -threads 8 -last_frame 30 -o $DIR -cfl_F 0.2 -no_implicit_plasticity -friction .1  -fooT3 1 -resolution 10 -fooT2 35  -use_reflect_friction -sand_color
+../mpm -3d 44 -strong_cfl -sound_cfl -symplectic_euler -max_dt 1 -use_reflect -threads 8 -last_frame 144 -o $DIR -cfl_F 0.2 -no_implicit_plasticity -friction .1  -fooT3 1 -resolution 144 -fooT2 35  -use_reflect_friction -sand_color -scale_E .1
 
