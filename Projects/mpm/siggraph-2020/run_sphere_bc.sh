01df3792c0dca06013e39ea92ce511a247e22905
YunxinSun <martin.yunxin.sun@gmail.com>
2020-01-16 11:35:26 -0800

    change sphere stiffness

--- Projects/mpm/siggraph-2020/run_sphere_bc.sh
+++ Projects/mpm/siggraph-2020/run_sphere_bc.sh
@@ -4,9 +4,9 @@ DIR="out_sphere_bc"
 
 RES=32
 
-CMD="../mpm -3d 76 -strong_cfl -sound_cfl -symplectic_euler -max_dt 1 -use_reflect -threads 8 -last_frame 200 -cfl_F 0.2 -fooT1 5 -scale_E 10000 -resolution $RES -use_reflect_friction -float"
+CMD="../mpm -3d 76 -strong_cfl -sound_cfl -symplectic_euler -max_dt 1 -use_reflect -threads 8 -last_frame 200 -cfl_F 0.2 -fooT1 5 -scale_E 250 -resolution $RES -use_reflect_friction -float"
 
-$CMD -o $DIR"_no_mu" -separate -friction 0
-$CMD -o $DIR"_mu" -separate -friction .3 
-$CMD -o $DIR"_slip" -slip 
-$CMD -o $DIR"_stick" -stick 
+$CMD -o $DIR"_no_mu" -separate -friction 0 | grep frame&
+$CMD -o $DIR"_mu" -separate -friction .3 | grep frame &
+$CMD -o $DIR"_slip" -slip | grep frame &
+$CMD -o $DIR"_stick" -stick  | grep frame & 
